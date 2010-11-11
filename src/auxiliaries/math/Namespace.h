fba557d9e28d10aedd165dfc0729e1d949788bc5
Stephan Zibner <stephan.zibner@ini.rub.de>
2010-11-11 18:08:51 +0100

    whoops, forgot to hg add

---
+++ src/auxiliaries/math/Namespace.h
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------------------------------------------------
+ ----- Institute:   Ruhr-Universitaet Bochum
+                    Institut fuer Neuroinformatik
+
+ ----- File:        Namespace.h
+
+ ----- Author:      Stephan Zibner
+ ----- Email:       stephan.zibner@ini.rub.de
+ ----- Date:        2010 11 11
+
+ ----- Description: Namespace file for cedar::aux::math.
+
+ ----- Credits:
+ ---------------------------------------------------------------------------------------------------------------------*/
+
+#ifndef CEDAR_AUX_MATH_NAMESPACE_H
+#define CEDAR_AUX_MATH_NAMESPACE_H
+
+// LOCAL INCLUDES
+
+// PROJECT INCLUDES
+#include "src/Namespace.h"
+
+// SYSTEM INCLUDES
+#include <boost/smart_ptr.hpp>
+
+
+namespace cedar
+{
+  namespace aux
+  {
+    //!@brief Namespace for all exception classes.
+    namespace math
+    {
+//      class ExceptionBase;
+//      typedef boost::shared_ptr<ExceptionBase> ExceptionBasePtr;
+//
+//      class NullPointerException;
+//      typedef boost::shared_ptr<NullPointerException> NullPointerExceptionPtr;
+//
+//      class IndexOutOfRangeException;
+//      typedef boost::shared_ptr<IndexOutOfRangeException> IndexOutOfRangePtr;
+    }
+  }
+}
+
+#endif // CEDAR_AUX_MATH_NAMESPACE_H
