bfbc66646ccacf57dac73d2db96c43ffddcfd194
Mathis Richter <5708333+mathisrichter@users.noreply.github.com>
2010-11-12 15:39:50 +0100

    Added the folder for a ReferenceGeometry unit test.

---
+++ tests/unit/devices/robot/ReferenceGeometry/main.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------------------------------------------------
+ ----- Institute:   Ruhr-Universitaet Bochum
+                    Institut fuer Neuroinformatik
+
+ ----- File:        main.cpp
+
+ ----- Author:      Mathis Richter
+ ----- Email:       mathis.richter@ini.rub.de
+ ----- Date:        2010 11 12
+
+ ----- Description: Implements all unit tests for the \em cedar::dev::robot::ReferenceGeometry class.
+
+ ----- Credits:
+ ---------------------------------------------------------------------------------------------------------------------*/
+
+#include "src/devices/robot/ReferenceGeometry.h"
+#include <string>
+
+using namespace cedar::dev::robot;
+
+int main()
+{
+  ReferenceGeometry reference_geometry("test.conf");
+
+  std::cout << "name: " << reference_geometry.getName() << "\n";
+
+  return 0;
+}
