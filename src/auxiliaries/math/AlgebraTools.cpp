e09bbdc4417424750fd001f649c8246026cd3347
Stephan Zibner <stephan.zibner@ini.rub.de>
2010-11-12 11:43:16 +0100

    some work on AlgebraTools, now featuring vector inputs and outputs - unit test is missing

--- src/auxiliaries/math/AlgebraTools.cpp
+++ src/auxiliaries/math/AlgebraTools.cpp
@@ -39,60 +39,61 @@ using namespace std;
  */
 
 
-int solveQuadric( double c[ 3 ], double s[ 2 ] )
+std::vector<double> cedar::aux::math::solveQuadric(const std::vector<double>& rCoefficients)
 {
+  std::vector<double> solutions;
   double p, q, D;
-  
+  assert(rCoefficients.size() == 3);
   /* normal form: x^2 + px + q = 0 */
   
-  p = c[ 1 ] / (2 * c[ 2 ]);
-  q = c[ 0 ] / c[ 2 ];
+  p = rCoefficients[1] / (2 * rCoefficients[2]);
+  q = rCoefficients[0] / rCoefficients[2];
   
   D = p * p - q;
   
   if (IsZero(D))
   {
-    s[ 0 ] = - p;
-    return 1;
+    solutions.push_back(-p);
+    return solutions;
   }
   else if (D < 0)
   {
-    return 0;
+    return solutions;
   }
   else if (D > 0)
   {
     double sqrt_D = sqrt(D);
     
-    s[ 0 ] =   sqrt_D - p;
-    s[ 1 ] = - sqrt_D - p;
-    return 2;
+    solutions.push_back(sqrt_D - p);
+    solutions.push_back(-sqrt_D - p);
+    return solutions;
   }
-  return 0;
+  return solutions;
 }
 
-int solveCubic( double c[ 4 ], double s[ 3 ] )
+std::vector<double> cedar::aux::math::solveCubic(const std::vector<double>& rCoefficients)
 {
-  int     i, num;
-  double  sub;
-  double  A, B, C;
-  double  sq_A, p, q;
-  double  cb_p, D;
+  assert(rCoefficients.size() == 4);
+  std::vector<double> solutions;
+  double sub;
+  double A, B, C;
+  double sq_A, p, q;
+  double cb_p, D;
   
   /* normal form: x^3 + Ax^2 + Bx + C = 0 */
   
-  A = c[ 2 ] / c[ 3 ];
-  B = c[ 1 ] / c[ 3 ];
-  C = c[ 0 ] / c[ 3 ];
+  A = rCoefficients[2] / rCoefficients[3];
+  B = rCoefficients[1] / rCoefficients[3];
+  C = rCoefficients[0] / rCoefficients[3];
   
   /*  substitute x = y - A/3 to eliminate quadric term:
    x^3 +px + q = 0 */
   
   sq_A = A * A;
-  p = 1.0/3 * (- 1.0/3 * sq_A + B);
-  q = 1.0/2 * (2.0/27 * A * sq_A - 1.0/3 * A * B + C);
+  p = 1.0/3.0 * (-1.0/3.0*sq_A + B);
+  q = 1.0/2.0 * (2.0/27.0*A*sq_A - 1.0/3.0*A*B + C);
   
   /* use Cardano's formula */
-  
   cb_p = p * p * p;
   D = q * q + cb_p;
   
@@ -100,138 +101,157 @@ int solveCubic( double c[ 4 ], double s[ 3 ] )
   {
     if (IsZero(q)) /* one triple solution */
     {
-	    s[ 0 ] = 0;
-	    num = 1;
+      solutions.push_back(0);
     }
     else /* one single and one double solution */
     {
-	    double u = cbrt(-q);
-	    s[ 0 ] = 2 * u;
-	    s[ 1 ] = - u;
-	    num = 2;
+      double u = cbrt(-q);
+      solutions.push_back(2 * u);
+      solutions.push_back(-u);
     }
   }
   else if (D < 0) /* Casus irreducibilis: three real solutions */
   {
-    double phi = 1.0/3 * acos(-q / sqrt(-cb_p));
-    double t = 2 * sqrt(-p);
+    double phi = 1.0/3.0 * acos(-q / sqrt(-cb_p));
+    double t = 2.0 * sqrt(-p);
     
-    s[ 0 ] =   t * cos(phi);
-    s[ 1 ] = - t * cos(phi + M_PI / 3);
-    s[ 2 ] = - t * cos(phi - M_PI / 3);
-    num = 3;
+    solutions.push_back(t * cos(phi));
+    solutions.push_back(-t * cos(phi + M_PI / 3.0));
+    solutions.push_back(-t * cos(phi - M_PI / 3.0));
   }
   else /* one real solution */
   {
     double sqrt_D = sqrt(D);
     double u = cbrt(sqrt_D - q);
-    double v = - cbrt(sqrt_D + q);
+    double v = -cbrt(sqrt_D + q);
     
-    s[ 0 ] = u + v;
-    num = 1;
+    solutions.push_back(u + v);
   }
   
   /* resubstitute */
   
-  sub = 1.0/3 * A;
+  sub = 1.0/3.0 * A;
   
-  for (i = 0; i < num; ++i)
-    s[ i ] -= sub;
-  
-  return num;
+  for (unsigned int i = 0; i < solutions.size(); ++i)
+  {
+    solutions[i] -= sub;
+  }
+  return solutions;
 }
 
-int solveQuartic( double c[ 5 ], double s[ 4 ] )
+std::vector<double> cedar::aux::math::solveQuartic(const std::vector<double>& rCoefficients)
 {
-  double  coeffs[ 4 ];
+  assert(rCoefficients.size() == 5);
+  std::vector<double> coeffs;
+  coeffs.resize(4);
+  std::vector<double> solutions;
   double  z, u, v, sub;
   double  A, B, C, D;
   double  sq_A, p, q, r;
-  int     i, num;
+//  int i, num;
   
   /* normal form: x^4 + Ax^3 + Bx^2 + Cx + D = 0 */
   
-  A = c[ 3 ] / c[ 4 ];
-  B = c[ 2 ] / c[ 4 ];
-  C = c[ 1 ] / c[ 4 ];
-  D = c[ 0 ] / c[ 4 ];
+  A = rCoefficients[3] / rCoefficients[4];
+  B = rCoefficients[2] / rCoefficients[4];
+  C = rCoefficients[1] / rCoefficients[4];
+  D = rCoefficients[0] / rCoefficients[4];
   
   /*  substitute x = y - A/4 to eliminate cubic term:
    x^4 + px^2 + qx + r = 0 */
   
-  sq_A = A * A;
-  p = - 3.0/8 * sq_A + B;
-  q = 1.0/8 * sq_A * A - 1.0/2 * A * B + C;
-  r = - 3.0/256*sq_A*sq_A + 1.0/16*sq_A*B - 1.0/4*A*C + D;
+  sq_A = A*A;
+  p = -3.0/8.0*sq_A + B;
+  q = 1.0/8.0*sq_A*A - 1.0/2.0*A*B + C;
+  r = -3.0/256.0*sq_A*sq_A + 1.0/16.0*sq_A*B - 1.0/4.0*A*C + D;
   
-  if ( IsZero(r) )
+  if (IsZero(r))
   {
     /* no absolute term: y(y^3 + py + q) = 0 */
-    
-    coeffs[ 0 ] = q;
-    coeffs[ 1 ] = p;
-    coeffs[ 2 ] = 0;
-    coeffs[ 3 ] = 1;
-    
-    num = solveCubic(coeffs, s);
-    
-    s[ num++ ] = 0;
+    coeffs[0] = q;
+    coeffs[1] = p;
+    coeffs[2] = 0.0;
+    coeffs[3] = 1.0;
+    solutions = solveCubic(coeffs);
+    solutions.push_back(0.0);
   }
   
   else
   {
-    // solve the resolvent cubic ...
-    
-    coeffs[ 0 ] = 1.0/2 * r * p - 1.0/8 * q * q;
-    coeffs[ 1 ] = - r;
-    coeffs[ 2 ] = - 1.0/2 * p;
-    coeffs[ 3 ] = 1;
-    
-    (void) solveCubic(coeffs, s);
-    
-    // ... and take the one real solution ...
-    
-    z = s[ 0 ];
-    
-    // ... to build two quadric equations
-    
+    // solve the resolvent cubic and take the one real solution ...
+    coeffs[0] = 1.0/2.0*r*p - 1.0/8.0*q*q;
+    coeffs[1] = -r;
+    coeffs[2] = -1.0/2.0*p;
+    coeffs[3] = 1.0;
+    z = solveCubic(coeffs).at(0);
+
+    // ... to build two quadratic equations
     u = z * z - r;
     v = 2 * z - p;
     
     if (IsZero(u))
+    {
       u = 0;
+    }
     else if (u > 0)
+    {
       u = sqrt(u);
+    }
     else
-      return 0;
+    {
+      return solutions;
+    }
     
     if (IsZero(v))
+    {
       v = 0;
+    }
     else if (v > 0)
+    {
       v = sqrt(v);
+    }
     else
-      return 0;
+    {
+      return solutions;
+    }
+    coeffs[0] = z - u;
+    if (q < 0)
+    {
+      coeffs[1] = -v;
+    }
+    else
+    {
+      coeffs[1] = v;
+    }
+    coeffs[2] = 1;
     
-    coeffs[ 0 ] = z - u;
-    coeffs[ 1 ] = q < 0 ? -v : v;
-    coeffs[ 2 ] = 1;
+    solutions = solveQuadric(coeffs);
     
-    num = solveQuadric(coeffs, s);
-    
-    coeffs[ 0 ]= z + u;
-    coeffs[ 1 ] = q < 0 ? v : -v;
-    coeffs[ 2 ] = 1;
-    
-    num += solveQuadric(coeffs, s + num);
-    
+    coeffs[0]= z + u;
+    if (q < 0)
+    {
+      coeffs[1] = v;
+    }
+    else
+    {
+      coeffs[1] = -v;
+    }
+    coeffs[2] = 1;
+    std::vector<double> second_solutions = solveQuadric(coeffs);
+    solutions.insert(solutions.end(), second_solutions.begin(), second_solutions.end());
   }
-  
   // resubstitute
-  sub = 1.0/4 * A;
+  sub = 1.0/4.0*A;
   
-  for (i = 0; i < num; ++i)
-    s[ i ] -= sub;
-  
-  return num;
-  
+  for (unsigned i = 0; i < solutions.size(); ++i)
+  {
+    solutions[i] -= sub;
+  }
+  return solutions;
+}
+
+std::vector<double> cedar::aux::math::solvePolynomial(const std::vector<double>& coefficients)
+{
+  std::vector<double> solutions;
+  return solutions;
 }
