dbd4cbe0b6c5f6dcc067ed5fc675b8d47773399a
Mathis Richter <mathis.richter@ini.rub.de>
2010-11-15 19:38:43 +0100

    Added a KinematicChain.cpp file.

--- src/auxiliaries/Base.h
+++ src/auxiliaries/Base.h
@@ -8,7 +8,7 @@
  ----- Email:       mathis.richter@ini.rub.de
  ----- Date:        2010 10 12
 
- ----- Description: Header for the \em cedar::aux::Base class.
+ ----- Description: Header for the @em cedar::aux::Base class.
 
  ----- Credits:
  ---------------------------------------------------------------------------------------------------------------------*/
