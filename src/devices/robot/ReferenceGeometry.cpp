1ed27940d6b255c4cffa7ac3d962226321ff5f6f
Mathis Richter <5708333+mathisrichter@users.noreply.github.com>
2010-11-16 14:57:29 +0100

    Fixed Doxygen coding standard violation.

--- src/devices/robot/ReferenceGeometry.cpp
+++ src/devices/robot/ReferenceGeometry.cpp
@@ -8,7 +8,7 @@
  ----- Email:       mathis.richter@ini.rub.de
  ----- Date:        2010 10 25
 
- ----- Description: Header for the \em ReferenceGeometry class.
+ ----- Description: Header for the @em ReferenceGeometry class.
 
  ----- Credits:
  ---------------------------------------------------------------------------------------------------------------------*/
