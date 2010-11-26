7a51eff4edbf4ebb9d1348d33665a13d701add88
Mathis Richter <5708333+mathisrichter@users.noreply.github.com>
2010-11-26 12:18:53 +0100

    Added cedarAux.h and cedarDev.h.
    Those files include all headers of their respective libraries.

---
+++ cedar/cedarAux.h
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------------------------------------------------
+ ----- Institute:   Ruhr-Universitaet Bochum
+                    Institut fuer Neuroinformatik
+
+ ----- File:        cedarAux.h
+
+ ----- Maintainer:  Mathis Richter
+ ----- Email:       mathis.richter@ini.rub.de
+ ----- Date:        2010 11 26
+
+ ----- Description: Header file that includes all headers of the auxiliaries library.
+
+ ----- Credits:
+ ---------------------------------------------------------------------------------------------------------------------*/
+
+#ifndef CEDAR_CEDAR_AUX_H
+#define CEDAR_CEDAR_AUX_H
+
+// LOCAL INCLUDES
+
+// PROJECT INCLUDES
+#include "cedar/auxiliaries/Base.h"
+#include "cedar/auxiliaries/ConfigurationInterface.h"
+#include "cedar/auxiliaries/LogFile.h"
+#include "cedar/auxiliaries/namespace.h"
+#include "cedar/auxiliaries/Thread.h"
+#include "cedar/auxiliaries/UserData.h"
+
+#include "cedar/auxiliaries/exceptions/ExceptionBase.h"
+#include "cedar/auxiliaries/exceptions/IndexOutOfRangeException.h"
+#include "cedar/auxiliaries/exceptions/namespace.h"
+#include "cedar/auxiliaries/exceptions/NullPointerException.h"
+
+#include "cedar/auxiliaries/math/algebraTools.h"
+#include "cedar/auxiliaries/math/coordinateTransformations.h"
+#include "cedar/auxiliaries/math/Limits.h"
+#include "cedar/auxiliaries/math/namespace.h"
+#include "cedar/auxiliaries/math/screwCalculus.h"
+#include "cedar/auxiliaries/math/sigmoids.h"
+#include "cedar/auxiliaries/math/tools.h"
+
+// SYSTEM INCLUDES
+
+#endif // CEDAR_CEDAR_AUX_H
