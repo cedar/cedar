e14f2b33e8f9894216e7095ae78dd99cbe32bdac
Hendrik Reimann <hendrik.reimann@ini.rub.de>
2010-12-04 23:39:27 -0600

    problems with pointer casting in SceneWidget

--- cedar/auxiliaries/gl/Object.cpp
+++ cedar/auxiliaries/gl/Object.cpp
@@ -100,51 +100,6 @@ int Object::resolution()
   return mResolution;
 }
 
-//cv::Mat Object::position()
-//{
-//  return mPosition.clone();
-//}
-//
-//double Object::positionX()
-//{
-//  return mPosition.at<double>(0, 0);
-//}
-//
-//double Object::positionY()
-//{
-//  return mPosition.at<double>(1, 0);
-//}
-//
-//double Object::positionZ()
-//{
-//  return mPosition.at<double>(2, 0);
-//}
-//
-//cv::Mat Object::orientationAngles()
-//{
-//  return mOrientationAngles.clone();
-//}
-//
-//double Object::orientationAngleAlpha()
-//{
-//  return mOrientationAngles.at<double>(0, 0);
-//}
-//
-//double Object::orientationAngleBeta()
-//{
-//  return mOrientationAngles.at<double>(1, 0);
-//}
-//
-//double Object::orientationAngleGamma()
-//{
-//  return mOrientationAngles.at<double>(2, 0);
-//}
-//
-//cv::Mat Object::transformation()
-//{
-//  return mTransformation.clone();
-//}
-
 double Object::colorR()
 {
   return mColorR;
@@ -182,58 +137,10 @@ void Object::setColor(double R, double G, double B)
   mColorB = B;
 }
 
-//void Object::setPosition(double x, double y, double z)
-//{
-//  mPosition.at<double>(0, 0) = x;
-//  mPosition.at<double>(1, 0) = y;
-//  mPosition.at<double>(2, 0) = z;
-//  updateTransformation();
-//}
-//
-//void Object::setPosition(const cv::Mat& position)
-//{
-//  assert(position.type() == mPosition.type());
-//  mPosition = position.clone();
-//  updateTransformation();
-//}
-//
-//void Object::setOrientationAngles(double alpha, double beta, double gamma)
-//{
-//  mOrientationAngles.at<double>(0, 0) = cedar::aux::math::normalizeAngle(alpha);
-//  mOrientationAngles.at<double>(1, 0) = cedar::aux::math::normalizeAngle(beta);
-//  mOrientationAngles.at<double>(2, 0) = cedar::aux::math::normalizeAngle(gamma);
-//  updateTransformation();
-//}
-//
-//void Object::setOrientationAngles(cv::Mat angles)
-//{
-//  mOrientationAngles.at<double>(0, 0) = cedar::aux::math::normalizeAngle(angles.at<double>(0, 0));
-//  mOrientationAngles.at<double>(1, 0) = cedar::aux::math::normalizeAngle(angles.at<double>(1, 0));
-//  mOrientationAngles.at<double>(2, 0) = cedar::aux::math::normalizeAngle(angles.at<double>(2, 0));
-//  updateTransformation();
-//}
-//
-//void Object::updateTransformation()
-//{
-//  double alpha = mOrientationAngles.at<double>(0, 0);
-//  double beta  = mOrientationAngles.at<double>(1, 0);
-//  double gamma = mOrientationAngles.at<double>(2, 0);
-//	mTransformation.at<double>(0, 0) = cos(alpha) * cos(beta) * cos(gamma) - sin(alpha) * sin(gamma);
-//	mTransformation.at<double>(1, 0) = sin(alpha) * cos(beta) * cos(gamma) + cos(alpha) * sin(gamma);
-//	mTransformation.at<double>(2, 0) = - sin(beta) * cos(gamma);
-//	mTransformation.at<double>(0, 1) = - cos(alpha) * cos(beta) * sin(gamma) - sin(alpha) * cos(gamma);
-//	mTransformation.at<double>(1, 1) = - sin(alpha) * cos(beta) * sin(gamma) + cos(alpha) * cos(gamma);
-//	mTransformation.at<double>(2, 1) = sin(beta) * sin(gamma);
-//	mTransformation.at<double>(0, 2) = cos(alpha) * sin(beta);
-//	mTransformation.at<double>(1, 2) = sin(alpha) * sin(beta);
-//	mTransformation.at<double>(2, 2) = cos(beta);
-//
-//	mTransformation.at<double>(0, 3) = mPosition.at<double>(0, 0);
-//	mTransformation.at<double>(1, 3) = mPosition.at<double>(1, 0);
-//	mTransformation.at<double>(2, 3) = mPosition.at<double>(2, 0);
-//
-//	mTransformation.at<double>(3, 3) = 1;
-//}
+cedar::aux::ObjectPtr Object::getObject()
+{
+  return mpObject;
+}
 
 void Object::setVisibility(bool state)
 { 
--- cedar/auxiliaries/gl/Object.h
+++ cedar/auxiliaries/gl/Object.h
@@ -93,33 +93,6 @@ public:
   //!@brief returns B value of main object color in RGB
   double colorB();
   
-  //!@brief returns position of the object in homogeneous coordinates
-//  cv::Mat position();
-  
-  //!@brief returns x-position of the object frame origin in world frame
-//  double positionX();
-  
-  //!@brief returns y-position of the object frame origin in world frame
-//  double positionY();
-  
-  //!@brief returns z-position of the object frame origin in world frame
-//  double positionZ();
-  
-  //!@brief returns object frame orientation as vector of ???-Euler angles
-//  cv::Mat orientationAngles();
-  
-  //!@brief returns alpha (first) angle of the object orientation in ???-Euler angles
-//  double orientationAngleAlpha();
-  
-  //!@brief returns beta (second) angle of the object orientation in ???-Euler angles
-//  double orientationAngleBeta();
-  
-  //!@brief returns gamma (third) angle of the object orientation in ???-Euler angles
-//  double orientationAngleGamma();
-  
-  //!@brief returns the 4 \time 4 rigid transformation matrix of the object frame relative to the world frame
-//  cv::Mat transformation();
-  
   //!@brief switch betwen drawing the object with full surfaces or as wire frame only
   void drawAsWireFrame(const bool state);
 
@@ -137,30 +110,19 @@ public:
    * @param B    value for blue channel in RGB color
    */
   void setColor(const double R, const double G, const double B);
-  
-  /*!@brief set the position of the object frame origin in the world frame
-   * @param x    coordinates of 
-   * @param y    value for green channel in RGB color
-   * @param z    value for blue channel in RGB color
+
+  /*!@brief returns a smart pointer to the object being visualized
+   *
+   * @return    smart pointer to the object
    */
-//  void setPosition(const double x, const double y, const double z);
-  
-  //!@brief set the position of the object frame origin in the world frame
-//  void setPosition(const cv::Mat& position);
-  
-  //!@brief set the orientation of the object frame, given in ??? Euler angles
-//  void setOrientationAngles(const double alpha, const double beta, const double gamma);
-  
-  //!@brief set the orientation of the object frame, given in ??? Euler angles
-//  void setOrientationAngles(const cv::Mat angles);
- 
+  cedar::aux::ObjectPtr getObject();
+
   //--------------------------------------------------------------------------------------------------------------------
   // private methods
   //--------------------------------------------------------------------------------------------------------------------
 private:
+  //!@brief initialization function
   virtual void init();
-  //!@brief recalculates the rigid transformation to the object frame from position and orientation
-//  void updateTransformation();
   
 public slots:
   //!@brief turns the visibility of the object on and off
