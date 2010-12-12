7b82bb6a0991ec7e587a7df6d13355d242c0b878
Hendrik Reimann <hendrik.reimann@ini.rub.de>
2010-12-12 16:40:20 +0100

    cosmetic changes to make KinematicChain adhere to coding standard

---
+++ cedar/auxiliaries/LoopedThread.cpp
@@ -0,0 +1,255 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        LoopedThread.cpp
+
+    Maintainer:  Bjoern Weghenkel
+    Email:       bjoern.weghenkel@ini.rub.de
+    Date:        2010 12 02
+
+    Description: Implementation of the @em cedar::aux::LoopedThread class.
+
+    Credits:
+
+======================================================================================================================*/
+
+
+// LOCAL INCLUDES
+#include "auxiliaries/LoopedThread.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+//#include <QTime>
+
+
+using namespace std;
+using namespace boost::posix_time;
+
+//------------------------------------------------------------------------------
+// constructors and destructor
+//------------------------------------------------------------------------------
+
+cedar::aux::LoopedThread::LoopedThread(double stepSize, double idleTime, const std::string& configFileName)
+:
+cedar::aux::ConfigurationInterface(configFileName)
+{
+  // TODO: step size should be moved to the configuration file
+  addParameter(&_mName, "name", "<name>");
+  readConfiguration();
+  mStepSize = microseconds(static_cast<unsigned int>(1000 * stepSize + 0.5));
+  mIdleTime = static_cast<unsigned int>(1000 * idleTime + 0.5);
+  mStop  = false;
+  mUseFixedStepSize = true;
+  mSimulatedTime = microseconds(0);
+  initStatistics();
+}
+
+cedar::aux::LoopedThread::~LoopedThread()
+{
+}
+
+//------------------------------------------------------------------------------
+// methods
+//------------------------------------------------------------------------------
+
+void cedar::aux::LoopedThread::stop(unsigned int time, bool suppressWarning)
+{
+  mStop = true;
+  wait(time);
+  if (suppressWarning == false && mMaxStepsTaken > 1.01 && mSimulatedTime.total_microseconds() == 0)
+  {
+    cout << "Warning: The system was not fast enough to stay to scheduled thread timing. ";
+    cout << "Consider using a larger step size." << endl;
+    cout << "Execution stats:" << endl;
+    cout << "  avg. time steps between execution: " << (double) mSumOfStepsTaken / (double) mNumberOfSteps << endl;
+    cout << "  max. time steps between execution: " << mMaxStepsTaken << endl;
+  }
+}
+
+void cedar::aux::LoopedThread::run(void)
+{
+  // we do not want to change the step size while running
+  time_duration step_size = mStepSize;
+  bool use_fixed_step_size = mUseFixedStepSize;
+  initStatistics();
+
+  // which mode?
+  if(step_size.total_microseconds() == 0)
+  {
+    // fast running mode
+
+    mLastTimeStepStart = microsec_clock::universal_time();
+    mLastTimeStepEnd = microsec_clock::universal_time();
+    time_duration time_difference;
+
+    while(!mStop)
+    {
+      usleep(mIdleTime);
+      mLastTimeStepStart = mLastTimeStepEnd;
+      mLastTimeStepEnd = microsec_clock::universal_time();
+
+      if(mSimulatedTime.total_microseconds() == 0)
+      {
+        time_difference = mLastTimeStepEnd - mLastTimeStepStart;
+        step(time_difference.total_microseconds() * 0.001);
+      }
+      else
+      {
+        step(mSimulatedTime.total_microseconds() * 0.001);
+      }
+    }
+  }
+  else // if(step_size == 0)
+  {
+    // regular mode with time slots
+
+    // initialization
+    mLastTimeStepStart = microsec_clock::universal_time();
+    mLastTimeStepEnd = microsec_clock::universal_time();
+    ptime scheduled_wakeup = mLastTimeStepEnd + step_size;
+    srand(microsec_clock::universal_time().time_of_day().total_milliseconds());
+
+    // some auxiliary variables
+    time_duration sleep_duration = microseconds(0);
+    time_duration step_duration = microseconds(0);
+
+    while(!mStop)
+    {
+      // sleep until next wake up time
+      sleep_duration = scheduled_wakeup - microsec_clock::universal_time();
+      usleep(std::max<int>(0, sleep_duration.total_microseconds()));
+
+      // determine time since last run
+      mLastTimeStepStart = mLastTimeStepEnd;
+      mLastTimeStepEnd = scheduled_wakeup;
+      step_duration = mLastTimeStepEnd - mLastTimeStepStart;
+
+      // calculate number of time steps taken
+      double steps_taken = static_cast<double>(step_duration.total_microseconds()) / static_cast<double>(step_size.total_microseconds());
+      unsigned int full_steps_taken = static_cast<unsigned int>( steps_taken + 0.5 );
+
+      #ifdef DEBUG
+      // print warning if time steps have been skipped
+      if( steps_taken > 1.01 )
+        cerr << "WARNING: " << steps_taken << " time steps taken at once! "
+        << "Your system might be too slow for an execution interval of "
+        << step_size.total_microseconds() << " microseconds. Consider using a "
+        << "longer interval!" << endl;
+      #endif
+
+      // here we have to distinguish between the different working modes of the
+      // thread. either we want to wake up on the next fixed time step or as
+      // soon as possible if we are already late.
+      if(use_fixed_step_size)
+      {
+        // update statistics
+        updateStatistics(full_steps_taken);
+
+        // call step function
+        if(mSimulatedTime.total_microseconds() == 0)
+        {
+          step(full_steps_taken * step_size.total_microseconds() * 0.001);
+        }
+        else
+        {
+          step(mSimulatedTime.total_microseconds() * 0.001);
+        }
+
+        // schedule the next wake up
+        while(scheduled_wakeup < microsec_clock::universal_time())
+        {
+          scheduled_wakeup = scheduled_wakeup + step_size;
+        }
+
+      }
+      else // if(mUseFixedStepSize)
+      {
+        // update statistics
+        updateStatistics(steps_taken);
+
+        // call step function
+        if(mSimulatedTime.total_microseconds() == 0)
+        {
+          step(steps_taken * step_size.total_microseconds() * 0.001);
+        }
+        else
+        {
+          step(mSimulatedTime.total_microseconds() * 0.001);
+        }
+
+        // schedule the next wake up
+        scheduled_wakeup = max<ptime>(scheduled_wakeup + step_size, microsec_clock::universal_time());
+
+      } // if(mUseFixedStepSize)
+
+    } // while(!mStop)
+
+  } // if(step_size != 0)
+
+  mStop = false;
+  return;
+}
+
+void cedar::aux::LoopedThread::initStatistics()
+{
+  mNumberOfSteps = 0;
+  mSumOfStepsTaken = 0.0;
+  mMaxStepsTaken = 0.0;
+}
+
+void cedar::aux::LoopedThread::updateStatistics(double stepsTaken)
+{
+
+  double old_sum = mSumOfStepsTaken;
+
+  mNumberOfSteps += 1.0;
+  mSumOfStepsTaken += stepsTaken;
+  if(stepsTaken > mMaxStepsTaken)
+  {
+    mMaxStepsTaken = stepsTaken;
+  }
+
+  if(old_sum > mSumOfStepsTaken)
+  {
+    cerr << "Warning: Value overflow in thread statistics. Statistics will be reseted." << endl;
+    initStatistics();
+  }
+
+  return;
+}
+
+void cedar::aux::LoopedThread::singleStep() {
+  if(!isRunning())
+  {
+    if(mSimulatedTime.total_microseconds() == 0)
+    {
+      step(mStepSize.total_microseconds());
+    }
+    else
+    {
+      step(mSimulatedTime.total_microseconds());
+    }
+  }
+}
---
+++ tests/interactive/devices/gl/KinematicChain/main.cpp
@@ -0,0 +1,105 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+ ----- Institute:   Ruhr-Universitaet Bochum
+                    Institut fuer Neuroinformatik
+
+ ----- File:        main.cpp
+
+ ----- Maintainer:  Hendrik Reimann
+ ----- Email:       hendrik.reimann@ini.rub.de
+ ----- Date:        2010 11 17
+
+ ----- Description: interactive test for the kinematic chain simulation
+
+ ----- Credits:
+ -----------------------------------------------------------------------------*/
+
+// LOCAL INCLUDES
+
+// PROJECT INCLUDES
+#include "devices/robot/ReferenceGeometry.h"
+#include "devices/robot/SimulatedKinematicChain.h"
+#include "devices/robot/KinematicChainModel.h"
+#include "devices/robot/gl/KinematicChain.h"
+#include "auxiliaries/gl/Scene.h"
+#include "auxiliaries/gl/Block.h"
+#include "auxiliaries/gl/Viewer.h"
+#include "auxiliaries/gl/SceneWidget.h"
+
+
+// SYSTEM INCLUDES
+#include <QApplication>
+
+using namespace std;
+using namespace cv;
+using namespace cedar::aux::gl;
+using namespace cedar::dev::robot::gl;
+using namespace cedar::dev::robot;
+
+
+int main(int argc, char **argv)
+{
+  QApplication a(argc, argv);
+
+  // create simulated arm
+  cedar::dev::robot::KinematicChainPtr p_test_arm(new SimulatedKinematicChain(std::string("../../../tests/interactive/devices/gl/KinematicChain/test_arm.conf")));
+
+  // create model of simulated arm
+  KinematicChainModelPtr p_test_arm_model(new KinematicChainModel(p_test_arm));
+  p_test_arm_model->startTimer(50);
+
+  // create gl visualization object
+  cedar::dev::robot::gl::KinematicChainPtr p_test_arm_visualization(new cedar::dev::robot::gl::KinematicChain(p_test_arm_model));
+
+  // create scene and viewer to display the arm
+  ScenePtr p_scene(new cedar::aux::gl::Scene);
+  p_scene->setSceneLimit(2);
+  p_scene->drawFloor(true);
+
+  cedar::aux::gl::ObjectPtr p_object = p_test_arm_visualization;
+  p_scene->addObject(p_object);
+
+  cout << "name = " << p_test_arm->getName() << endl;
+  cout << "name = " << p_object->getObjectName() << endl;
+
+
+  // create a simple viewer for the scene
+  Viewer viewer(p_scene);
+  viewer.show();
+  viewer.setSceneRadius(p_scene->getSceneLimit());
+  viewer.startTimer(50);
+
+  // create a widget to control the scene
+  SceneWidgetPtr p_scene_widget(new SceneWidget(p_scene));
+  p_scene_widget->show();
+
+  p_test_arm->start();
+  p_test_arm->setJointAngle(2, -M_PI*0.5);
+  p_test_arm->setJointAngle(3, M_PI*0.5);
+  p_test_arm->setJointVelocity(0, .3);
+  p_test_arm->setJointVelocity(1, -1);
+  p_test_arm->setJointVelocity(2, -.5);
+  p_test_arm->setJointVelocity(3, .3);
+
+  a.exec();
+  return 0;
+}
