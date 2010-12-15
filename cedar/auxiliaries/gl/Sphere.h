5c933473a3a3cd6ff37d76c22fa81d339ca59fac
Stephan Zibner <37666884+raul-ini@users.noreply.github.com>
2010-12-15 09:18:32 +0100

    improve documentation

---
+++ cedar/auxiliaries/gl/Sphere.h
@@ -0,0 +1,97 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+ ----- Institute:   Ruhr-Universitaet-Bochum
+                    Institut fuer Neuroinformatik
+ 
+ ----- File:        Sphere.h
+ 
+ ----- Maintainer:  Hendrik Reimann
+ ------Email:       hendrik.reimann@ini.rub.de
+ ----- Date:        2010 10 28
+ 
+ ----- Description: visualization for a sphere
+ 
+ ----- Credits:     
+ ---------------------------------------------------------------------------------------------------------------------*/
+
+#ifndef CEDAR_AUX_GL_SPHERE_H
+#define CEDAR_AUX_GL_SPHERE_H
+
+// LOCAL INCLUDES
+#include "namespace.h"
+#include "Object.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+
+class cedar::aux::gl::Sphere : public cedar::aux::gl::Object
+{
+public:
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+  /*!@brief standard constructor. 
+   * @param name    identifier of the instance, should be unique
+   */
+	Sphere(cedar::aux::ObjectPtr pObject);
+
+  /*!@brief constructor. 
+   * @param name    identifier of the instance, should be unique
+   * @param radius    radius of the sphere
+   * @param R    color, value for red channel in RGB
+   * @param G    color, value for green channel in RGB
+   * @param B    color, value for blue channel in RGB
+   */
+  Sphere(
+          cedar::aux::ObjectPtr pObject,
+          const double radius,
+          const double R=1,
+          const double G=0,
+          const double B=0
+        );
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief draws a visualization of the object in the current GL context
+	void draw();
+
+  /*!@brief set radius of the sphere
+   * @param value    new radius
+   */
+	void setRadius(double value);
+	
+  /*!@brief get radius of the sphere
+   * @return    radius
+   */
+  double radius();
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+	double mRadius;
+};
+
+#endif // CEDAR_AUX_GL_SPHERE_H
