1d301526fd98ef87193ece53d94174c171883488
Bjoern Weghenkel <bjoern.weghenkel@ini.ruhr-uni-bochum.de>
2011-01-24 16:31:33 +0100

    No thread for velocity mode

--- tests/interactive/devices/gui/AmtecKinematicChain/main.cpp
+++ tests/interactive/devices/gui/AmtecKinematicChain/main.cpp
@@ -59,6 +59,7 @@ int main(int argc, char *argv[]) {
   KinematicChainPtr p_kinematic_chain(new AmtecKinematicChain("../../../tests/interactive/devices/gui/AmtecKinematicChain/test_arm.conf"));
   QApplication app(argc, argv);
   KinematicChainWidget widget(p_kinematic_chain);
+  widget.simulateAcceleration(true);
   widget.show();
   return app.exec();
 }
