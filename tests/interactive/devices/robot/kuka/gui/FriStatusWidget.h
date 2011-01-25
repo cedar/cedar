5300b9cb2ceca9a155b75296c160631a89c1ecea
gknips <gknips@localhost>
2011-01-25 11:40:12 +0100

    Started creating the GUI for testing

---
+++ tests/interactive/devices/robot/kuka/gui/FriStatusWidget.h
@@ -0,0 +1,119 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        FriStatusWidget.h
+
+    Maintainer:  Guido Knips
+    Email:       gknips@ini.rub.de
+    Date:        2011 01 25
+
+    Description: Widget for showing FRI information
+
+    Credits:
+
+======================================================================================================================*/
+//TODO find the correct namespace
+
+#ifndef CEDAR_TEST_FRI_STATUS_WIDGET_H
+#define CEDAR_TEST_FRI_STATUS_WIDGET_H
+
+// LOCAL INCLUDES
+
+// PROJECT INCLUDES
+#include "devices/robot/kuka/KukaInterface.h"
+
+// SYSTEM INCLUDES
+#include <QObject>
+
+/*!@brief Widget that displays informations about the status of the KUKA-FRI
+ *
+ * This includes the status, the connection quality, the sample time and if the robot is powered
+ */
+class cedar::test::FriStatusWidget :
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // macros
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  Q_OBJECT
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  /*!@brief The constructor.
+
+   * @param pKukaIn pointer to an instance of KukaInterface, where this widget gets the data
+   */
+  FriStatusWidget(cedar::dev::robot::kuka::KukaInterface * pKukaIn, QWidget *parent=0);
+
+  //!@brief Destructor
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  /*!@brief initializes the class
+   */
+  void init();
+  /*!@brief updates the displayed information
+   */
+  void updateInformation();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet (hopefully never!)
+protected:
+  // none yet
+private:
+  cedar::dev::robot::kuka::KukaInterface *mpKukaIn //!<this is an external reference
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet (hopefully never!)
+protected:
+  // none yet
+
+private:
+  // none yet
+
+}; // class cedar::test::FriStatusWidget
+
+#endif // CEDAR_TEST_FRI_STATUS_WIDGET_H
+
