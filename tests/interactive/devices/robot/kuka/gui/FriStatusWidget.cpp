05d7d65c677d53d409131cdf9b2369d8835d2100
gknips <gknips@localhost>
2011-01-25 11:40:12 +0100

    Started creating the GUI for testing

---
+++ tests/interactive/devices/robot/kuka/gui/FriStatusWidget.cpp
@@ -0,0 +1,77 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        FriStatusWidget.cpp
+
+    Maintainer:  Guido Knips
+    Email:       gknips@ini.rub.de
+    Date:        2011 01 25
+
+    Description: implementation of the widget that shows information about the status of the Kuka FRI
+
+    Credits:
+
+======================================================================================================================*/
+
+// LOCAL INCLUDES
+#include "FriStatusWidget.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+#include <sstream>
+
+using namespace cedar::dev::robot::kuka;
+using namespace cedar::test;
+using namespace std;
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+void FriStatusWidget::updateInformation()
+{
+  //Translations from status-/quality-values to text
+  string quality_names[] = { string("BAD (0)"), string("UNACCEPTABLE (1)"), string("GOOD (2)"), string("PERFECT (3)") };
+  string state_names[] = { string("OFF (0)"), string("Monitor Mode"), string("Command Mode") };
+  //set Text for Fri state and Fri quality
+  mpLabelStateData->setText(state_names[mpKukaIn.getState()]);
+  mpLabelQualData->setText(quality_names[mp.kukaIn.getState()]);
+  //set text for power status
+  if (mpKukaIn.isPowerOn())
+  {
+    mpLabelPowerData->setText("ON");
+  }
+  else
+  {
+    mpLabelPowerData->setText("OFF");
+  }
+  //set text for sample time
+  stringstream s;
+  s <<mpKukaIn.getSampleTime()<<"s";
+  mpLabelSampleTimeData->setText(s.to_s());
+}
