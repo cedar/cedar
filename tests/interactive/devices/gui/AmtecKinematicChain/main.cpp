e499cd1d82289e3f76435dd3c1a2a5a356de723b
Bjoern Weghenkel <bjoern.weghenkel@ini.ruhr-uni-bochum.de>
2011-01-27 15:31:44 +0100

    Commit before merge

--- tests/interactive/devices/gui/AmtecKinematicChain/main.cpp
+++ tests/interactive/devices/gui/AmtecKinematicChain/main.cpp
@@ -61,8 +61,6 @@ int main(int argc, char *argv[]) {
     KinematicChainPtr p_kinematic_chain(new AmtecKinematicChain("../../../tests/interactive/devices/gui/AmtecKinematicChain/test_arm.conf"));
     QApplication app(argc, argv);
     KinematicChainWidget widget(p_kinematic_chain);
-    //widget.simulateVelocity(true);
-    widget.simulateAcceleration(true);
     widget.show();
     return app.exec();
   }
