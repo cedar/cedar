08c517a7b79a75d0ed2ad5b8d3c72635a13b222c
Guido Knips <Guido Knips@localhost>
2011-03-22 11:32:25 +0100

    merge

---
+++ cedar/auxiliaries/LoopedThread.cpp
@@ -0,0 +1,365 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        LoopedThread.cpp
+
+    Maintainer:  Bjoern Weghenkel
+    Email:       bjoern.weghenkel@ini.rub.de
+    Date:        2010 12 02
+
+    Description: Implementation of the @em cedar::aux::LoopedThread class.
+
+    Credits:
+
+======================================================================================================================*/
+
+
+// LOCAL INCLUDES
+#include "auxiliaries/LoopedThread.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+//#include <QTime>
+
+
+using namespace std;
+using namespace boost::posix_time;
+
+//------------------------------------------------------------------------------
+// constructors and destructor
+//------------------------------------------------------------------------------
+cedar::aux::LoopedThread::LoopedThread(double stepSize, double idleTime)
+{
+  mStop  = false;
+  initStatistics();
+
+  mStepSize = microseconds(static_cast<unsigned int>(1000 * stepSize + 0.5));
+  mIdleTime = static_cast<unsigned int>(1000 * idleTime + 0.5);
+  mUseFixedStepSize = true;
+  mSimulatedTime = microseconds(0);
+}
+
+cedar::aux::LoopedThread::LoopedThread(const std::string& configFileName)
+  :cedar::aux::ConfigurationInterface(configFileName)
+{
+  mStop = false;
+  initStatistics();
+  readParamsFromConfigFile();
+}
+
+cedar::aux::LoopedThread::LoopedThread(const char* pConfigFileName)
+  :cedar::aux::ConfigurationInterface(pConfigFileName)
+{
+  mStop = false;
+  initStatistics();
+  readParamsFromConfigFile();
+}
+
+cedar::aux::LoopedThread::~LoopedThread()
+{
+}
+
+//------------------------------------------------------------------------------
+// methods
+//------------------------------------------------------------------------------
+
+void cedar::aux::LoopedThread::stop(unsigned int time, bool suppressWarning)
+{
+  if(isRunning())
+  {
+    mStop = true;
+    wait(time);
+
+    if(isRunning())
+    {
+      cout << "Warning: Thread is still running after call of stop()!" << endl;
+    }
+
+    if(suppressWarning == false && mMaxStepsTaken > 1.01 && mSimulatedTime.total_microseconds() == 0)
+    {
+      cout << "Warning: The system was not fast enough to stay to scheduled thread timing. ";
+      cout << "Consider using a larger step size." << endl;
+      cout << "Execution stats:" << endl;
+      cout << "  avg. time steps between execution: " << (double) mSumOfStepsTaken / (double) mNumberOfSteps << endl;
+      cout << "  max. time steps between execution: " << mMaxStepsTaken << endl;
+    }
+  }
+  return;
+}
+
+void cedar::aux::LoopedThread::run(void)
+{
+  // we do not want to change the step size while running
+  time_duration step_size = mStepSize;
+  bool use_fixed_step_size = mUseFixedStepSize;
+  initStatistics();
+
+  // which mode?
+  if(step_size.total_microseconds() == 0)
+  {
+    // fast running mode
+
+    mLastTimeStepStart = microsec_clock::universal_time();
+    mLastTimeStepEnd = microsec_clock::universal_time();
+    time_duration time_difference;
+
+    while(!mStop)
+    {
+      usleep(mIdleTime);
+      mLastTimeStepStart = mLastTimeStepEnd;
+      mLastTimeStepEnd = microsec_clock::universal_time();
+
+      if(mSimulatedTime.total_microseconds() == 0)
+      {
+        time_difference = mLastTimeStepEnd - mLastTimeStepStart;
+        step(time_difference.total_microseconds() * 0.001);
+      }
+      else
+      {
+        step(mSimulatedTime.total_microseconds() * 0.001);
+      }
+    }
+  }
+  else // if(step_size == 0)
+  {
+    // regular mode with time slots
+
+    // initialization
+    mLastTimeStepStart = microsec_clock::universal_time();
+    mLastTimeStepEnd = microsec_clock::universal_time();
+    ptime scheduled_wakeup = mLastTimeStepEnd + step_size;
+    srand(microsec_clock::universal_time().time_of_day().total_milliseconds());
+
+    // some auxiliary variables
+    time_duration sleep_duration = microseconds(0);
+    time_duration step_duration = microseconds(0);
+
+    while(!mStop)
+    {
+      // sleep until next wake up time
+      sleep_duration = scheduled_wakeup - microsec_clock::universal_time();
+      usleep(std::max<int>(0, sleep_duration.total_microseconds()));
+
+      // determine time since last run
+      mLastTimeStepStart = mLastTimeStepEnd;
+      mLastTimeStepEnd = scheduled_wakeup;
+      step_duration = mLastTimeStepEnd - mLastTimeStepStart;
+
+      // calculate number of time steps taken
+      double steps_taken = static_cast<double>(step_duration.total_microseconds()) / static_cast<double>(step_size.total_microseconds());
+      unsigned int full_steps_taken = static_cast<unsigned int>( steps_taken + 0.5 );
+
+      #ifdef DEBUG
+      // print warning if time steps have been skipped
+      if( steps_taken > 1.01 )
+        cerr << "WARNING: " << steps_taken << " time steps taken at once! "
+        << "Your system might be too slow for an execution interval of "
+        << step_size.total_microseconds() << " microseconds. Consider using a "
+        << "longer interval!" << endl;
+      #endif
+
+      // here we have to distinguish between the different working modes of the
+      // thread. either we want to wake up on the next fixed time step or as
+      // soon as possible if we are already late.
+      if(use_fixed_step_size)
+      {
+        // update statistics
+        updateStatistics(full_steps_taken);
+
+        // call step function
+        if(mSimulatedTime.total_microseconds() == 0)
+        {
+          step(full_steps_taken * step_size.total_microseconds() * 0.001);
+        }
+        else
+        {
+          step(mSimulatedTime.total_microseconds() * 0.001);
+        }
+
+        // schedule the next wake up
+        while(scheduled_wakeup < microsec_clock::universal_time())
+        {
+          scheduled_wakeup = scheduled_wakeup + step_size;
+        }
+
+      }
+      else // if(mUseFixedStepSize)
+      {
+        // update statistics
+        updateStatistics(steps_taken);
+
+        // call step function
+        if(mSimulatedTime.total_microseconds() == 0)
+        {
+          step(steps_taken * step_size.total_microseconds() * 0.001);
+        }
+        else
+        {
+          step(mSimulatedTime.total_microseconds() * 0.001);
+        }
+
+        // schedule the next wake up
+        scheduled_wakeup = max<ptime>(scheduled_wakeup + step_size, microsec_clock::universal_time());
+
+      } // if(mUseFixedStepSize)
+
+    } // while(!mStop)
+
+  } // if(step_size != 0)
+
+  mStop = false;
+  return;
+}
+
+void cedar::aux::LoopedThread::initStatistics()
+{
+  mNumberOfSteps = 0;
+  mSumOfStepsTaken = 0.0;
+  mMaxStepsTaken = 0.0;
+}
+
+void cedar::aux::LoopedThread::updateStatistics(double stepsTaken)
+{
+
+  double old_sum = mSumOfStepsTaken;
+
+  mNumberOfSteps += 1.0;
+  mSumOfStepsTaken += stepsTaken;
+  if(stepsTaken > mMaxStepsTaken)
+  {
+    mMaxStepsTaken = stepsTaken;
+  }
+
+  if(old_sum > mSumOfStepsTaken)
+  {
+    cerr << "Warning: Value overflow in thread statistics. Statistics will be reseted." << endl;
+    initStatistics();
+  }
+
+  return;
+}
+
+void cedar::aux::LoopedThread::singleStep()
+{
+  if(!isRunning())
+  {
+    if(mSimulatedTime.total_microseconds() == 0)
+    {
+      step(mStepSize.total_microseconds());
+    }
+    else
+    {
+      step(mSimulatedTime.total_microseconds());
+    }
+  }
+}
+
+bool cedar::aux::LoopedThread::stopRequested()
+{
+  if(isRunning() && mStop == true)
+  {
+    return true;
+  }
+
+  return false;
+}
+
+void cedar::aux::LoopedThread::readParamsFromConfigFile()
+{
+  double step_size = 100.0;
+  double idle_time = 0.001;
+  double simulated_time = 0.0;
+
+  // mStepSize
+
+  if(addParameter(&step_size, "threadStepSize", 100.0) != CONFIG_SUCCESS)
+  {
+    cout << "LoopedThread: Error reading parameter 'stepSize' from config file!" << endl;
+  }
+
+  // mIdleTime
+
+  if(addParameter(&idle_time, "threadIdleTime", 0.001) != CONFIG_SUCCESS)
+  {
+    cout << "LoopedThread: Error reading parameter 'idleTime' from config file!" << endl;
+  }
+
+  // mUseFixedStepSize
+
+  if(addParameter(&mUseFixedStepSize, "threadUseFixedStepSize", true) != CONFIG_SUCCESS)
+  {
+    cout << "LoopedThread: Error reading parameter 'useFixedStepSize' from config file!" << endl;
+  }
+
+  // mSimulatedTime
+
+  if(addParameter(&simulated_time, "threadSimulatedTime", 0.0) != CONFIG_SUCCESS)
+  {
+    cout << "LoopedThread: Error reading parameter 'simulatedTime' from config file!" << endl;
+  }
+
+  readOrDefaultConfiguration();
+
+  mStepSize = microseconds(static_cast<unsigned int>(1000 * step_size + 0.5));
+  mIdleTime = static_cast<unsigned int>(1000 * idle_time + 0.5);
+  mSimulatedTime = microseconds(static_cast<unsigned>(1000 * simulated_time + 0.5));
+
+  return;
+}
+
+void cedar::aux::LoopedThread::setStepSize(double stepSize)
+{
+  mStepSize = boost::posix_time::microseconds(static_cast<unsigned int>(1000 * stepSize + 0.5));
+}
+
+void cedar::aux::LoopedThread::setIdleTime(double idleTime)
+{
+  mIdleTime = static_cast<unsigned int>(1000 * idleTime + 0.5);
+}
+
+void cedar::aux::LoopedThread::useFixedStepSize(bool useFixedStepSize)
+{
+  mUseFixedStepSize = useFixedStepSize;
+}
+
+void cedar::aux::LoopedThread::setSimulatedTime(double simulatedTime)
+{
+  mSimulatedTime = boost::posix_time::microseconds(static_cast<unsigned>(1000 * simulatedTime + 0.5));
+}
+
+boost::posix_time::ptime cedar::aux::LoopedThread::getLastTimeStepStart() const
+{
+  return mLastTimeStepStart;
+}
+
+boost::posix_time::ptime cedar::aux::LoopedThread::getLastTimeStepEnd() const
+{
+  return mLastTimeStepEnd;
+}
+
+boost::posix_time::time_duration cedar::aux::LoopedThread::getLastTimeStepDuration() const
+{
+  return mLastTimeStepStart - mLastTimeStepEnd;
+}
---
+++ cedar/auxiliaries/Object.h
@@ -0,0 +1,169 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        Object.h
+
+    Maintainer:  Hendrik Reimann
+    Email:       hendrik.reimann@ini.rub.de
+    Date:        2010 12 04
+
+    Description: header of cedar::aux::Object class, providing geometry of a rigid object
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_AUX_OBJECT_H
+#define CEDAR_AUX_OBJECT_H
+
+// LOCAL INCLUDES
+#include "namespace.h"
+#include "Base.h"
+#include "ConfigurationInterface.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+#include <QObject>
+#include <cv.h>
+
+
+/*!@brief Provides the geometry of a rigid object
+ *
+ * rigid between world coordinate frame and object coordinate frame
+ */
+class cedar::aux::Object : public QObject, public cedar::aux::ConfigurationInterface
+{
+private:
+
+  Q_OBJECT;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief The standard constructor.
+  Object();
+  //!@brief constructor for setting saved to a configuration file
+  Object(const std::string& configFileName);
+
+  //!@brief Destructor
+  virtual ~Object();
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief returns position of the object in homogeneous coordinates
+  cv::Mat getPosition();
+
+  //!@brief returns x-position of the object frame origin in world frame
+  double getPositionX();
+
+  //!@brief returns y-position of the object frame origin in world frame
+  double getPositionY();
+
+  //!@brief returns z-position of the object frame origin in world frame
+  double getPositionZ();
+
+  //!@brief returns object frame orientation as a unit quaternion
+  double getOrientationQuaternion(unsigned int component);
+
+  //!@brief returns object frame orientation as a unit quaternion
+  cv::Mat getOrientationQuaternion();
+
+  //!@brief returns the \f$4 \times 4\f$ rigid transformation matrix of the object frame relative to the world frame
+  cv::Mat getTransformation();
+
+public slots:
+  /*!@brief set the position of the object frame origin in the world frame
+   * @param x    coordinates of
+   * @param y    value for green channel in RGB color
+   * @param z    value for blue channel in RGB color
+   */
+  void setPosition(const double x, const double y, const double z);
+
+  /*!@brief set the position of the object frame origin in the world frame
+   * @param position    new position in homogeneous coordinates
+   */
+  void setPosition(const cv::Mat& position);
+
+  /*!@brief set the orientation of the object frame, given as a unit quaternion
+   * @param quaternion    new unit quaternion for the rotation, 4x1 matrix with norm 1
+   */
+  void setOrientationQuaternion(const cv::Mat quaternion);
+
+  /*!@brief rotates the object around one of the main axes of the object frame
+   * @param axis    index of the axis to rotate around, between 0 and 2
+   * @param angle    value of angle that the object is rotated by
+   */
+  void rotate(unsigned int axis, double angle);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  //!@brief recalculates the rigid transformation to the object frame from position and orientation
+  void updateTransformation();
+  //!@brief initialization function
+  void init();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet (hopefully never!)
+
+protected:
+  //!@brief rigid transformation to the object
+  cv::Mat mTransformation;
+
+private:
+  cv::Mat mPosition; // position of the point obstacle, in homogeneous coordinates
+  cv::Mat mOrientationQuaternion; // quaternion representing the orientation
+  cv::Mat mTransformationTranspose; // transpose equivalent to representation compatible with OpenGl
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet (hopefully never!)
+protected:
+  // none yet
+
+private:
+  //! position
+  std::vector<double> _mPosition;
+  //! orientation
+  std::vector<double> _mOrientation;
+
+}; // class cedar::aux::Object
+
+#endif // CEDAR_AUX_OBJECT_H
+
