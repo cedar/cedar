894f6220d10dd0cad19b208e44d9f96f18572c01
lomplbx <lomplbx@localhost>
2011-05-24 17:43:24 +0200

    - Updated some naming of methods and removed arguments from the triggers. - Rewrote the unit test to be more flexible.

--- tests/unit/auxiliaries/computation/SequenceTest/SequenceTest.cpp
+++ tests/unit/auxiliaries/computation/SequenceTest/SequenceTest.cpp
@@ -50,6 +50,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <string>
 #include <QReadWriteLock>
 
 class ComputableTest : public cedar::aux::comp::Computable
@@ -77,6 +78,91 @@ class ComputableTest : public cedar::aux::comp::Computable
 
 typedef boost::shared_ptr<ComputableTest> ComputableTestPtr;
 
+// =====================================================================================================================
+
+cedar::aux::LogFile log_file ("SequenceTest.log");
+
+/*
+ * Tests the correct generation of a sequence.
+ *
+ * The sequence is specified as a sequence string: for example, in the string
+ *    111232311
+ * each number specifies the number of steps to execute in parallel before going to the next stage (i.e., the next
+ * number).
+ */
+void testSequence(const std::string& sequenceString, unsigned int& errors)
+{
+  using cedar::aux::comp::Trigger;
+  using cedar::aux::comp::TriggerPtr;
+
+  QReadWriteLock lock;
+  std::vector<unsigned int> sequence_buffer;
+
+  log_file << "Creating computation chain for sequence string \"" << sequenceString << "\"." << std::endl;
+
+  TriggerPtr sequence_trigger(new Trigger());
+
+  TriggerPtr prev_trigger = sequence_trigger;
+  std::string desired_sequence = "";
+
+  unsigned int num_steps = 0;
+  for (size_t i = 0; i < sequenceString.size(); ++i)
+  {
+    std::string chr = "";
+    chr += sequenceString.at(i);
+    int no_steps = atoi(chr.c_str());
+    if (no_steps <= 0)
+    {
+      log_file << "Malformed sequence string \"" << sequenceString << "\" at position " << i <<
+          ": Cannot convert to number, or number is less than 1." << std::endl;
+      return;
+    }
+    std::vector<ComputableTestPtr> steps;
+
+    for (int no = 0; no < no_steps; ++no)
+    {
+      ComputableTestPtr step (new ComputableTest(i, sequence_buffer, lock));
+      steps.push_back(step);
+      prev_trigger->addListener(step);
+
+      std::stringstream strstr;
+      strstr << i;
+      desired_sequence += strstr.str();
+      ++num_steps;
+    }
+    prev_trigger = TriggerPtr(new Trigger());
+  }
+
+  log_file << "Triggering sequence " << sequenceString << "." << std::endl;
+  sequence_trigger->trigger();
+
+  log_file << "Checking sequence " << sequenceString << "." << std::endl;
+  unsigned int prev_stage_no = sequence_buffer.at(0);
+  std::string sequence = "";
+  for (size_t i = 0; i < sequence_buffer.size(); ++i)
+  {
+    unsigned int stage_no = sequence_buffer.at(i);
+    // an error occurs if the stage number is more than one bigger than the last one
+    if (stage_no != prev_stage_no + 1 && stage_no != prev_stage_no)
+    {
+      log_file << "Wrong order detected in sequence " << sequenceString << "." << std::endl;
+      ++errors;
+    }
+    prev_stage_no = stage_no;
+
+    std::stringstream strstr;
+    strstr << stage_no;
+    sequence += strstr.str();
+  }
+
+  if (desired_sequence != sequence)
+  {
+    log_file << "Wrong sequence generated for " << sequenceString << "." << std::endl;
+    log_file << "Generated sequence is: " << sequence << "." << std::endl;
+    log_file << "Desired sequence is: " << desired_sequence << "." << std::endl;
+    ++errors;
+  }
+}
 
 
 int main(int argc, char** argv)
@@ -87,22 +173,19 @@ int main(int argc, char** argv)
 
   unsigned int errors = 0;
 
-  LogFile log_file("SequenceTest.log");
   log_file.addTimeStamp();
   log_file << std::endl;
 
-  QReadWriteLock lock;
-  std::vector<unsigned int> sequence_buffer;
+  testSequence("112311", errors);
 
-  log_file << "Creating computation chain." << std::endl;
+/*  log_file << "Creating computation chain." << std::endl;
   std::vector<ComputableTestPtr> steps;
   for (size_t i = 0; i < 4; ++i)
   {
     steps.push_back(ComputableTestPtr(new ComputableTest(i, sequence_buffer, lock)));
-  }
+  }*/
 
-  TriggerPtr start_trigger(new Trigger());
-
+/*
   start_trigger->addListener(steps[0]);
 
   steps[0]->getFinishedTrigger().addListener(steps[1]);
@@ -122,6 +205,7 @@ int main(int argc, char** argv)
     sequence += strstr.str();
   }
   log_file << "Generated sequence:" << sequence << std::endl;
+  */
 
   log_file << "Done. There were " << errors << " errors." << std::endl;
 
