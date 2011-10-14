6ff1b3ed6b33dcce00474b4703da1e77a35cd542
Stephan Zibner <37666884+raul-ini@users.noreply.github.com>
2011-10-14 12:00:34 +0200

    renamed tools to executables, made all adaptations in cmake files

---
+++ cedar/auxiliaries/kernel/Gauss.cpp
@@ -0,0 +1,294 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        Gauss.cpp
+
+    Maintainer:  Stephan Zibner
+    Email:       stephan.zibner@ini.rub.de
+    Date:        2011 07 07
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+// LOCAL INCLUDES
+#include "auxiliaries/kernel/Gauss.h"
+#include "auxiliaries/NumericParameter.h"
+#include "auxiliaries/DataT.h"
+#include "auxiliaries/math/functions.h"
+#include "auxiliaries/exceptions.h"
+#include "auxiliaries/NumericVectorParameter.h"
+#include "auxiliaries/assert.h"
+#include "auxiliaries/exceptions/IndexOutOfRangeException.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+#include <iostream>
+#include <limits.h>
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+cedar::aux::kernel::Gauss::Gauss()
+:
+cedar::aux::kernel::Separable(),
+_mAmplitude(new cedar::aux::DoubleParameter(this, "amplitude", -10000.0, 10000.0)),
+_mSigmas(new cedar::aux::DoubleVectorParameter(this, "sigmas", 2, 3.0, 0.0, 10000)),
+_mShifts(new cedar::aux::DoubleVectorParameter(this, "shifts", 2, 3.0, 0.0, 10000)),
+_mLimit(new cedar::aux::DoubleParameter(this, "limit", 0.01, 1000.0))
+{
+  this->onInit();
+}
+
+cedar::aux::kernel::Gauss::Gauss(
+                                  double amplitude,
+                                  std::vector<double> sigmas,
+                                  std::vector<double> shifts,
+                                  double limit,
+                                  unsigned int dimensionality,
+                                  const std::string& kernelFile
+                                )
+:
+cedar::aux::kernel::Separable(dimensionality, kernelFile),
+_mAmplitude(new cedar::aux::DoubleParameter(this, "amplitude", amplitude, -10000.0, 10000.0)),
+_mSigmas(new cedar::aux::DoubleVectorParameter(this, "sigmas", sigmas, 0.0, 10000)),
+_mShifts(new cedar::aux::DoubleVectorParameter(this, "shifts", shifts, 0.0, 10000)),
+_mLimit(new cedar::aux::DoubleParameter(this, "limit", limit, 0.01, 1000.0))
+{
+  this->mCenters.resize(dimensionality);
+  this->mKernelParts.resize(dimensionality);
+  this->mSizes.resize(dimensionality);
+  this->onInit();
+}
+
+cedar::aux::kernel::Gauss::~Gauss()
+{
+#ifdef DEBUG
+  std::cout << "> freeing data (Gauss)" << std::endl;
+#endif
+}
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+void cedar::aux::kernel::Gauss::onInit()
+{
+  calculate();
+  QObject::connect(_mAmplitude.get(), SIGNAL(valueChanged()), this, SLOT(updateKernel()));
+  QObject::connect(_mLimit.get(), SIGNAL(valueChanged()), this, SLOT(updateKernel()));
+  QObject::connect(_mSigmas.get(), SIGNAL(valueChanged()), this, SLOT(updateKernel()));
+  QObject::connect(_mShifts.get(), SIGNAL(valueChanged()), this, SLOT(updateKernel()));
+  QObject::connect(_mDimensionality.get(), SIGNAL(valueChanged()), this, SLOT(updateDimensionality()));
+}
+
+void cedar::aux::kernel::Gauss::calculate()
+{
+  mpReadWriteLockOutput->lockForWrite();
+  const unsigned int& dimensionality = _mDimensionality->get();
+  const double& amplitude = _mAmplitude->get();
+  // sanity check
+  CEDAR_DEBUG_ASSERT(dimensionality == _mSigmas->get().size());
+  CEDAR_DEBUG_ASSERT(dimensionality == _mShifts->get().size());
+  try
+  {
+    mKernelParts.resize(dimensionality);
+    mCenters.resize(dimensionality);
+    mSizes.resize(dimensionality);
+    // calculate the kernel parts for every dimension
+    for (unsigned int dim = 0; dim < dimensionality; dim++)
+    {
+      // estimate width
+      if (_mSigmas->get().at(dim) != 0)
+      {
+        mSizes.at(dim) = getWidth(dim);
+      }
+      else
+      {
+        mSizes.at(dim) = 1;
+      }
+      mCenters.at(dim) = static_cast<int>(mSizes.at(dim) / 2) + _mShifts->get().at(dim);
+      mKernelParts.at(dim) = cv::Mat::zeros(mSizes.at(dim), 1, CV_32FC1);
+
+      // calculate kernel part
+      if (_mSigmas->get().at(dim) != 0)
+      {
+        for (unsigned int j = 0; j < mSizes.at(dim); j++)
+        {
+          //!\todo move filling up of matrix to some tool function
+          mKernelParts.at(dim).at<float>(j, 0)
+              = cedar::aux::math::gauss(static_cast<int>(j) - mCenters.at(dim), _mSigmas->get().at(dim));
+        }
+      }
+      else // discrete case
+      {
+        mKernelParts.at(dim).at<float>(0, 0) = 1;
+      }
+      // normalize
+      mKernelParts.at(dim) = mKernelParts.at(dim) * (1. / cv::sum(mKernelParts.at(dim)).val[0]);
+      if (dim == 0)
+      {
+        mKernelParts.at(dim) = amplitude * mKernelParts.at(dim);
+      }
+    }
+    // assemble the kernel
+    std::vector<int> sizes(static_cast<size_t>(dimensionality));
+    for (size_t i = 0; i < mSizes.size(); i++)
+    {
+      sizes[i] = mSizes.at(i);
+    }
+    if (dimensionality == 1)
+    {
+      mKernel->getData<cv::Mat>() = cv::Mat(sizes[0], 1, CV_32F);
+    }
+    else
+    {
+      mKernel->getData<cv::Mat>() = cv::Mat(static_cast<int>(dimensionality), &sizes.at(0), CV_32F);
+    }
+    cv::Mat& kernel = mKernel->getData<cv::Mat>();
+    // now fill up the big kernel matrix
+    std::vector<int> position(static_cast<size_t>(dimensionality));
+    unsigned int max_index = 1;
+    double max_index_d = 1.0;
+    for (unsigned int dim = 0; dim < dimensionality; dim++)
+    {
+      position[dim] = 0;
+      max_index_d *= mSizes.at(dim);
+      if (max_index_d > std::numeric_limits<unsigned int>::max()/100.0)
+      {
+        CEDAR_THROW(cedar::aux::RangeException, "cannot handle kernels of this size");
+      }
+    }
+    max_index = static_cast<unsigned int>(max_index_d);
+    for (unsigned int i = 0; i < max_index; i++)
+    {
+      float value = 1.0;
+      for (unsigned int dim = 0; dim < dimensionality; dim++)
+      {
+        value *= mKernelParts.at(dim).at<float>(position[dim], 0);
+      }
+      if (dimensionality == 1)
+      {
+        kernel.at<float>(position[0], 1) = value;
+      }
+      else
+      {
+        kernel.at<float>(&position.at(0)) = value;
+      }
+      // increment index
+      position[0]++;
+      for (unsigned int dim = 0; dim < dimensionality; dim++)
+      {
+        if (position[dim] >= static_cast<int>(mSizes.at(dim)))
+        {
+          position[dim] = 0;
+          if (dim+1 < dimensionality)
+          {
+            position[dim+1]++;
+          }
+        }
+      }
+    }
+  }
+  catch(std::out_of_range& error)
+  {
+    std::cerr << "> Error (kernel::Gauss) :" << error.what() << " in calculate().\n"
+        << "  Check your configuration files." << std::endl;
+    CEDAR_THROW(
+                 cedar::aux::exc::IndexOutOfRangeException,
+                 "kernel::Gauss has encountered inconsistent vector sizes, check your configuration file"
+               );
+  }
+  mpReadWriteLockOutput->unlock();
+}
+
+void cedar::aux::kernel::Gauss::setSigma(unsigned int dimension, double sigma)
+{
+  try
+  {
+    _mSigmas->set(dimension, sigma);
+  }
+  catch(std::exception& e)
+  {
+    std::cout << "Error in " << this->_mName->get()
+              << " (cedar::aux::kernel::Gauss::setSigma): vector out of bounds " << std::endl;
+  }
+}
+
+double cedar::aux::kernel::Gauss::getSigma(unsigned int dimension) const
+{
+  return _mSigmas->get().at(dimension);
+}
+
+void cedar::aux::kernel::Gauss::setShift(unsigned int dimension, double shift)
+{
+  try
+  {
+    _mShifts->set(dimension, shift);
+  }
+  catch(std::exception& e)
+  {
+    std::cout << "Error in " << this->_mName->get()
+              << " (cedar::aux::kernel::Gauss::setShift): vector out of bounds " << std::endl;
+  }
+}
+
+double cedar::aux::kernel::Gauss::getShift(unsigned int dimension) const
+{
+  return _mShifts->get().at(dimension);
+}
+
+void cedar::aux::kernel::Gauss::setAmplitude(double amplitude)
+{
+  _mAmplitude->set(amplitude);
+}
+
+double cedar::aux::kernel::Gauss::getAmplitude() const
+{
+  return _mAmplitude->get();
+}
+
+unsigned int cedar::aux::kernel::Gauss::getWidth(unsigned int dim) const
+{
+  unsigned int tmp;
+  /* size of kernel is determined by limit * sigma
+   */
+  tmp = static_cast<unsigned int>(ceil(_mLimit->get() * _mSigmas->get().at(dim)));
+  // check if kernel size is even and if so, make it odd
+  if (tmp % 2 == 0)
+  {
+    tmp++;
+  }
+  return tmp;
+}
+
+void cedar::aux::kernel::Gauss::updateDimensionality()
+{
+  int new_dimensionality = static_cast<int>(_mDimensionality->get());
+  _mSigmas->resize(new_dimensionality, _mSigmas->getDefaultValue());
+  _mShifts->resize(new_dimensionality, _mShifts->getDefaultValue());
+  this->updateKernel();
+}
---
+++ cedar/processing/Trigger.cpp
@@ -0,0 +1,283 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        Trigger.cpp
+
+
+    Maintainer:  Oliver Lomp,
+                 Mathis Richter,
+                 Stephan Zibner
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de,
+                 mathis.richter@ini.ruhr-uni-bochum.de,
+                 stephan.zibner@ini.ruhr-uni-bochum.de
+    Date:        2011 05 23
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+// LOCAL INCLUDES
+#include "processing/Trigger.h"
+#include "processing/Step.h"
+#include "processing/Manager.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+#include <algorithm>
+
+// MACROS
+//#define DEBUG_TRIGGERING
+
+#ifdef DEBUG_TRIGGERING
+#  include "auxiliaries/System.h"
+#endif // DEBUG_TRIGGERING
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+cedar::proc::Trigger::Trigger(const std::string& name)
+:
+mRegisteredAt(NULL)
+{
+  this->setName(name);
+}
+
+cedar::proc::Trigger::~Trigger()
+{
+#ifdef DEBUG
+  std::cout << "> freeing data (Trigger)" << std::endl;
+#endif
+  this->mListeners.clear();
+  this->mTriggers.clear();
+}
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+void cedar::proc::Trigger::setRegistry(cedar::proc::TriggerRegistry* pRegistry)
+{
+  this->mRegisteredAt = pRegistry;
+}
+
+void cedar::proc::Trigger::trigger(cedar::proc::ArgumentsPtr arguments)
+{
+  for (size_t i = 0; i < this->mListeners.size(); ++i)
+  {
+    // If the arguments can be set, trigger the step.
+    //!@todo For dynamics, this can mean that some step times are discarded rather than accumulated.
+    //!@todo cedar::proc::Step::setNextArguments should take a const pointer.
+    if (this->mListeners.at(i)->setNextArguments(arguments))
+    {
+#ifdef DEBUG_TRIGGERING
+      cedar::aux::System::mCOutLock.lockForWrite();
+      std::cout << "Trigger " << this->getName() << " triggers " << this->mListeners.at(i)->getName() << std::endl;
+      cedar::aux::System::mCOutLock.unlock();
+#endif // DEBUG_TRIGGERING
+      this->mListeners.at(i)->onTrigger();
+    }
+    // Otherwise, the step is skipped this time.
+#ifdef DEBUG_TRIGGERING
+    else
+    {
+      cedar::aux::System::mCOutLock.lockForWrite();
+      std::cout << "Trigger " << this->getName() << " failed to trigger " << this->mListeners.at(i)->getName() << std::endl;
+      cedar::aux::System::mCOutLock.unlock();
+    }
+#endif // DEBUG_TRIGGERING
+  }
+  for (size_t i = 0; i < this->mTriggers.size(); ++i)
+  {
+    this->mTriggers.at(i)->onTrigger(this);
+  }
+}
+
+void cedar::proc::Trigger::onTrigger(Trigger*)
+{
+}
+
+void cedar::proc::Trigger::addListener(cedar::proc::StepPtr step)
+{
+  std::vector<cedar::proc::StepPtr>::iterator iter;
+  iter = this->find(step);
+  if (iter == this->mListeners.end())
+  {
+    this->mListeners.push_back(step);
+  }
+}
+
+bool cedar::proc::Trigger::isListener(cedar::proc::StepPtr step)
+{
+  return this->find(step) != this->mListeners.end();
+}
+
+bool cedar::proc::Trigger::isListener(cedar::proc::TriggerPtr trigger)
+{
+  return this->find(trigger) != this->mTriggers.end();
+}
+
+void cedar::proc::Trigger::addTrigger(cedar::proc::TriggerPtr trigger)
+{
+  std::vector<cedar::proc::TriggerPtr>::iterator iter;
+  iter = this->find(trigger);
+  if (iter == this->mTriggers.end())
+  {
+    this->mTriggers.push_back(trigger);
+    trigger->notifyConnected(this);
+  }
+}
+
+void cedar::proc::Trigger::removeListener(cedar::proc::StepPtr step)
+{
+  std::vector<cedar::proc::StepPtr>::iterator iter;
+  iter = this->find(step);
+  if (iter != this->mListeners.end())
+  {
+    this->mListeners.erase(iter);
+  }
+}
+
+void cedar::proc::Trigger::removeTrigger(cedar::proc::TriggerPtr trigger)
+{
+  std::vector<cedar::proc::TriggerPtr>::iterator iter;
+  iter = this->find(trigger);
+  if (iter != this->mTriggers.end())
+  {
+    this->mTriggers.erase(iter);
+    trigger->notifyDisconnected(this);
+  }
+}
+
+void cedar::proc::Trigger::notifyConnected(cedar::proc::Trigger* /* trigger */)
+{
+}
+
+void cedar::proc::Trigger::notifyDisconnected(cedar::proc::Trigger* /* trigger */)
+{
+}
+
+std::vector<cedar::proc::StepPtr>::iterator
+  cedar::proc::Trigger::find(cedar::proc::StepPtr step)
+{
+  return std::find(this->mListeners.begin(), this->mListeners.end(), step);
+}
+
+std::vector<cedar::proc::TriggerPtr>::iterator cedar::proc::Trigger::find(cedar::proc::TriggerPtr step)
+{
+  return std::find(this->mTriggers.begin(), this->mTriggers.end(), step);
+}
+
+const std::vector<cedar::proc::StepPtr>& cedar::proc::Trigger::getListeners() const
+{
+  return this->mListeners;
+}
+
+const std::vector<cedar::proc::TriggerPtr>& cedar::proc::Trigger::getTriggerListeners() const
+{
+  return this->mTriggers;
+}
+
+void cedar::proc::Trigger::saveConfiguration(cedar::aux::ConfigurationNode& node)
+{
+  this->cedar::aux::Configurable::saveConfiguration(node);
+
+  cedar::aux::ConfigurationNode listeners;
+  for (size_t i = 0; i < this->mListeners.size(); ++i)
+  {
+    cedar::proc::StepPtr& step = this->mListeners.at(i);
+    cedar::aux::ConfigurationNode listener(step->getName());
+    listeners.push_back(cedar::aux::ConfigurationNode::value_type("", listener));
+  }
+  if (!listeners.empty())
+    node.add_child("listeners", listeners);
+
+  cedar::aux::ConfigurationNode trigger_listeners;
+  for (size_t i = 0; i < this->mTriggers.size(); ++i)
+  {
+    cedar::proc::TriggerPtr& trigger = this->mTriggers.at(i);
+    cedar::aux::ConfigurationNode trigger_listener(trigger->getName());
+    trigger_listeners.push_back(cedar::aux::ConfigurationNode::value_type("", trigger_listener));
+  }
+  if (!trigger_listeners.empty())
+    node.add_child("triggerListeners", trigger_listeners);
+}
+
+void cedar::proc::Trigger::readConfiguration(const cedar::aux::ConfigurationNode& node)
+{
+  this->cedar::aux::Configurable::readConfiguration(node);
+  // listeners
+  try
+  {
+    const cedar::aux::ConfigurationNode& listeners = node.get_child("listeners");
+
+    for (cedar::aux::ConfigurationNode::const_iterator iter = listeners.begin();
+        iter != listeners.end();
+        ++iter)
+    {
+      std::string listener_name = iter->second.data();
+
+#ifdef DEBUG_FILE_READING
+  std::cout << "Adding listener " << listener_name << std::endl;
+#endif // DEBUG_FILE_READING
+
+      cedar::proc::StepPtr step = cedar::proc::Manager::getInstance().steps().get(listener_name);
+      this->addListener(step);
+    }
+  }
+  catch (const boost::property_tree::ptree_bad_path&)
+  {
+    // no listeners declared -- this is ok.
+  }
+
+  // trigger listeners
+  try
+  {
+    const cedar::aux::ConfigurationNode& trigger_listeners = node.get_child("triggerListeners");
+
+    for (cedar::aux::ConfigurationNode::const_iterator iter = trigger_listeners.begin();
+        iter != trigger_listeners.end();
+        ++iter)
+    {
+      std::string listener_name = iter->second.data();
+
+#ifdef DEBUG_FILE_READING
+  std::cout << "Adding trigger listener " << listener_name << std::endl;
+#endif // DEBUG_FILE_READING
+
+      cedar::proc::TriggerPtr trigger = cedar::proc::Manager::getInstance().triggers().get(listener_name);
+      this->addTrigger(trigger);
+    }
+  }
+  catch (const boost::property_tree::ptree_bad_path&)
+  {
+    // no trigger listeners declared -- this is ok.
+  }
+}
+
