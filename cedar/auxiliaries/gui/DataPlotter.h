a10fe4a5a2a495f1a54a9d4f5eeb0237a5bfe27e
Stephan Zibner <37666884+raul-ini@users.noreply.github.com>
2011-11-10 11:28:05 +0100

    fixed issue #69 add 0D field in testing - also dadapted the MatViewer to allow for display of 0D data (with HistoryPlot0D)

---
+++ cedar/auxiliaries/gui/DataPlotter.h
@@ -0,0 +1,128 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        DataPlotter.h
+
+    Maintainer:  Oliver Lomp,
+                 Mathis Richter,
+                 Stephan Zibner
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de,
+                 mathis.richter@ini.ruhr-uni-bochum.de,
+                 stephan.zibner@ini.ruhr-uni-bochum.de
+    Date:        2011 07 14
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_PROC_GUI_DATA_PLOTTER_H
+#define CEDAR_PROC_GUI_DATA_PLOTTER_H
+
+// LOCAL INCLUDES
+#include "cedar/auxiliaries/gui/namespace.h"
+#include "cedar/auxiliaries/TypeBasedFactory.h"
+#include "cedar/auxiliaries/Data.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+#include <QDockWidget>
+
+
+/*!@brief Abstract description of the class.
+ *
+ * More detailed description of the class.
+ */
+class cedar::aux::gui::DataPlotter : public QDockWidget
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // macros
+  //--------------------------------------------------------------------------------------------------------------------
+  Q_OBJECT
+
+public:
+  typedef cedar::aux::TypeBasedFactory<cedar::aux::Data, QWidget> WidgetFactory;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief The standard constructor.
+  DataPlotter(const std::string& title, QWidget *pParent = NULL);
+
+  //!@brief Destructor
+  ~DataPlotter();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  void plot(cedar::aux::DataPtr data);
+
+  static WidgetFactory& getWidgetFactory();
+
+public slots:
+  void dataChanged();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet (hopefully never!)
+protected:
+  // none yet
+private:
+  cedar::aux::DataPtr mData;
+
+  static WidgetFactory mTypePlotters;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet (hopefully never!)
+protected:
+  // none yet
+
+private:
+  // none yet
+
+}; // class cedar::aux::gui::DataPlotter
+
+#endif // CEDAR_PROC_GUI_DATA_PLOTTER_H
+
---
+++ tests/unit/auxiliaries/gl/Sphere/main.cpp
@@ -0,0 +1,80 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+ ----- Institute:   Ruhr-Universitaet Bochum
+                    Institut fuer Neuroinformatik
+
+ ----- File:        main.cpp
+
+ ----- Author:      Hendrik Reimann
+ ----- Email:       hendrik.reimann@ini.rub.de
+ ----- Date:        2010 11 27
+
+ ----- Description: Tests cedar::aux::gl::Sphere class
+
+ ----- Credits:
+ ---------------------------------------------------------------------------------------------------------------------*/
+
+// LOCAL INCLUDES
+
+// PROJECT INCLUDES
+#include "cedar/auxiliaries/gl/Sphere.h"
+#include "cedar/auxiliaries/LogFile.h"
+
+// SYSTEM INCLUDES
+#include <string>
+
+using namespace cedar::aux;
+using namespace cedar::aux::gl;
+using namespace std;
+
+int main()
+{
+  LogFile log_file("UnitTestSphere.log");
+  log_file.addTimeStamp();
+  log_file << std::endl;
+  // the number of errors encountered in this test
+  int errors = 0;
+
+  // test constructors
+  cedar::aux::ObjectPtr p_object(new cedar::aux::Object());
+  Sphere testSphere(p_object);
+  Sphere sphere(p_object, 1);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // radius
+  //--------------------------------------------------------------------------------------------------------------------
+  log_file << "test: radius" << std::endl;
+  sphere.setRadius(10.1);
+  if (sphere.radius() != 10.1)
+  {
+    errors++;
+    log_file << "ERROR with setRadius() or radius()" << std::endl;
+  }
+
+
+  log_file << "test finished, there were " << errors << " errors" << std::endl;
+  if (errors > 255)
+  {
+    errors = 255;
+  }
+  return errors;
+}
