f5c2c2456ea9c9c64a271407370369d3ed8e6a42
ghartinger <ghartinger@localhost>
2011-11-29 17:32:17 +0100

    work in progess

---
+++ cedar/devices/sensors/visual/CameraProperty.h
@@ -0,0 +1,135 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        CameraProperty.h
+
+    Maintainer:  Georg Hartinger
+    Email:       georg.hartinger@ini.rub.de
+    Date:        2011 08 01
+
+    Description:  Header for CameraProperty enum-type class
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_CEDAR_DEV_SENSORS_VISUAL_CAMERA_PROPERTY_H
+#define CEDAR_CEDAR_DEV_SENSORS_VISUAL_CAMERA_PROPERTY_H
+
+// LOCAL INCLUDES
+#include "cedar/auxiliaries/EnumType.h"
+//#include "opencv2/highgui/highui_c.h"
+#include "../../../../../OpenCV-2.3.1/modules/highgui/include/opencv2/highgui/highgui_c.h"
+#include "namespace.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+
+
+/*!@brief Enum class for camera properties direct mapped from opencv2/highgui/highui_c.h
+ * (also in dc1394/control.h)
+ *
+ * Use this type for the CameraGrabber::setCameraProperty() and getCameraProperty() method
+ */
+class cedar::dev::sensors::visual::CameraProperty
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // typedefs
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  typedef cedar::aux::EnumId Id;
+public:
+  typedef boost::shared_ptr<cedar::aux::EnumBase> TypePtr;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief The standard constructor.
+
+  //!@brief Destructor
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  static void construct();
+
+  static const cedar::aux::EnumBase& type();
+  static const cedar::dev::sensors::visual::CameraProperty::TypePtr& typePtr();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  static cedar::aux::EnumType<cedar::dev::sensors::visual::CameraProperty> mType;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  static const Id PROP_BRIGHTNESS    = CV_CAP_PROP_BRIGHTNESS; // = 10
+  static const Id PROP_SATURATION    = CV_CAP_PROP_SATURATION; // = 12;
+  static const Id PROP_HUE           = CV_CAP_PROP_HUE; //13;
+  static const Id PROP_GAIN          = CV_CAP_PROP_GAIN; //14;
+  static const Id PROP_EXPOSURE      = CV_CAP_PROP_EXPOSURE; //15;
+  static const Id PROP_WHITE_BALANCE_BLUE_U = CV_CAP_PROP_WHITE_BALANCE_BLUE_U; //17;
+  static const Id PROP_SHARPNESS     = CV_CAP_PROP_SHARPNESS; //20;
+  static const Id PROP_AUTO_EXPOSURE = CV_CAP_PROP_AUTO_EXPOSURE; //21
+  static const Id PROP_GAMMA         = CV_CAP_PROP_GAMMA; //22;
+  static const Id PROP_TEMPERATURE   = CV_CAP_PROP_TEMPERATURE; // 23;
+  static const Id PROP_TRIGGER       = CV_CAP_PROP_TRIGGER; // 24;
+  static const Id PROP_TRIGGER_DELAY = CV_CAP_PROP_TRIGGER_DELAY; // 25;
+  static const Id PROP_WHITE_BALANCE_RED_V = CV_CAP_PROP_WHITE_BALANCE_RED_V; // 26;
+  static const Id PROP_ZOOM          = 27; //CV_CAP_PROP_ZOOM; // 27;
+  static const Id PROP_FOCUS         = 28; //CV_CAP_PROP_FOCUS; // 28;
+  static const Id PROP_GUID          = 29; //CV_CAP_PROP_GUID; // 29;
+
+protected:
+  // none yet
+private:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet (hopefully never!)
+protected:
+  // none yet
+
+private:
+  // none yet
+
+}; // class cedar::xxx
+
+#endif // CEDAR_CEDAR_DEV_SENSORS_VISUAL_CAMERA_PROPERTY_H
+
