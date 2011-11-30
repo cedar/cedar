791f179893e83768f38831214711ea365fd37815
Stephan Zibner <37666884+raul-ini@users.noreply.github.com>
2011-11-30 13:55:26 +0100

    fixed a bug in the FFT convolution

---
+++ tests/interactive/devices/gl/CoraHwAndSim/main.cpp
@@ -0,0 +1,116 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+ ----- Institute:   Ruhr-Universitaet Bochum
+                    Institut fuer Neuroinformatik
+
+ ----- File:        main.cpp
+
+ ----- Maintainer:  Hendrik Reimann
+ ----- Email:       hendrik.reimann@ini.rub.de
+ ----- Date:        2010 11 17
+
+ ----- Description: interactive test for the kinematic chain simulation
+
+ ----- Credits:
+ -----------------------------------------------------------------------------*/
+
+// LOCAL INCLUDES
+#include "ForwardInverseWidget.h"
+#include "iiClosedFormInverseKinematics.hpp"
+
+// PROJECT INCLUDES
+#include "cedar/devices/amtec/KinematicChain.h"
+#include "cedar/devices/robot/KinematicChainModel.h"
+#include "cedar/devices/robot/SimulatedKinematicChain.h"
+#include "cedar/devices/robot/gl/CoraArm.h"
+#include "cedar/devices/robot/gl/CoraHead.h"
+#include "cedar/devices/robot/gui/KinematicChainWidget.h"
+#include "cedar/auxiliaries/gl/Scene.h"
+#include "cedar/auxiliaries/gui/Viewer.h"
+#include "cedar/auxiliaries/gui/SceneWidget.h"
+
+// SYSTEM INCLUDES
+
+
+using namespace std;
+using namespace cedar::aux::gl;
+using namespace cedar::aux::gui;
+using namespace cedar::dev::robot::gl;
+using namespace cedar::dev::robot;
+
+bool useHardware = true;
+
+int main(int argc, char **argv)
+{
+  QApplication a(argc, argv);
+
+  // create kinematic chains
+  cedar::dev::robot::KinematicChainPtr p_cora_arm_sim(new SimulatedKinematicChain(string("../../../tests/interactive/devices/gl/CoraHwAndSim/cora_arm.conf")));
+  cedar::dev::robot::KinematicChainPtr p_cora_arm_hw;
+  if(useHardware)
+  {
+    p_cora_arm_hw = cedar::dev::robot::KinematicChainPtr(new cedar::dev::amtec::KinematicChain(string("../../../tests/interactive/devices/gl/CoraHwAndSim/cora_arm.conf")));
+  }
+
+  // create models calculation of the transformation
+  KinematicChainModelPtr p_cora_arm_model(new KinematicChainModel(p_cora_arm_sim));
+
+  // create gl visualization objects
+  cedar::dev::robot::gl::KinematicChainPtr p_cora_arm_visualization(new cedar::dev::robot::gl::CoraArm(p_cora_arm_model));
+
+  // create scene and viewer to display the arm
+  ScenePtr p_scene(new cedar::aux::gl::Scene);
+  p_scene->setSceneLimit(2);
+  p_scene->drawFloor(true);
+
+  cedar::aux::gl::ObjectPtr p_object;
+  p_object = p_cora_arm_visualization;
+  p_scene->addObject(p_object);
+
+  // create a simple viewer for the scene
+  Viewer viewer(p_scene);
+  viewer.show();
+  viewer.setSceneRadius(p_scene->getSceneLimit());
+
+  // create a widget to control the scene
+  //SceneWidgetPtr p_scene_widget(new SceneWidget(p_scene));
+  //p_scene_widget->show();
+
+  // create control widgets
+  vector<cedar::dev::robot::KinematicChainPtr> kinematic_chains;
+  kinematic_chains.push_back(p_cora_arm_sim);
+  if(useHardware)
+  {
+    kinematic_chains.push_back(p_cora_arm_hw);
+  }
+  ForwardInverseWidget output_widget(kinematic_chains, p_cora_arm_model);
+  output_widget.show();
+
+  p_cora_arm_model->startTimer(50.0);
+  viewer.startTimer(50);
+  a.exec();
+
+  p_cora_arm_sim->stop();
+  sleep(1);
+
+  return 0;
+}
---
+++ tests/unit/processing/Network/main.cpp
@@ -0,0 +1,120 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        main.cpp
+
+    Maintainer:  Oliver Lomp,
+                 Mathis Richter,
+                 Stephan Zibner
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de,
+                 mathis.richter@ini.ruhr-uni-bochum.de,
+                 stephan.zibner@ini.ruhr-uni-bochum.de
+    Date:        2011 06 24
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+
+// LOCAL INCLUDES
+
+// PROJECT INCLUDES
+#include "cedar/auxiliaries/LogFile.h"
+#include "cedar/processing/Manager.h"
+#include "cedar/processing/Network.h"
+#include "cedar/processing/Step.h"
+#include "cedar/processing/Arguments.h"
+#include "cedar/processing/ElementDeclaration.h"
+#include "cedar/processing/DeclarationRegistry.h"
+
+// SYSTEM INCLUDES
+#include <iostream>
+
+using namespace cedar::aux;
+
+class TestModule : public cedar::proc::Step
+{
+public:
+  TestModule()
+  {
+  }
+
+  void compute(const cedar::proc::Arguments& /* arguments */)
+  {
+    std::cout << "processing " << this->getName() << " = " << this->_mMessage << std::endl;
+  }
+
+  void readConfiguration(const cedar::aux::ConfigurationNode& node)
+  {
+    this->cedar::proc::Step::readConfiguration(node);
+    _mMessage = node.get<std::string>("outputString", "defaultOutputString");
+  }
+
+  std::string _mMessage;
+};
+
+
+
+int main(int /* argc */, char** /* argv */)
+{
+  using cedar::proc::Network;
+  using cedar::proc::StepPtr;
+  using cedar::proc::Step;
+
+  unsigned int errors = 0;
+
+  LogFile log_file("Network.log");
+  log_file.addTimeStamp();
+  log_file << std::endl;
+
+  log_file << "Creating step declaration ... ";
+  cedar::proc::ElementDeclarationPtr test_module_decl
+  (
+    new cedar::proc::ElementDeclarationTemplate<TestModule>()
+  );
+  log_file << "done." << std::endl;
+
+  log_file << "Adding declaration to the registry ... ";
+  cedar::proc::DeclarationRegistrySingleton::getInstance()->declareClass(test_module_decl);
+  log_file << "done." << std::endl;
+
+  log_file << "Reading Sample.json ... ";
+  cedar::proc::NetworkPtr network(new cedar::proc::Network());
+  network->readFile("Sample.json");
+  log_file << "done." << std::endl;
+
+  log_file << "Trying to call compute functions ... ";
+  StepPtr step_a = network->getElement<Step>("stepA");
+  step_a->onTrigger();
+
+  StepPtr step_b = network->getElement<Step>("stepB");
+  step_b->onTrigger();
+  log_file << "done." << std::endl;
+
+  // return
+  log_file << "Done. There were " << errors << " errors." << std::endl;
+  return errors;
+}
