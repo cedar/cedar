2de5fb0f2c42aaaed8f381bfb172f2604b885db6
Mathis Richter <5708333+mathisrichter@users.noreply.github.com>
2011-12-07 18:14:51 +0100

    Improved const-correctness and file style.

---
+++ cedar/auxiliaries/AbstractFactoryDerived.h
@@ -0,0 +1,111 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        AbstractFactoryDerived.h
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2011 03 09
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_AUX_ABSTRACT_FACTORY_DERIVED_H
+#define CEDAR_AUX_ABSTRACT_FACTORY_DERIVED_H
+
+// LOCAL INCLUDES
+#include "cedar/auxiliaries/namespace.h"
+#include "cedar/auxiliaries/AbstractFactory.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+
+
+/*!@brief Version of cedar::aux::FactoryDerived that works for abstract types.
+ *
+ * @see cedar::aux::FactoryDerived
+ */
+template <typename BaseType, typename DerivedType>
+class cedar::aux::AbstractFactoryDerived : public AbstractFactory<BaseType>
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // macros
+  //--------------------------------------------------------------------------------------------------------------------
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief allocates an object of type DerivedType and returns a pointer to BaseType
+  boost::shared_ptr<BaseType> allocate() const
+  {
+    return boost::shared_ptr<BaseType> (new DerivedType());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet (hopefully never!)
+protected:
+  // none yet
+private:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet (hopefully never!)
+protected:
+  // none yet
+
+private:
+  // none yet
+
+}; // class cedar::aux::AbstractFactoryDerived
+
+#endif // CEDAR_AUX_ABSTRACT_FACTORY_DERIVED_H
+
