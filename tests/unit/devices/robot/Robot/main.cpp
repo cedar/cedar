c01bbce4fb8b1b9ec887fe17c33854b59c639a98
Stephan Zibner <37666884+raul-ini@users.noreply.github.com>
2011-12-09 12:09:59 +0100

    fixed a bug in the build system (executables folders were not iterated correctly)

---
+++ tests/unit/devices/robot/Robot/main.cpp
@@ -0,0 +1,57 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        main.cpp
+
+    Maintainer:  Mathis Richter
+    Email:       mathis.richter@ini.rub.de
+    Date:        2010 11 08
+
+    Description: Implements all unit tests for the @em cedar::dev::robot::Robot class.
+
+    Credits:
+
+======================================================================================================================*/
+
+
+// LOCAL INCLUDES
+#include "unit/devices/robot/Robot/TestRobot.h"
+
+// PROJECT INCLUDES
+#include "cedar/devices/robot/Robot.h"
+
+// SYSTEM INCLUDES
+#include <string>
+
+
+using namespace cedar::tests::unit::dev::robot::Robot;
+
+int main()
+{
+  TestRobot robot;
+  robot.getComponent("TestComponent1");
+  robot.getComponent("TestComponent2");
+
+  return 0;
+}
