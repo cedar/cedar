1a54a487b86b56e7a62cec096742f18f7801859a
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2011-12-09 15:11:02 +0100

    Added dummy include files for NumericParameters.

---
+++ cedar/auxiliaries/gui/ImagePlot.cpp
@@ -0,0 +1,203 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        MatrixPlot.cpp
+
+    Maintainer:  Oliver Lomp,
+                 Mathis Richter,
+                 Stephan Zibner
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de,
+                 mathis.richter@ini.ruhr-uni-bochum.de,
+                 stephan.zibner@ini.ruhr-uni-bochum.de
+    Date:        2011 07 14
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+// LOCAL INCLUDES
+#include "cedar/auxiliaries/gui/ImagePlot.h"
+#include "cedar/auxiliaries/assert.h"
+#include "cedar/auxiliaries/gui/exceptions.h"
+#include "cedar/auxiliaries/ImageData.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+#include <QVBoxLayout>
+#include <iostream>
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+cedar::aux::gui::ImagePlot::ImagePlot(QWidget *pParent)
+:
+cedar::aux::gui::DataPlotInterface(pParent),
+mTimerId(0)
+{
+  QVBoxLayout *p_layout = new QVBoxLayout();
+  p_layout->setContentsMargins(0, 0, 0, 0);
+  this->setLayout(p_layout);
+  this->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+
+  mpImageDisplay = new QLabel("no image loaded");
+  p_layout->addWidget(mpImageDisplay);
+  mpImageDisplay->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+  mpImageDisplay->setWordWrap(true);
+}
+
+cedar::aux::gui::ImagePlot::~ImagePlot()
+{
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+void cedar::aux::gui::ImagePlot::timerEvent(QTimerEvent * /*pEvent*/)
+{
+  cv::Mat& mat = this->mData->getData();
+  switch(mat.type())
+  {
+    case CV_8UC1:
+    {
+      this->mData->lockForRead();
+      cv::Mat converted = threeChannelGrayscale(mat);
+      this->mData->unlock();
+      CEDAR_DEBUG_ASSERT(converted.type() == CV_8UC3);
+      this->imageFromMat(converted);
+      break;
+    }
+
+    case CV_8UC3:
+    {
+      this->mData->lockForRead();
+      this->imageFromMat(mat);
+      this->mData->unlock();
+      break;
+    }
+
+    case CV_32FC1:
+    {
+      //!@todo Some code here is redundant
+      // find min and max for scaling
+      double min, max;
+
+      this->mData->lockForRead();
+      cv::minMaxLoc(mat, &min, &max);
+      cv::Mat scaled = (mat - min) / (max - min) * 255.0;
+      this->mData->unlock();
+      cv::Mat mat_8u;
+      scaled.convertTo(mat_8u, CV_8U);
+
+      // convert grayscale to three-channel matrix
+      cv::Mat converted = threeChannelGrayscale(mat_8u);
+      CEDAR_DEBUG_ASSERT(converted.type() == CV_8UC3);
+      this->imageFromMat(converted);
+      break;
+    }
+
+    case CV_32FC3:
+      this->mpImageDisplay->setText("Cannot display CV_32FC3 matrix.");
+      return;
+
+    default:
+      QString text = QString("Unhandled matrix type %1.").arg(mat.type());
+      this->mpImageDisplay->setText(text);
+      return;
+  }
+
+  this->mpImageDisplay->setPixmap(QPixmap::fromImage(this->mImage));
+  this->resizePixmap();
+}
+
+cv::Mat cedar::aux::gui::ImagePlot::threeChannelGrayscale(const cv::Mat& in) const
+{
+  cv::Mat converted;
+  std::vector<cv::Mat> merge_vec;
+  merge_vec.push_back(in);
+  merge_vec.push_back(in);
+  merge_vec.push_back(in);
+  cv::merge(merge_vec, converted);
+  return converted;
+}
+
+void cedar::aux::gui::ImagePlot::imageFromMat(const cv::Mat& mat)
+{
+  this->mImage = QImage
+  (
+    mat.data,
+    mat.cols,
+    mat.rows,
+    mat.step,
+    QImage::Format_RGB888
+  ).rgbSwapped();
+}
+
+void cedar::aux::gui::ImagePlot::display(cedar::aux::DataPtr data)
+{
+  if (mTimerId != 0)
+    this->killTimer(mTimerId);
+
+  this->mData = boost::shared_dynamic_cast<cedar::aux::ImageData>(data);
+  if (!this->mData)
+  {
+    CEDAR_THROW(cedar::aux::gui::InvalidPlotData,
+                "Cannot cast to cedar::aux::ImageData in cedar::aux::gui::ImagePlot::display.");
+  }
+
+  mpImageDisplay->setText("no image loaded");
+
+  if (!this->mData->getData().empty())
+  {
+    mpImageDisplay->setText("");
+    this->mTimerId = this->startTimer(70);
+    CEDAR_DEBUG_ASSERT(mTimerId != 0);
+  }
+}
+
+void cedar::aux::gui::ImagePlot::resizeEvent(QResizeEvent * /*pEvent*/)
+{
+  this->resizePixmap();
+}
+
+void cedar::aux::gui::ImagePlot::resizePixmap()
+{
+  QSize scaled_size = this->mImage.size();
+  scaled_size.scale(this->mpImageDisplay->size(), Qt::KeepAspectRatio);
+  if ( (!this->mpImageDisplay->pixmap()
+        || scaled_size != this->mpImageDisplay->pixmap()->size()
+        )
+        && !this->mImage.isNull()
+      )
+  {
+    QImage scaled_image = this->mImage.scaled(this->mpImageDisplay->size(),
+                                              Qt::KeepAspectRatio,
+                                              Qt::SmoothTransformation);
+    this->mpImageDisplay->setPixmap(QPixmap::fromImage(scaled_image));
+  }
+}
---
+++ cedar/processing/sources/GaussInput.h
@@ -0,0 +1,124 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        GaussInput.h
+
+    Maintainer:  Oliver Lomp,
+                 Mathis Richter,
+                 Stephan Zibner
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de,
+                 mathis.richter@ini.ruhr-uni-bochum.de,
+                 stephan.zibner@ini.ruhr-uni-bochum.de
+    Date:        2011 07 19
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_PROC_SOURCES_GAUSS_INPUT_H
+#define CEDAR_PROC_SOURCES_GAUSS_INPUT_H
+
+// LOCAL INCLUDES
+#include "cedar/processing/sources/namespace.h"
+#include "cedar/processing/namespace.h"
+#include "cedar/dynamics/namespace.h"
+#include "cedar/processing/Step.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+
+
+/*!@brief Generates a matrix with a Gaussian.
+ *
+ * @todo More detailed description.
+ */
+class cedar::proc::sources::GaussInput : public cedar::proc::Step
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // macros
+  //--------------------------------------------------------------------------------------------------------------------
+  Q_OBJECT
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief The standard constructor.
+  GaussInput();
+
+  //!@brief Destructor
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief refreshes the internal matrix containing the Gaussian input
+  void compute(const cedar::proc::Arguments& arguments);
+
+public slots:
+  //!@brief a slot that is triggered if any of the Gauss function parameters are changed
+  void updateMatrix();
+  //!@brief a slot to process changes in dimensionality, including reinitializing the buffers
+  void updateDimensionality();
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  //!@brief the buffer containing the output
+  cedar::aux::MatDataPtr mOutput;
+private:
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  //!@brief the amplitude of the Gauss function
+  cedar::aux::DoubleParameterPtr _mAmplitude;
+  //!@brief the dimensionality of the Gauss function
+  cedar::aux::UIntParameterPtr _mDimensionality;
+  //!@brief the vector of sigmas (i.e. width) of the Gauss function
+  cedar::aux::DoubleVectorParameterPtr _mSigmas;
+  //!@brief the vector of centers of the Gauss function
+  cedar::aux::DoubleVectorParameterPtr _mCenters;
+  //!@brief the vector of sizes of matrix containing the Gauss function for each dimension
+  cedar::aux::UIntVectorParameterPtr _mSizes;
+
+private:
+
+}; // class cedar::proc::source::GaussInput
+
+#endif // CEDAR_PROC_SOURCES_GAUSS_INPUT_H
+
