aa61dab9d0966c92a461b11e980df701bfd07887
Stephan Zibner <37666884+raul-ini@users.noreply.github.com>
2011-12-12 16:40:48 +0100

    processingIde is now installed during make install

---
+++ cedar/auxiliaries/math/coordinateTransformations.h
@@ -0,0 +1,112 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        coordinateTransformations.h
+
+    Maintainer:  Hendrik Reimann
+    Email:       hendrik.reimann@ini.rub.de
+    Date:        2010 11 11
+
+    Description: This file provides transformations between different coordinate systems of the same space.
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_AUX_MATH_COORDNIATE_TRANSFORMATIONS_H
+#define CEDAR_AUX_MATH_COORDNIATE_TRANSFORMATIONS_H
+
+// LOCAL INCLUDES
+#include "cedar/auxiliaries/lib.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+#include <opencv2/opencv.hpp>
+
+namespace cedar
+{
+  namespace aux
+  {
+    namespace math
+    {
+      /*! Cartesian --> polar coordinates of 2-dimensional real space
+       * @param x    Cartesian x coordinate
+       * @param y    Cartesian y coordinate
+       * @param rho    polar coordinate radius
+       * @param theta    polar coordinate angle
+       */
+      CEDAR_AUX_LIB_EXPORT void cartesianToPolar(const double x, const double y, double& rho, double& theta);
+
+      /*! spherical --> cartesian coordinates for points of 3-dimensional space
+       * spherical coordinate explanation:
+       * radius: distance from origin
+       * theta: angular displacement from the z-axis in radians (inclination)
+       * phi: angular displacement from positive x-axis in x-y-plane in radians (azimuth)
+       * @param spherical    spherical coordinates (radius, theta, phi), use 3 rows, 1 column
+       * @param cartesian    Cartesian coordinates (x, y, z), use 3 rows, 1 column
+       */
+      template<typename T>
+      void sphericalToCartesian(const cv::Mat& spherical, cv::Mat& cartesian);
+
+      /*! spherical --> cartesian coordinates for points of 3-dimensional space
+       * spherical coordinate explanation:
+       * radius: distance from origin
+       * theta: angular displacement from the z-axis in radians (inclination)
+       * phi: angular displacement from positive x-axis in x-y-plane in radians (azimuth)
+       * @param spherical    spherical coordinates (radius, theta, phi), use 3 rows, 1 column
+       * @return Cartesian coordinates (x, y, z), uses 3 rows, 1 column
+       */
+      template<typename T>
+      cv::Mat sphericalToCartesian(const cv::Mat& spherical);
+
+      /*! cartesian --> spherical coordinates of 3-dimensional real space
+       * radius: distance from origin
+       * theta: angular displacement from the z-axis in radians (inclination)
+       * phi: angular displacement from positive x-axis in x-y-plane in radians (azimuth)
+       * @param cartesian    Cartesian coordinates (x, y, z), use 3 rows, 1 column
+       * @param spherical    spherical coordinates (radius, theta, phi), use 3 rows, 1 column
+       */
+      template<typename T>
+      void cartesianToSpherical(const cv::Mat& cartesian, cv::Mat& spherical);
+//
+//      //! transforms a vector at a point, both given in spherical coords, to a vector in cartesian coords
+//      //! spherical = r, phi, theta -- radius, azimuth, zenith
+//      void timeDerivativeSphericalToCartesian(
+//                                               CvMAT& timeDerivativeSpherical,
+//                                               CvMAT& pointSpherical,
+//                                               CvMAT& timeDerivativeCartesian
+//                                             );
+//
+//      //! transforms a vector at a point, both given in cartesian coords, to a vector in spherical coords
+//      //! spherical = r, phi, theta -- radius, azimuth, zenith
+//      void timeDerivativeCartesianToSpherical(
+//                                               CvMAT& timeDerivativeCartesian,
+//                                               CvMAT& pointCartesian,
+//                                               CvMAT& timeDerivativeSpherical
+//                                             );
+    }
+  }
+}
+#endif  // CEDAR_AUX_MATH_COORDNIATE_TRANSFORMATIONS_H
---
+++ cedar/processing/gui/PropertyPane.h
@@ -0,0 +1,177 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        PropertyPane.h
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2011 03 09
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_PROC_PROPERTY_PANE_H
+#define CEDAR_PROC_PROPERTY_PANE_H
+
+// LOCAL INCLUDES
+#include "cedar/processing/gui/namespace.h"
+#include "cedar/processing/namespace.h"
+#include "cedar/auxiliaries/TypeBasedFactory.h"
+#include "cedar/processing/Step.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+#include <QTableWidget>
+#include <boost/signals2/connection.hpp>
+
+
+/*!@brief A widget for displaying the parameters of configurables.
+ *
+ *        This widget offers a quick way of creating a user interface for classes that implement the
+ *        cedar::aux::Configurable interface. Parameters of the configurable are displayed in a tabular widget by their
+ *        name and have an edit widget associated with them.
+ */
+class cedar::proc::gui::PropertyPane : public QTableWidget
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // macros
+  //--------------------------------------------------------------------------------------------------------------------
+  Q_OBJECT
+
+private:
+  //! Type of a factory that creates control widgets based on parameter types.
+  typedef
+      cedar::aux::TypeBasedFactory
+      <
+        cedar::aux::Parameter,
+        cedar::proc::gui::Parameter,
+        cedar::aux::ParameterPtr
+      >
+      DataWidgetTypes;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  /*!@brief The standard constructor.
+   */
+  PropertyPane(QWidget *pParent = NULL);
+
+  //!@brief Destructor.
+  ~PropertyPane();
+
+  /*!@brief Displays the parameters for a cedar::proc::Step.
+   *
+   * @todo  Remove this function, use name undecorating instead.
+   */
+  void display(cedar::proc::StepPtr pStep);
+
+
+  /*!@brief Displays the parameters for a cedar::proc::Trigger.
+   *
+   * @todo  Remove this function, use name undecorating instead.
+   */
+  void display(cedar::proc::TriggerPtr pTrigger);
+
+  /*!@brief Displays the parameters for a given configurable.
+   */
+  void display(cedar::aux::ConfigurablePtr pConfigurable);
+
+  /*!@brief Resets the contents of the widget.
+   */
+  void resetContents();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  /*!@brief Returns the factory used for creating widgets based on the data types.
+   */
+  DataWidgetTypes& dataWidgetTypes();
+
+public slots:
+  /*!@brief Resets the pointer to the configurable displayed in this widget.
+   */
+  void resetPointer();
+
+  /*!@brief Recreates the widget.
+   */
+  void redraw();
+
+  /*!@brief Slot that reacts to a change in row size.
+   */
+  void rowSizeChanged();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  /*!@brief Appends the representation for a parameter to the table.
+   */
+  void append(cedar::aux::Configurable::ParameterList& parameters);
+
+  /*!@brief Adds a row containing a heading.
+   */
+  void addHeadingRow(const std::string& label);
+
+  /*!@brief Adds a row that contains only a label.
+   */
+  void addLabelRow(const std::string& label);
+
+  /*!@brief Adds a row that displays a given parameter.
+   */
+  void addPropertyRow(cedar::aux::ParameterPtr parameter);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+private:
+  //! Factory that associates parameter types with the widgets used to display them.
+  static DataWidgetTypes mDataWidgetTypes;
+
+  //! Weak pointer to the configurable being displayed by this property pane.
+  boost::weak_ptr<cedar::aux::Configurable> mDisplayedConfigurable;
+
+  //! Association from parameters to rows.
+  std::map<cedar::proc::gui::Parameter*, int> mParameterRowIndex;
+
+  //! Connection to the configurable's tree changed signal.
+  boost::signals2::connection mSlotConnection;
+
+}; // class cedar::proc::gui::PropertyPane
+
+#endif // CEDAR_PROC_PROPERTY_PANE_H
+
