1a962f3ba7c732f4cb1c1fd594e6ca2169e03658
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2011-12-13 15:06:05 +0100

    Fixed a few typos.

---
+++ cedar/auxiliaries/gl/Prism.cpp
@@ -0,0 +1,117 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+ ----- Institute:   Ruhr-Universitaet-Bochum
+                    Institut fuer Neuroinformatik
+ 
+ ----- File:        Prism.cpp
+ 
+ ----- Maintainer:  Hendrik Reimann
+ ------Email:       hendrik.reimann@ini.rub.de
+ ----- Date:        2010 10 29
+ 
+ ----- Description: visualization for a prism with equilateral triangular base
+ 
+ ----- Credits:     
+ ---------------------------------------------------------------------------------------------------------------------*/
+
+// CEDAR INCLUDES
+#include "drawShapes.h"
+#include "Prism.h"
+
+// SYSTEM INCLUDES
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+cedar::aux::gl::Prism::Prism(cedar::aux::ObjectPtr pObject)
+:
+cedar::aux::gl::Object(pObject)
+{
+  mWidth = 3;
+  mHeight = 1;
+  mColorR = 1;
+  mColorG = 0;
+  mColorB = 0;
+  mObjectType = "Prism";
+}
+
+cedar::aux::gl::Prism::Prism(
+                              cedar::aux::ObjectPtr pObject,
+                              double width,
+                              double height,
+                              double R,
+                              double G,
+                              double B
+                            )
+:
+cedar::aux::gl::Object(pObject)
+{
+  mWidth = width;
+  mHeight = height;
+  mColorR = R;
+  mColorG = G;
+  mColorB = B;
+  mObjectType = "Prism";
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+void cedar::aux::gl::Prism::draw()
+{
+  // move to origin
+  glPopMatrix();
+  glPushMatrix();
+  
+  // move to object coordinates
+  mTransformationTranspose = mpObject->getTransformation().t();
+  glMultMatrixd((GLdouble*)mTransformationTranspose.data);
+  
+  // draw object
+  if (mIsVisible)
+  {
+    gl::setColor(mColorR, mColorG, mColorB);
+    drawPrism(mWidth, mHeight, mIsDrawnAsWireFrame);
+  }
+}
+
+void cedar::aux::gl::Prism::setWidth(double value)
+{
+  mWidth = value;
+}
+
+void cedar::aux::gl::Prism::setHeight(double value)
+{
+  mHeight = value;
+}
+
+double cedar::aux::gl::Prism::width() const
+{
+  return mWidth;
+}
+
+double cedar::aux::gl::Prism::height() const
+{
+  return mHeight;
+}
---
+++ cedar/auxiliaries/gl/Sphere.h
@@ -0,0 +1,102 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+ ----- Institute:   Ruhr-Universitaet-Bochum
+                    Institut fuer Neuroinformatik
+ 
+ ----- File:        Sphere.h
+ 
+ ----- Maintainer:  Hendrik Reimann
+ ------Email:       hendrik.reimann@ini.rub.de
+ ----- Date:        2010 10 28
+ 
+ ----- Description: visualization for a sphere
+ 
+ ----- Credits:     
+ ---------------------------------------------------------------------------------------------------------------------*/
+
+#ifndef CEDAR_AUX_GL_SPHERE_H
+#define CEDAR_AUX_GL_SPHERE_H
+
+// CEDAR INCLUDES
+#include "cedar/auxiliaries/gl/namespace.h"
+#include "cedar/auxiliaries/gl/Object.h"
+
+// SYSTEM INCLUDES
+
+/*!@brief Simple OpenGL visualization of a sphere
+ *
+ * This class visualizes an instance of cedar::aux::Object as a sphere with specified dimensions
+ *
+ * @remarks To get a simple visualization of the Object on screen, add an instance of this class to a
+ * cedar::aux::gl::Scene and create a cedar::aux::gui::Viewer for it
+ */
+class cedar::aux::gl::Sphere : public cedar::aux::gl::Object
+{
+public:
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+  /*!@brief standard constructor.
+   * @param pObject    pointer to the aux::Object being visualized
+   */
+  Sphere(cedar::aux::ObjectPtr pObject);
+
+  /*!@brief constructor. 
+   * @param pObject pointer to the aux::Object being visualized
+   * @param radius radius of the sphere
+   * @param R color, value for red channel in RGB
+   * @param G color, value for green channel in RGB
+   * @param B color, value for blue channel in RGB
+   */
+  Sphere(
+          cedar::aux::ObjectPtr pObject,
+          double radius,
+          double R = 1,
+          double G = 0,
+          double B = 0
+        );
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief draws a visualization of the object in the current GL context
+  void draw();
+
+  /*!@brief set radius of the sphere
+   * @param value    new radius
+   */
+  void setRadius(double value);
+
+  /*!@brief get radius of the sphere
+   * @return    radius
+   */
+  double radius() const;
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  double mRadius;
+};
+
+#endif // CEDAR_AUX_GL_SPHERE_H
