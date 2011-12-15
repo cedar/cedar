8fda23e9ba613574e32c97662a62e467a6e99cc7
Mathis Richter <5708333+mathisrichter@users.noreply.github.com>
2011-12-15 10:47:02 +0100

    Fixed bugs in the Projection class for projections to 0D.

---
+++ cedar/auxiliaries/gl/Scene.h
@@ -0,0 +1,140 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        Scene.h
+
+    Maintainer:  Hendrik Reimann
+    Email:       hendrik.reimann@ini.rub.de
+    Date:        2010 11 25
+
+    Description: Scene for organizing instances of cedar::aux::gl::Object
+
+    Credits:
+
+======================================================================================================================*/
+
+
+#ifndef CEDAR_AUX_GL_SCENE_H
+#define CEDAR_AUX_GL_SCENE_H
+
+// CEDAR INCLUDES
+#include "cedar/auxiliaries/gl/namespace.h"
+#include "cedar/auxiliaries/gl/Object.h"
+
+// SYSTEM INCLUDES
+#include <string>
+#include <QList>
+
+/*!@brief This class visualizes several objects in the same environment
+ *
+ * An instance of this class manages pointers to several instances of gl::Object.
+ *
+ * @remarks To get a simple visualization of the Objects in the scene, create a aux::gui::Viewer for it
+ */
+class cedar::aux::gl::Scene
+{
+public:
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+  /*!@brief the constructor */
+  Scene();
+  
+  /*!@brief the destructor */
+  ~Scene();
+  //!\todo something is not destroyed properly, find out what happens and fix it
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  /*!@brief tells the scene whether a simple floor in the xy-plane should be drawn or not
+   * @param state truth value of 'floor being drawn'
+   */
+  void drawFloor(bool state=true);
+
+  /*!@brief sets a rough delimiter of the scene within which things are guaranteed to be visualized
+   * @param value new scene delimiter
+   */
+  void setSceneLimit(double value);
+  
+  /*!@brief returns the rough delimiter of the scene within which things are guaranteed to be visualized
+   * @return scene limit
+   */
+  double getSceneLimit() const;
+  
+  /*!@brief adds an object to the scene
+   * @param rpObject pointer to the object
+   * @return index of the object in the scene
+   */
+  int addObject(cedar::aux::gl::ObjectPtr& rpObject);
+  
+  /*!@brief removes an object from the scene
+   * @param index index of the object in the scene
+   */
+  void deleteObject(int index);
+  
+  /*!@brief removes all objects from the scene */
+  void clear();
+  
+  /*!@brief draws all objects in the scene */
+  void draw();
+  
+  /*!@brief convenience function to check whether a scene containts any objects
+   * @return true if there is no object in the scene
+   */
+  bool isEmpty() const;
+  
+  /*!@todo rename getNumberOfObjects
+   * @brief gives the number of objects currently in the scene
+   * @return number of objects currently in the scene
+   */
+  int numberOfObjects() const;
+  
+  /*!@brief access to a single object in the scene
+   * @param index index of the desired object within the scene
+   * @return pointer to the object with the specified index
+   */
+  cedar::aux::gl::ObjectPtr getObject(int index);
+  
+  /*!@brief initializes gl setup, should be called from all viewers */
+  void initGl();
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  /*!@brief class initialization */
+  void init();
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  bool mIsDrawingFloor;
+  double mSceneLimit;
+  QList<cedar::aux::gl::ObjectPtr> mObjects;
+};
+
+#endif  // CEDAR_AUX_GL_SCENE_H
---
+++ cedar/auxiliaries/math/functions.h
@@ -0,0 +1,78 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        functions.h
+
+    Maintainer:  Stephan Zibner
+    Email:       stephan.zibner@ini.rub.de
+    Date:        2011 07 19
+
+    Description: Math functions
+
+    Credits:
+
+======================================================================================================================*/
+
+
+#ifndef CEDAR_AUX_MATH_FUNCTIONS_H
+#define CEDAR_AUX_MATH_FUNCTIONS_H
+
+// LOCAL INCLUDES
+#include "cedar/auxiliaries/math/namespace.h"
+
+// PROJECT INCLUDES
+
+// SYSTEM INCLUDES
+#include <math.h>
+
+namespace cedar
+{
+  namespace aux
+  {
+    namespace math
+    {
+    /*!@brief A simple one-dimensional Gauss function.
+     */
+      inline double gauss(double x, double sigma)
+      {
+        return exp(-pow(x, 2.0) / (2.0 * pow(sigma, 2.0)));
+      }
+
+      /*!@brief Rounds a number to the closest integer.
+       */
+      inline double round(double val)
+      {
+        return static_cast<double>(static_cast<int>(val + 0.5));
+      }
+
+      /*!@brief Rounds a number to the closest integer.
+       */
+      inline float round(float val)
+      {
+        return static_cast<float>(static_cast<int>(val + 0.5));
+      }
+    }
+  }
+}
+#endif  // CEDAR_AUX_MATH_FUNCTIONS_H
