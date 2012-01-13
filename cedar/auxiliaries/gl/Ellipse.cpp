c5592b14f5bfeed60dfbc0f33f1916d4aa4c48a3
StephanZibner <StephanZibner@localhost>
2012-01-13 11:31:53 +0100

    fixed an error in scan path of doxygen after using "make package", this is issue #149 in testing

---
+++ cedar/auxiliaries/gl/Ellipse.cpp
@@ -0,0 +1,131 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        Ellipse.cpp
+
+    Maintainer:  Hendrik Reimann
+    Email:       hendrik.reimann@ini.rub.de
+    Date:        2010 11 29
+
+    Description: Visualization of an ellipse
+
+    Credits:
+
+======================================================================================================================*/
+
+// CEDAR INCLUDES
+#include "drawShapes.h"
+#include "Ellipse.h"
+
+// SYSTEM INCLUDES
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+cedar::aux::gl::Ellipse::Ellipse(cedar::aux::ObjectPtr pObject)
+:
+cedar::aux::gl::Object(pObject)
+{
+  mLength = 2;
+  mWidth = 3;
+  mThickness = .3;
+  mColorR = 1;
+  mColorG = 0;
+  mColorB = 0;
+  mObjectType = "Ellipse";
+}
+
+cedar::aux::gl::Ellipse::Ellipse(
+                                  cedar::aux::ObjectPtr pObject,
+                                  double length,
+                                  double width,
+                                  double thickness,
+                                  double R,
+                                  double G,
+                                  double B
+                                )
+:
+cedar::aux::gl::Object(pObject)
+{
+  mLength = length;
+  mWidth = width;
+  mThickness = thickness;
+  mColorR = R;
+  mColorG = G;
+  mColorB = B;
+  mObjectType = "Ellipse";
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+void cedar::aux::gl::Ellipse::setLength(double value)
+{
+  mLength = value;
+}
+
+void cedar::aux::gl::Ellipse::setWidth(double value)
+{
+  mWidth = value;
+}
+
+void cedar::aux::gl::Ellipse::setThickness(double value)
+{
+  mThickness = value;
+}
+
+double cedar::aux::gl::Ellipse::length() const
+{
+  return mLength;
+}
+
+double cedar::aux::gl::Ellipse::width() const
+{
+  return mWidth;
+}
+
+double cedar::aux::gl::Ellipse::thickness() const
+{
+  return mThickness;
+}
+
+void cedar::aux::gl::Ellipse::draw()
+{
+  // move to origin
+  glPopMatrix();
+  glPushMatrix();
+  
+  // move to object coordinates
+  mTransformationTranspose = mpObject->getTransformation().t();
+  glMultMatrixd((GLdouble*)mTransformationTranspose.data);
+  
+  // draw the Ellipse
+  if (mIsVisible)
+  {
+    gl::setColor(mColorR, mColorG, mColorB);
+    drawEllipse(mWidth, mLength, mThickness, mResolution, mResolution, mIsDrawnAsWireFrame);
+  }
+}
---
+++ cedar/auxiliaries/gui/MatrixPlot.h
@@ -0,0 +1,113 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        MatrixPlot.h
+
+    Maintainer:  Oliver Lomp,
+                 Mathis Richter,
+                 Stephan Zibner
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de,
+                 mathis.richter@ini.ruhr-uni-bochum.de,
+                 stephan.zibner@ini.ruhr-uni-bochum.de
+    Date:        2011 07 14
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_AUX_GUI_MATRIX_PLOT_H
+#define CEDAR_AUX_GUI_MATRIX_PLOT_H
+
+// CEDAR INCLUDES
+#include "cedar/auxiliaries/gui/namespace.h"
+#include "cedar/auxiliaries/gui/DataPlotInterface.h"
+
+// SYSTEM INCLUDES
+#include <QWidget>
+#include <QReadWriteLock>
+#include <opencv2/opencv.hpp>
+#include <qwtplot3d/qwt3d_types.h>
+
+/*!@brief Base class for plots that can display matrices.
+ *
+ * @todo Write more detailed description of the class here.
+ */
+class cedar::aux::gui::MatrixPlot : public cedar::aux::gui::DataPlotInterface
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // macros
+  //--------------------------------------------------------------------------------------------------------------------
+  Q_OBJECT
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief The standard constructor.
+  MatrixPlot(QWidget *pParent = NULL);
+
+  //!@brief Destructor
+  ~MatrixPlot();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief display a MatData
+  void display(cedar::aux::DataPtr data);
+
+  //!@brief return vector of standard colors
+  static const Qwt3D::ColorVector& getStandardColorVector();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+private:
+  //!@brief the displayed MatData
+  cedar::aux::MatDataPtr mData;
+  //!@brief the plot widget
+  QWidget *mpCurrentPlotWidget;
+  //!@brief window title
+  QString mWindowTitle;
+  //!@brief vector filled with standard colors
+  static Qwt3D::ColorVector mStandardColorVector;
+}; // class cedar::aux::gui::MatrixPlot
+
+#endif // CEDAR_AUX_GUI_MATRIX_PLOT_H
