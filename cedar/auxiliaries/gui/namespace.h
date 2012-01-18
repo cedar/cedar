dbf5173469bbb4021e15f3e10297075133608772
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2012-01-18 13:27:27 +0100

    Added a parameter for files.

--- cedar/auxiliaries/gui/namespace.h
+++ cedar/auxiliaries/gui/namespace.h
@@ -76,6 +76,7 @@ namespace cedar
       class CEDAR_AUX_LIB_EXPORT DoubleParameter;
       class CEDAR_AUX_LIB_EXPORT DoubleVectorParameter;
       class CEDAR_AUX_LIB_EXPORT EnumParameter;
+      class CEDAR_AUX_LIB_EXPORT FileParameter;
       class CEDAR_AUX_LIB_EXPORT Parameter;
       class CEDAR_AUX_LIB_EXPORT StringParameter;
       class CEDAR_AUX_LIB_EXPORT UIntParameter;
