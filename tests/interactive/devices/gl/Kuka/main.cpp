153702927038168007ac566eb4f515c5bd1145ae
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2012-03-20 15:10:38 +0100

    Added a function for converting strings to numbers.

---
+++ tests/interactive/devices/gl/Kuka/main.cpp
@@ -0,0 +1,118 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+ ----- Institute:   Ruhr-Universitaet Bochum
+                    Institut fuer Neuroinformatik
+
+ ----- File:        main.cpp
+
+ ----- Maintainer:  Hendrik Reimann
+ ----- Email:       hendrik.reimann@ini.rub.de
+ ----- Date:        2010 11 17
+
+ ----- Description: interactive test for the kinematic chain simulation
+
+ ----- Credits:
+ -----------------------------------------------------------------------------*/
+
+// CEDAR INCLUDES
+#include "cedar/devices/robot/SimulatedKinematicChain.h"
+#include "cedar/devices/robot/KinematicChainModel.h"
+#include "cedar/devices/robot/gl/KukaArm.h"
+#include "cedar/devices/robot/gui/KinematicChainWidget.h"
+#include "cedar/auxiliaries/System.h"
+#include "cedar/auxiliaries/gl/Scene.h"
+#include "cedar/auxiliaries/gui/Viewer.h"
+#include "cedar/auxiliaries/gui/SceneWidget.h"
+#include "cedar/auxiliaries/sleepFunctions.h"
+
+
+// SYSTEM INCLUDES
+#include <QApplication>
+
+
+int main(int argc, char **argv)
+{
+  std::string configuration_file = cedar::aux::System::locateResource("configs/kuka_lwr4.conf");
+  std::string polygon_file_path = "";
+  // help requested?
+  if ((argc == 2) && (std::string(argv[1]) == "-h"))
+  { // Check the value of argc. If not enough parameters have been passed, inform user and exit.
+    std::cout << "Usage is -c <config file> -p <path to polygon data>" << std::endl;
+    return 0;
+  }
+  // parse arguments
+  for (int i = 1; i < argc; i++)
+  {
+    if (i+1 != argc) // check that we haven't finished parsing already
+    {
+      if (std::string(argv[i]) == "-c")
+      {
+        configuration_file = std::string(argv[i+1]);
+      }
+    }
+  }
+
+
+
+
+
+  QApplication a(argc, argv);
+
+  // create simulated kinematic chains
+  cedar::dev::robot::KinematicChainPtr p_kuka_arm(new cedar::dev::robot::SimulatedKinematicChain(configuration_file));
+
+  // create models calculation of the transformation
+  cedar::dev::robot::KinematicChainModelPtr p_kuka_arm_model
+  (
+    new cedar::dev::robot::KinematicChainModel(p_kuka_arm)
+  );
+
+  // create gl visualization objects
+  cedar::aux::gl::RigidBodyVisualizationPtr p_arm_visualization;
+  cedar::dev::robot::gl::KinematicChainPtr p_kuka_arm_visualization
+  (
+    new cedar::dev::robot::gl::KukaArm(p_kuka_arm_model)
+  );
+  p_arm_visualization = p_kuka_arm_visualization;
+
+  // create scene and viewer to display the arm
+  cedar::aux::gl::ScenePtr p_scene(new cedar::aux::gl::Scene());
+  p_scene->setSceneLimit(2);
+  p_scene->drawFloor(true);
+  p_scene->addRigidBodyVisualization(p_arm_visualization);
+  cedar::aux::gui::Viewer viewer(p_scene);
+  viewer.show();
+  viewer.setSceneRadius(p_scene->getSceneLimit());
+
+  // create control widgets for the scene and the arm
+  cedar::aux::gui::SceneWidgetPtr p_scene_widget(new cedar::aux::gui::SceneWidget(p_scene));
+  cedar::dev::robot::gui::KinematicChainWidget widget_arm(p_kuka_arm);
+
+  // show and start everything
+  p_scene_widget->show();
+  widget_arm.show();
+  p_kuka_arm_model->startTimer(50.0);
+  viewer.startTimer(50);
+  a.exec();
+
+  return 0;
+}
---
+++ tests/unit/auxiliaries/math/tools/main.cpp
@@ -0,0 +1,127 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        main.cpp
+
+    Maintainer:  Stephan Zibner
+    Email:       stephan.zibner@rub.de
+    Date:        2010 11 11
+
+    Description: Tests all math tools.
+
+    Credits:
+
+======================================================================================================================*/
+
+
+// LOCAL INCLUDES
+
+// PROJECT INCLUDES
+#include "cedar/auxiliaries/math/tools.h"
+#include "cedar/auxiliaries/math/constants.h"
+
+// SYSTEM INCLUDES
+#include <opencv2/opencv.hpp>
+
+
+int main()
+{
+  // the number of errors encountered in this test
+  int errors = 0;
+  // the current test number
+  int test_number = 0;
+
+  // test stuff
+  std::cout << "test no" << test_number++ << std::endl;
+  cv::Mat identity = cv::Mat::eye(3, 3, CV_64FC1);
+  std::cout << "this should be the 3x3 identity matrix:" << std::endl;
+  cedar::aux::math::write(identity);
+
+  double min_val, max_val;
+  min_val = cedar::aux::math::min(identity);
+  max_val = cedar::aux::math::max(identity);
+  if (min_val != 0.0)
+  {
+    std::cout << "error in min(const cv::Mat)" << std::endl;
+    errors++;
+  }
+  if (max_val != 1.0)
+   {
+     std::cout << "error in max(const cv::Mat)" << std::endl;
+     errors++;
+   }
+  std::cout << "test no" << test_number++ << std::endl;
+  if (cedar::aux::math::normalizeAngle(5.9) <= -cedar::aux::math::pi
+      || cedar::aux::math::normalizeAngle(5.9) > cedar::aux::math::pi)
+  {
+    std::cout << "error in normalizeAngle(double)" << std::endl;
+    errors++;
+  }
+  if (cedar::aux::math::normalizeAngle(-cedar::aux::math::pi) <= -cedar::aux::math::pi
+      || cedar::aux::math::normalizeAngle(-cedar::aux::math::pi) > cedar::aux::math::pi)
+  {
+    std::cout << "error in normalizeAngle(double)" << std::endl;
+    errors++;
+  }
+  if (cedar::aux::math::normalizeAngle(10000) <= -cedar::aux::math::pi
+      || cedar::aux::math::normalizeAngle(10000) > cedar::aux::math::pi)
+  {
+    std::cout << "error in normalizeAngle(double)" << std::endl;
+    errors++;
+  }
+  // test getDimensionalityOf
+  cv::Mat matrix_0D(1, 1, CV_32F);
+  unsigned int dim = cedar::aux::math::getDimensionalityOf(matrix_0D);
+  if (dim != 0)
+  {
+    std::cout << "error in getDimensionalityOf(cv::Mat), expected dimensionality 0, got " << dim << std::endl;
+  }
+  cv::Mat matrix_1D(11, 1, CV_32F);
+  dim = cedar::aux::math::getDimensionalityOf(matrix_1D);
+  if (dim != 0)
+  {
+    std::cout << "error in getDimensionalityOf(cv::Mat), expected dimensionality 1, got " << dim << std::endl;
+  }
+  dim = cedar::aux::math::getDimensionalityOf(matrix_1D.t());
+  if (dim != 0)
+  {
+    std::cout << "error in getDimensionalityOf(cv::Mat), expected dimensionality 1, got " << dim << std::endl;
+  }
+  cv::Mat matrix_2D(11, 30, CV_32F);
+  dim = cedar::aux::math::getDimensionalityOf(matrix_2D);
+  if (dim != 0)
+  {
+    std::cout << "error in getDimensionalityOf(cv::Mat), expected dimensionality 2, got " << dim << std::endl;
+  }
+
+  // test round
+
+
+  std::cout << "test finished, there were " << errors << " errors" << std::endl;
+  if (errors > 255)
+  {
+    errors = 255;
+  }
+  return errors;
+}
