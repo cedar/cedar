4e0c076ac9958d832b91d0d00c6869d0a9742b66
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2012-06-18 14:22:43 +0200

    Implemented a switch step that mixes two signals based on a factor.

--- cedar/processing/steps/namespace.h
+++ cedar/processing/steps/namespace.h
@@ -54,10 +54,11 @@ namespace cedar
     namespace steps
     {
       //!@cond SKIPPED_DOCUMENTATION
-      CEDAR_DECLARE_PROC_CLASS(StaticGain);
+      CEDAR_DECLARE_PROC_CLASS(Convolution);
       CEDAR_DECLARE_PROC_CLASS(Projection);
       CEDAR_DECLARE_PROC_CLASS(Resize);
-      CEDAR_DECLARE_PROC_CLASS(Convolution);
+      CEDAR_DECLARE_PROC_CLASS(StaticGain);
+      CEDAR_DECLARE_PROC_CLASS(Switch);
 
 #ifdef CEDAR_USE_YARP
       CEDAR_DECLARE_PROC_CLASS(NetWriterSink);
