a0a608a43f48e7b60d4c17af201eec4629fb84b3
Hendrik Reimann <hendrik.reimann@ini.rub.de>
2012-06-18 19:24:04 +0200

    added internal camera matrix to MountedCameraViewer

--- cedar/devices/robot/gui/MountedCameraViewer.cpp
+++ cedar/devices/robot/gui/MountedCameraViewer.cpp
@@ -60,9 +60,6 @@ mMountingJoint
 {
   this->addConfigurableChild("coordinate frame", mCameraCoordinateFrame);
   this->showEntireScene();
-//  this->setCameraIsEdited();
-
-
 }
 
 cedar::dev::robot::gui::MountedCameraViewer::~MountedCameraViewer()
@@ -93,37 +90,65 @@ void cedar::dev::robot::gui::MountedCameraViewer::readConfiguration(const cedar:
 
 void cedar::dev::robot::gui::MountedCameraViewer::draw()
 {
-  // calculate coordinate transformation to camera frame
+  // calculate coordinate transformation to camera frame, i.e. external transformation
   mCameraCoordinateFrame->setTransformation
   (
     mArm->getRootCoordinateFrame()->getTransformation()
     * mArm->getProductOfExponentials(mMountingJoint->getValue())
     * mReferenceCameraTransformation
   );
+  cv::Mat external = mCameraCoordinateFrame->getTransformation().inv();
 
-  // extract relevant vectors and transform to QGLViewer format
-  cv::Mat camera_rotation = mCameraCoordinateFrame->getRotation();
+  // calculate internal transformation
+  cv::Mat internal = cv::Mat::zeros(3, 3, CV_64FC1);
+  internal.at<double>(0, 0) = 1000;
+  internal.at<double>(1, 1) = 1000;
+  internal.at<double>(0, 2) = 320;
+  internal.at<double>(1, 2) = 240;
+  internal.at<double>(2, 2) = 1.0;
 
-  qglviewer::Vec new_position
-  (
-    mCameraCoordinateFrame->getTranslationX(),
-    mCameraCoordinateFrame->getTranslationY(),
-    mCameraCoordinateFrame->getTranslationZ()
-  );
-  qglviewer::Vec new_up_vector
-  (
-    camera_rotation.at<double>(0, 1), camera_rotation.at<double>(1, 1), camera_rotation.at<double>(2, 1)
-  );
-  qglviewer::Vec new_view_direction
-  (
-    camera_rotation.at<double>(0, 2), camera_rotation.at<double>(1, 2), camera_rotation.at<double>(2, 2)
-  );
+  // projection
+  cv::Mat projection = cv::Mat::eye(3, 4, CV_64FC1);
+
+
+  cv::Mat projection_matrix_double = internal * projection * external;
+  cv::Mat projection_matrix_float;
+  projection_matrix_double.convertTo(projection_matrix_float, CV_32FC1);
+
+  cedar::aux::math::write(external);
+  cedar::aux::math::write(projection);
+  cedar::aux::math::write(internal);
+  cedar::aux::math::write(projection_matrix_double);
+  cedar::aux::math::write(projection_matrix_float);
+
+  std::cout << "-----------------------------------------------------------" << std::endl;
+
+
+//  // extract relevant vectors and transform to QGLViewer format
+//  cv::Mat camera_rotation = mCameraCoordinateFrame->getRotation();
+//
+//  qglviewer::Vec new_position
+//  (
+//    mCameraCoordinateFrame->getTranslationX(),
+//    mCameraCoordinateFrame->getTranslationY(),
+//    mCameraCoordinateFrame->getTranslationZ()
+//  );
+//  qglviewer::Vec new_up_vector
+//  (
+//    camera_rotation.at<double>(0, 1), camera_rotation.at<double>(1, 1), camera_rotation.at<double>(2, 1)
+//  );
+//  qglviewer::Vec new_view_direction
+//  (
+//    camera_rotation.at<double>(0, 2), camera_rotation.at<double>(1, 2), camera_rotation.at<double>(2, 2)
+//  );
 
   // export these to the camera in the viewer
   qglviewer::Camera* viewer_camera = this->camera();
-  viewer_camera->setPosition(new_position);
-  viewer_camera->setUpVector(new_up_vector);
-  viewer_camera->setViewDirection(new_view_direction);
+//  viewer_camera->setPosition(new_position);
+//  viewer_camera->setUpVector(new_up_vector);
+//  viewer_camera->setViewDirection(new_view_direction);
+
+  viewer_camera->setFromProjectionMatrix(projection_matrix_float.ptr<float>());
 
   cedar::aux::gui::Viewer::draw();
 }
