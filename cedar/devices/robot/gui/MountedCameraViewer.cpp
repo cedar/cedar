dbdd8b2baca026825b6a8d9859cf1daafeb541b1
Hendrik Reimann <hendrik.reimann@ini.rub.de>
2012-06-28 11:35:39 +0200

    MountedCameraViewer is working properly

--- cedar/devices/robot/gui/MountedCameraViewer.cpp
+++ cedar/devices/robot/gui/MountedCameraViewer.cpp
@@ -53,13 +53,33 @@ cedar::dev::robot::gui::MountedCameraViewer::MountedCameraViewer
 cedar::aux::gui::Viewer(pScene),
 mArm(chain),
 mCameraCoordinateFrame(new cedar::aux::LocalCoordinateFrame()),
-mMountingJoint
+_mMountingJoint
 (
   new cedar::aux::IntParameter(this, "mounting joint", mArm->getNumberOfJoints()-1, 0, mArm->getNumberOfJoints()-1)
-)
+),
+_mImageWidth(new cedar::aux::IntParameter(this, "image width", 640, 0, INT_MAX)),
+_mImageHeight(new cedar::aux::IntParameter(this, "image height", 480, 0, INT_MAX)),
+mCalibrationMatrix(3, 3, CV_64FC1),
+mProjection(3, 4, CV_64FC1)
 {
+  std::vector<double> calibration_matrix_default(9, 0.0);
+  calibration_matrix_default[0] = 1000.0;
+  calibration_matrix_default[4] = 1000.0;
+  calibration_matrix_default[2] = 320.0;
+  calibration_matrix_default[5] = 240.0;
+  calibration_matrix_default[8] = 1.0;
+  _mCalibrationMatrix = cedar::aux::DoubleVectorParameterPtr
+  (
+    new cedar::aux::DoubleVectorParameter(this, "calibration matrix", calibration_matrix_default)
+  );
+  _mCalibrationMatrix->makeDefault();
+  mProjection = cv::Mat::eye(3, 4, CV_64FC1);
+
   this->addConfigurableChild("coordinate frame", mCameraCoordinateFrame);
   this->showEntireScene();
+
+  // widget size
+  this->setFixedSize(640, 480);
 }
 
 cedar::dev::robot::gui::MountedCameraViewer::~MountedCameraViewer()
@@ -85,7 +105,20 @@ void cedar::dev::robot::gui::MountedCameraViewer::readConfiguration(const cedar:
 {
   cedar::aux::Configurable::readConfiguration(node);
   mReferenceCameraTransformation = mCameraCoordinateFrame->getTransformation();
-  cedar::aux::math::write(mReferenceCameraTransformation);
+
+  CEDAR_ASSERT(_mCalibrationMatrix->size() >=9);
+  mCalibrationMatrix.at<double>(0, 0) = _mCalibrationMatrix->at(0);
+  mCalibrationMatrix.at<double>(0, 1) = _mCalibrationMatrix->at(1);
+  mCalibrationMatrix.at<double>(0, 2) = _mCalibrationMatrix->at(2);
+  mCalibrationMatrix.at<double>(1, 0) = _mCalibrationMatrix->at(3);
+  mCalibrationMatrix.at<double>(1, 1) = _mCalibrationMatrix->at(4);
+  mCalibrationMatrix.at<double>(1, 2) = _mCalibrationMatrix->at(5);
+  mCalibrationMatrix.at<double>(2, 0) = _mCalibrationMatrix->at(6);
+  mCalibrationMatrix.at<double>(2, 1) = _mCalibrationMatrix->at(7);
+  mCalibrationMatrix.at<double>(2, 2) = _mCalibrationMatrix->at(8);
+
+  // widget size
+  this->setFixedSize(_mImageWidth->getValue(), _mImageHeight->getValue());
 }
 
 void cedar::dev::robot::gui::MountedCameraViewer::draw()
@@ -94,60 +127,18 @@ void cedar::dev::robot::gui::MountedCameraViewer::draw()
   mCameraCoordinateFrame->setTransformation
   (
     mArm->getRootCoordinateFrame()->getTransformation()
-    * mArm->getProductOfExponentials(mMountingJoint->getValue())
+    * mArm->getProductOfExponentials(_mMountingJoint->getValue())
     * mReferenceCameraTransformation
   );
   cv::Mat external = mCameraCoordinateFrame->getTransformation().inv();
 
-  // calculate internal transformation
-  cv::Mat internal = cv::Mat::zeros(3, 3, CV_64FC1);
-  internal.at<double>(0, 0) = 1000;
-  internal.at<double>(1, 1) = 1000;
-  internal.at<double>(0, 2) = 320;
-  internal.at<double>(1, 2) = 240;
-  internal.at<double>(2, 2) = 1.0;
-
   // projection
-  cv::Mat projection = cv::Mat::eye(3, 4, CV_64FC1);
-
-
-  cv::Mat projection_matrix_double = internal * projection * external;
+  cv::Mat projection_matrix_double = mCalibrationMatrix * mProjection * external;
   cv::Mat projection_matrix_float;
   projection_matrix_double.convertTo(projection_matrix_float, CV_32FC1);
 
-  cedar::aux::math::write(external);
-  cedar::aux::math::write(projection);
-  cedar::aux::math::write(internal);
-  cedar::aux::math::write(projection_matrix_double);
-  cedar::aux::math::write(projection_matrix_float);
-
-  std::cout << "-----------------------------------------------------------" << std::endl;
-
-
-//  // extract relevant vectors and transform to QGLViewer format
-//  cv::Mat camera_rotation = mCameraCoordinateFrame->getRotation();
-//
-//  qglviewer::Vec new_position
-//  (
-//    mCameraCoordinateFrame->getTranslationX(),
-//    mCameraCoordinateFrame->getTranslationY(),
-//    mCameraCoordinateFrame->getTranslationZ()
-//  );
-//  qglviewer::Vec new_up_vector
-//  (
-//    camera_rotation.at<double>(0, 1), camera_rotation.at<double>(1, 1), camera_rotation.at<double>(2, 1)
-//  );
-//  qglviewer::Vec new_view_direction
-//  (
-//    camera_rotation.at<double>(0, 2), camera_rotation.at<double>(1, 2), camera_rotation.at<double>(2, 2)
-//  );
-
   // export these to the camera in the viewer
   qglviewer::Camera* viewer_camera = this->camera();
-//  viewer_camera->setPosition(new_position);
-//  viewer_camera->setUpVector(new_up_vector);
-//  viewer_camera->setViewDirection(new_view_direction);
-
   viewer_camera->setFromProjectionMatrix(projection_matrix_float.ptr<float>());
 
   cedar::aux::gui::Viewer::draw();
