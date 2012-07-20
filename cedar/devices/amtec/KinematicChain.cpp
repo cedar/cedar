ed135faf5cdc7a0e102180648ab4883847b7afbf
StephanZibner <StephanZibner@localhost>
2012-07-20 10:24:46 +0200

    fixed issue #300 in testing - renaming KukaInterface to kuka::KinematicChain

---
+++ cedar/devices/amtec/KinematicChain.cpp
@@ -0,0 +1,465 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        KinematicChain.cpp
+
+    Maintainer:  Hendrik Reimann
+    Email:       hendrik.reimann@ini.rub.de
+    Date:        2011 01 18
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+#ifdef CEDAR_USE_AMTEC
+
+// CEDAR INCLUDES
+#include "cedar/devices/amtec/KinematicChain.h"
+#include "cedar/auxiliaries/exceptions.h"
+#include "cedar/auxiliaries/math/LimitsParameter.h"
+
+// SYSTEM INCLUDES
+#ifdef CEDAR_OS_LINUX
+  #ifndef __LINUX__
+    #define __LINUX__
+  #endif // __LINUX__
+#endif // CEDAR_OS_LINUX
+
+#include "AmtecDeviceDriver/m5apiw32.h"
+
+#ifdef __LINUX__
+  #undef __LINUX__
+#endif // __LINUX__
+
+#include <QMutexLocker>
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+cedar::dev::amtec::KinematicChain::KinematicChain()
+:
+cedar::dev::robot::KinematicChain(),
+_mInitString(new cedar::aux::StringParameter(this, "amtec init string", "ESD:1,1000")),
+_mModuleMap
+(
+  new cedar::aux::IntVectorParameter
+  (
+    this, "amtec module map", 1, 0, 0, std::numeric_limits<int>::max()
+  )
+)
+{
+  mpDevice = NULL;
+
+  // todo: this cannot be called from the constructor any more, because the number of joints is not known yet
+  // current solution is to make it initDevice() public and call it from the main program after readJson() was called
+  // a restructuring of this system might be in order
+//  if(!initDevice())
+//  {
+//    std::cout << "Error initializing the Amtec module!" << std::endl;
+//    CEDAR_THROW(cedar::aux::InitializationException, "Error initializing the Amtec module!");
+//  }
+}
+
+cedar::dev::amtec::KinematicChain::~KinematicChain()
+{
+  for(unsigned int i = 0; i < getNumberOfJoints(); ++i)
+  {
+    this->setJointVelocity(i, 0.0);
+  }
+
+  if(mpDevice)
+  {
+    delete mpDevice;
+  }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+bool cedar::dev::amtec::KinematicChain::isMovable() const
+{
+  // todo: change this to something meaningful
+  return true;
+}
+
+bool cedar::dev::amtec::KinematicChain::initDevice()
+{
+  QMutexLocker mutex_locker(&mCanBusMutex);
+
+  if(!mpDevice)
+  {
+    mpDevice = newDevice(this->getInitString().c_str());
+  }
+
+  //
+  // init device itself
+  //
+
+  int ret_val = mpDevice->init(this->getInitString().c_str());
+
+  //!@todo Use the log interface instead of writing to cout.
+  switch(ret_val)
+  {
+  case CLD_OK:
+    break;
+  case CLDERR_INITIALIZATIONERROR:
+    std::cout << "Amtec Device: Initialization error!" << std::endl;
+    return false;
+    break;
+  default:
+    std::cout << "Amtec Device: Unknown initialization error!" << std::endl;
+    return false;
+  }
+
+  //
+  // get default module map
+  //
+  std::vector<int> modules;
+  mpDevice->getModuleIdMap(modules);
+
+  if(modules.size() <= 0)
+  {
+    std::cout << "No Amtec modules found!" << std::endl;
+    return false;
+  }
+
+  if(modules.size() != getNumberOfJoints())
+  {
+    std::cout << "Found " << modules.size() << " Amtec modules but "
+      << getNumberOfJoints()
+      << " are specified by configuration file!" << std::endl;
+    return false;
+  }
+
+  //
+  // read module map from config
+  //
+
+//  if(addParameter(&mModules, "amtecModuleMap", mModules) != CONFIG_SUCCESS)
+//  {
+//    std::cout << "KinematicChain: Error reading 'amtecModuleMap' from config file!" << std::endl;
+//  }
+//
+//  readOrDefaultConfiguration();
+
+  // print module mapping to console
+  std::cout << "Mapping of joints to modules:" << std::endl;
+  std::cout << "amtec module map = [ " << _mModuleMap->at(0);
+  for (unsigned int i = 1; i < _mModuleMap->size(); ++i)
+  {
+    std::cout << ", " << _mModuleMap->at(i);
+  }
+  std::cout << " ];" << std::endl;
+
+  // calibrate and configure the modules
+  mutex_locker.unlock();
+
+  for(unsigned int i = 0; i < _mModuleMap->size(); ++i)
+  {
+
+    if(!isCalibrated(_mModuleMap->at(i)))
+    {
+
+      if(!calibrateModule(_mModuleMap->at(i)))
+      {
+        return false;
+      }
+
+    }
+
+    // set position limits
+    //!@todo: replace this with applyJointLimits oder something
+    mpDevice->setMinPos(_mModuleMap->at(i), getJoint(i)->_mpAngleLimits->getLowerLimit());
+    mpDevice->setMaxPos(_mModuleMap->at(i), getJoint(i)->_mpAngleLimits->getUpperLimit());
+
+    // set velocity limits
+    //float min_velocity = mpReferenceGeometry->getJoint(i)->velocityLimits.min;
+    //float max_velocity = mpReferenceGeometry->getJoint(i)->velocityLimits.max;
+  }
+
+  return true;
+}
+
+
+double cedar::dev::amtec::KinematicChain::getJointAngle(unsigned int joint) const
+{
+  QMutexLocker mutex_locker(&mCanBusMutex);
+
+  if(!mpDevice)
+  {
+    std::cout << "Error: No Amtec device!" << std::endl;
+    return 0.0;
+  }
+
+  if(joint >= _mModuleMap->size())
+  {
+    std::cout << "Error: Trying to access the " << joint << ". module while only "
+      << _mModuleMap->size() << " were found." << std::endl;
+    return 0.0;
+  }
+
+  int module = _mModuleMap->at(joint);
+  float position = 0.0f;
+
+  mpDevice->getPos(module, &position);
+
+  return position;
+}
+
+
+double cedar::dev::amtec::KinematicChain::getJointVelocity(unsigned int joint) const
+{
+  QMutexLocker mutex_locker(&mCanBusMutex);
+
+  if(!mpDevice)
+  {
+    std::cout << "Error: No Amtec device!" << std::endl;
+    return 0.0;
+  }
+
+  if(joint >= _mModuleMap->size())
+  {
+    std::cout << "Error: Trying to access the " << joint << ". module while only "
+      << _mModuleMap->size() << " were found." << std::endl;
+    return 0.0;
+  }
+
+  int module = _mModuleMap->at(joint);
+  float velocity = 0.0f;
+
+  mpDevice->getVel(module, &velocity);
+
+  return velocity;
+}
+
+
+void cedar::dev::amtec::KinematicChain::setJointAngle(unsigned int index, double value)
+{
+  QMutexLocker mutex_locker(&mCanBusMutex);
+
+  if(!mpDevice)
+  {
+    std::cout << "Error: No Amtec device!" << std::endl;
+    return;
+  }
+
+  if(index >= _mModuleMap->size())
+  {
+    std::cout << "Error: Trying to access the " << index << ". module while only "
+      << _mModuleMap->size() << " were found." << std::endl;
+    return;
+  }
+
+  int module = _mModuleMap->at(index);
+  mpDevice->moveRamp(module, value, getJoint(index)->_mpVelocityLimits->getUpperLimit(), M_2_PI);
+
+  return;
+}
+
+
+bool cedar::dev::amtec::KinematicChain::setJointVelocity(unsigned int index, double velocity)
+{
+  QMutexLocker mutex_locker(&mCanBusMutex);
+
+  if(!mpDevice)
+  {
+    std::cout << "Error: No Amtec device!" << std::endl;
+    return true;
+  }
+
+  if(index >= _mModuleMap->size())
+  {
+    std::cout << "Error: Trying to access the " << index << ". module while only "
+      << _mModuleMap->size() << " were found." << std::endl;
+    return true;
+  }
+
+  velocity = std::max<double>(velocity, getJoint(index)->_mpVelocityLimits->getLowerLimit());
+  velocity = std::min<double>(velocity, getJoint(index)->_mpVelocityLimits->getUpperLimit());
+
+  int module = _mModuleMap->at(index);
+  mpDevice->moveVel(module, velocity);
+
+  return true;
+}
+
+
+bool cedar::dev::amtec::KinematicChain::calibrateModule(unsigned int module)
+{
+  QMutexLocker mutex_locker(&mCanBusMutex);
+
+  if(!mpDevice)
+  {
+    std::cout << "Trying to calibrate but no device initialized. This should never happen!" << std::endl;
+    return false;
+  }
+
+  // halt the module
+  int ret_val = mpDevice->haltModule(module);
+
+  if(ret_val != CLD_OK)
+  {
+    std::cout << "Error halting Amtec module " << module << ": " << ret_val << std::endl;
+    return false;
+  }
+
+  // homing the module
+  ret_val = mpDevice->homeModule(module);
+
+  if(ret_val != CLD_OK)
+  {
+    std::cout << "Error homing Amtec module " << module << ": " << ret_val << std::endl;
+    return false;
+  }
+
+  // wait until its home
+  unsigned long state = 0;
+
+  do
+  {
+    ret_val = mpDevice->getModuleState(module, &state);
+
+    if(ret_val != CLD_OK)
+    {
+      std::cout << "Error homing Amtec module " << module << ": " << ret_val << std::endl;
+      return false;
+    }
+  }
+  while(!(state & STATEID_MOD_HOME));
+
+  // resetting the module
+  ret_val = mpDevice->resetModule(module);
+
+  if(ret_val != CLD_OK)
+  {
+    std::cout << "Error resetting Amtec module " << module << ": " << ret_val << std::endl;
+    return false;
+  }
+
+  return true;
+}
+
+
+bool cedar::dev::amtec::KinematicChain::isCalibrated(unsigned int module)
+{
+  QMutexLocker mutex_locker(&mCanBusMutex);
+
+  if(!mpDevice)
+  {
+    std::cout << "Trying to read calibration state but no device initialized!" << std::endl;
+    return false;
+  }
+
+  // read module state
+
+  unsigned long state = 0;
+
+  int ret_val = mpDevice->getModuleState(module, &state);
+
+  if(ret_val != CLD_OK)
+  {
+    std::cout << "Error reading state of Amtec module " << module << ": " << ret_val << std::endl;
+    return false;
+  }
+
+  bool is_home = (state & STATEID_MOD_HOME);
+
+  return is_home;
+}
+
+
+float cedar::dev::amtec::KinematicChain::getMaxAcceleration(unsigned int index)
+{
+  QMutexLocker mutex_locker(&mCanBusMutex);
+
+  if(!mpDevice)
+  {
+    std::cout << "Trying to read max acceleration but no device initialized!" << std::endl;
+    return 0.0;
+  }
+
+  if(index >= _mModuleMap->size())
+  {
+    std::cout << "Error: Trying to access the " << index << ". module while only "
+      << _mModuleMap->size() << " were found." << std::endl;
+    return 0.0;
+  }
+
+  float acc = 0.0;
+  mpDevice->getMaxAcc(_mModuleMap->at(index), &acc);
+
+  return acc;
+}
+
+
+void cedar::dev::amtec::KinematicChain::setMaxAcceleration(unsigned int index, float maxAcc)
+{
+  QMutexLocker mutex_locker(&mCanBusMutex);
+
+  if(!mpDevice)
+  {
+    std::cout << "Trying to set max acceleration but no device initialized!" << std::endl;
+    return;
+  }
+
+  if(index >= _mModuleMap->size())
+  {
+    std::cout << "Error: Trying to access the " << index << ". module while only "
+      << _mModuleMap->size() << " were found." << std::endl;
+    return;
+  }
+
+  mpDevice->setMaxAcc(_mModuleMap->at(index), maxAcc);
+  return;
+}
+
+void cedar::dev::amtec::KinematicChain::setJointAngle(unsigned int index, double value, double stepTime)
+{
+  QMutexLocker mutex_locker(&mCanBusMutex);
+
+  if(!mpDevice)
+  {
+    std::cout << "Error: No Amtec device!" << std::endl;
+    return;
+  }
+
+  if(index >= _mModuleMap->size())
+  {
+    std::cout << "Error: Trying to access the " << index << ". module while only "
+        << _mModuleMap->size() << " were found." << std::endl;
+    return;
+  }
+
+  int module = _mModuleMap->at(index);
+  mpDevice->moveStep(module, value, stepTime);
+}
+
+#endif // CEDAR_USE_AMTEC
---
+++ cedar/processing/sources/Video.cpp
@@ -0,0 +1,228 @@
+/*======================================================================================================================
+
+    Copyright 2011 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        Video.cpp
+
+    Maintainer:  Georg Hartinger
+    Email:       georg.hartinger@ini.ruhr-uni-bochum.d
+    Date:        2012 04 20
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include "cedar/processing/sources/Video.h"
+#include "cedar/processing/ElementDeclaration.h"
+#include "cedar/processing/DeclarationRegistry.h"
+#include "cedar/processing/StepTime.h"
+
+// SYSTEM INCLUDES
+
+//----------------------------------------------------------------------------------------------------------------------
+// register the class
+//----------------------------------------------------------------------------------------------------------------------
+namespace
+{
+  bool declare()
+  {
+    using cedar::proc::ElementDeclarationPtr;
+    using cedar::proc::ElementDeclarationTemplate;
+
+    ElementDeclarationPtr declaration
+    (
+      new ElementDeclarationTemplate<cedar::proc::sources::Video>
+      (
+        "Sources",
+        "cedar.processing.sources.Video"
+      )
+    );
+    declaration->setIconPath(":/steps/video_grabber.svg");
+    declaration->setDescription
+    (
+      "Reads a video file and outputs the images. Supported formats depend on which ones are available via opencv."
+    );
+    cedar::proc::DeclarationRegistrySingleton::getInstance()->declareClass(declaration);
+
+    return true;
+  }
+
+  bool declared = declare();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+cedar::proc::sources::Video::Video()
+:
+cedar::proc::sources::GrabberBase(),
+mFrameDuration(0.0),
+mTimeElapsed(0.0),
+_mFileName(new cedar::aux::FileParameter(this, "videofile",cedar::aux::FileParameter::READ,"")),
+_mLoop(new cedar::aux::BoolParameter(this, "loop", true))
+{
+  //default config-filename
+  GrabberBase::_mConfigurationFileName->setValue("./videograbber.cfg");
+
+  //default-filenames
+  _mFileName->setValue("./video.avi");
+  mGrabber.reset();
+
+  this->declareOutput("video", mImage);
+  QObject::connect(_mFileName.get(), SIGNAL(valueChanged()), this, SLOT(setFileName()));
+  QObject::connect(_mLoop.get(), SIGNAL(valueChanged()), this, SLOT(setLoop()));
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+void cedar::proc::sources::Video::onStart()
+{
+  cedar::aux::LogSingleton::getInstance()->debugMessage("start grabber","cedar::proc::sources::Video::onStart()");
+  std::string filename = this->_mFileName->getPath();
+
+  // check if videofile is there
+  if (filename == "")
+  {
+    std::string message = this->mGrabber->getName()+ ": There is no file to grab from! Please set one!";
+    cedar::aux::LogSingleton::getInstance()->warning(message, "cedar::proc::sources::Video::onStart()");
+    // std::cout << "[cedar::proc::sources::Video::onStart()]" << message << std::endl;
+  }
+
+  // if there is no grabber instance, create one
+  else if (!mGrabber)
+  {
+    // std::cout << "\n\n[cedar::proc::sources::Video::onStart()] try to create a new grabber" << std::endl;
+    this->createGrabber();
+  }
+
+  // if grabber already grabbing, do nothing
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+void cedar::proc::sources::Video::reset()
+{
+  // Rewind the video
+  if (mGrabber)
+  {
+    this->getGrabber()->setPositionAbsolute(0);
+  }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void cedar::proc::sources::Video::compute(const cedar::proc::Arguments &arguments)
+{
+  if (mGrabber)
+  {
+    // grab a new frame only if FrameDuration is larger than fps of the video
+    try
+    {
+      const cedar::proc::StepTime& step_time = dynamic_cast<const cedar::proc::StepTime&>(arguments);
+      const cedar::unit::Time& t = step_time.getStepTime();
+      cedar::unit::Milliseconds step_time_ms(t);
+      mTimeElapsed += step_time_ms;
+    }
+    catch (const std::bad_cast& e)
+    {
+      CEDAR_THROW(cedar::proc::InvalidArgumentsException, "Bad arguments passed to dynamics. Expected StepTime.");
+    }
+
+    if (mTimeElapsed > mFrameDuration)
+    {
+      this->mGrabber->grab();
+
+      // get new frame
+      cv::Mat frame = this->mGrabber->getImage();
+
+      // set only if it isn't empty (could be, if videofile is over)
+      if (!frame.empty())
+      {
+        this->mImage->setData(frame.clone());
+      }
+      mTimeElapsed = 0.0;
+      //!@todo This should only happen once when the grabber is created/first frame is read.
+      this->annotateImage();
+    }
+  }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void cedar::proc::sources::Video::setFileName()
+{
+  std::string message = "Set new filename to \"" + this->_mFileName->getPath()+"\"";
+
+  // if already grabbing, a new grabber have to be created
+  // because there is no possibility to change the filename
+  if (mGrabber)
+  {
+    this->createGrabber();
+    message = this->mGrabber->getName() + ": " + message;
+  }
+  cedar::aux::LogSingleton::getInstance()->message(message,"cedar::proc::sources::Video::setFileName()");
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+void cedar::proc::sources::Video::setLoop()
+{
+  if (mGrabber)
+  {
+    this->getGrabber()->setLooped(this->_mLoop->getValue());
+  }
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+void cedar::proc::sources::Video::onCreateGrabber()
+{
+  std::string filename = this->_mFileName->getPath();
+
+  cedar::dev::sensors::visual::VideoGrabberPtr grabber;
+  grabber = cedar::dev::sensors::visual::VideoGrabberPtr
+            (
+               new cedar::dev::sensors::visual::VideoGrabber(this->_mConfigurationFileName->getPath(),filename)
+            );
+  const std::string message2= "New grabber created";
+  cedar::aux::LogSingleton::getInstance()->debugMessage(message2,"cedar::proc::sources::Video::createGrabber()");
+
+  // the new grabber created without exception, so we can use it
+  GrabberBase::mGrabber = grabber;
+
+  // calcualte the frameduration in ms
+  double fps = this->getGrabber()->getSourceFps(0);
+  mFrameDuration = static_cast<cedar::unit::Milliseconds>(1000.0 / fps);
+
+  // set the loop
+  this->getGrabber()->setLooped(this->_mLoop->getValue());
+
+}
