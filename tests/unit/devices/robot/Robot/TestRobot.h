59fbe5c47465f527568ce3d803446b47ea9a4b54
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2012-10-31 11:49:06 +0100

    Fixed a problem with the version detection of CMake in the unit test generation.

---
+++ tests/unit/devices/robot/Robot/TestRobot.h
@@ -0,0 +1,114 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        TestRobot.h
+
+    Maintainer:  Mathis Richter
+    Email:       mathis.richter@ini.rub.de
+    Date:        2010 11 08
+
+    Description: Header for the @em TestRobot class.
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_TESTS_UNIT_DEV_ROBOT_ROBOT_TEST_ROBOT_H
+#define CEDAR_TESTS_UNIT_DEV_ROBOT_ROBOT_TEST_ROBOT_H
+
+// LOCAL INCLUDES
+#include "unit/devices/robot/Robot/namespace.h"
+
+// PROJECT INCLUDES
+#include "cedar/devices/robot/Robot.h"
+#include "cedar/devices/robot/Component.h"
+
+// SYSTEM INCLUDES
+#include <string>
+
+
+/*!@brief Abstract description of the class.
+ *
+ * More detailed description of the class.
+ */
+class cedar::tests::unit::dev::robot::Robot::TestRobot : public cedar::dev::robot::Robot
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // macros
+  //--------------------------------------------------------------------------------------------------------------------
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief Constructor that gets a configuration file name.
+  TestRobot(void);
+
+  //!@brief Destructor
+  virtual ~TestRobot(void);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  virtual cedar::dev::robot::ComponentPtr createComponent(const std::string& rComponentName);
+
+protected:
+  // none yet
+private:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet (hopefully never!)
+protected:
+  // none yet
+
+private:
+  // none yet
+
+}; // class cedar::tests::unit::aux::dev::robot::Robot::TestRobot
+
+#endif // CEDAR_TESTS_UNIT_DEV_ROBOT_ROBOT_TEST_ROBOT_H
+
