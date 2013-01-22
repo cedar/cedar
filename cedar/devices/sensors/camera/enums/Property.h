675b9abee22f0127a20ca66cfc142240e967b419
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2013-01-22 22:16:58 +0100

    Fixed some compiler errors & warnings for MSVC.

--- cedar/devices/sensors/camera/enums/Property.h
+++ cedar/devices/sensors/camera/enums/Property.h
@@ -54,6 +54,35 @@
 #define CV_CAP_PROP_FOCUS 28
 #define CV_CAP_PROP_GUID 29
 
+// opencv 2.2 compatibility:
+#ifndef CV_CAP_PROP_WHITE_BALANCE_BLUE_U
+  #define CV_CAP_PROP_WHITE_BALANCE_BLUE_U 17
+#endif // CV_CAP_PROP_WHITE_BALANCE_BLUE_U
+
+#ifndef CV_CAP_PROP_SHARPNESS
+  #define CV_CAP_PROP_SHARPNESS 20
+#endif // CV_CAP_PROP_SHARPNESS
+
+#ifndef CV_CAP_PROP_AUTO_EXPOSURE
+  #define CV_CAP_PROP_AUTO_EXPOSURE 21
+#endif // CV_CAP_PROP_AUTO_EXPOSURE
+
+#ifndef CV_CAP_PROP_GAMMA
+  #define CV_CAP_PROP_GAMMA 22
+#endif // CV_CAP_PROP_GAMMA
+
+#ifndef CV_CAP_PROP_TEMPERATURE
+  #define CV_CAP_PROP_TEMPERATURE 24
+#endif // CV_CAP_PROP_TEMPERATURE
+
+#ifndef CV_CAP_PROP_WHITE_BALANCE_RED_V
+  #define CV_CAP_PROP_WHITE_BALANCE_RED_V 26
+#endif // CV_CAP_PROP_WHITE_BALANCE_RED_V
+
+#ifndef CV_CAP_PROP_DC1394_MODE_AUTO
+  #define CV_CAP_PROP_DC1394_MODE_AUTO -2
+#endif // CV_CAP_PROP_DC1394_MODE_AUTO
+
 //return value, if a property is not supported from the actual device
 //have to be larger than the highest value of the properties!
 #define CAMERA_PROPERTY_NOT_SUPPORTED 1000
