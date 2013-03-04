c7bf529bd0880b8eeece47a2f7e112a6a330af6a
StephanZibner <StephanZibner@localhost>
2013-03-04 17:46:12 +0100

    added widgets for several unit parameters

---
+++ cedar/devices/gl/Caren.cpp
@@ -0,0 +1,148 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        Caren.cpp
+
+    Maintainer:  Hendrik Reimann
+    Email:       hendrik.reimann@ini.rub.de
+    Date:        2010 12 14
+
+    Description: implementation for a class visualizing the head of CoRA
+
+    Credits:
+
+======================================================================================================================*/
+
+#define NOMINMAX // to avoid Windows issues
+
+// CEDAR INCLUDES
+#include "cedar/devices/gl/Caren.h"
+#include "cedar/devices/gl/KukaArm.h"
+#include "cedar/devices/gl/Sdh.h"
+#include "cedar/auxiliaries/gl/gl.h"
+#include "cedar/auxiliaries/gl/drawShapes.h"
+
+// SYSTEM INCLUDES
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+cedar::dev::gl::Caren::Caren
+(
+  cedar::dev::KinematicChainPtr trunk,
+  cedar::dev::KinematicChainPtr arm,
+  cedar::dev::KinematicChainPtr head,
+  cedar::dev::KinematicChainPtr palm,
+  cedar::dev::KinematicChainPtr fingerOne,
+  cedar::dev::KinematicChainPtr fingerTwo,
+  cedar::dev::KinematicChainPtr fingerThree
+)
+:
+cedar::aux::gl::ObjectVisualization(trunk->getRootCoordinateFrame()),
+mTrunk(trunk),
+mArm(arm),
+mHead(head),
+mPalm(palm),
+mFingerOne(fingerOne),
+mFingerTwo(fingerTwo),
+mFingerThree(fingerThree),
+mArmVisualization(new cedar::dev::gl::KukaArm(arm)),
+mHandVisualization(new cedar::dev::gl::Sdh(fingerOne, fingerTwo, fingerThree, palm))
+{
+
+}
+
+cedar::dev::gl::Caren::~Caren()
+{
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+void cedar::dev::gl::Caren::initializeGl()
+{
+  mArmVisualization->initializeGl();
+  mHandVisualization->initializeGl();
+}
+
+void cedar::dev::gl::Caren::draw()
+{
+//  drawBase();
+  mArmVisualization->draw();
+  mHandVisualization->draw();
+}
+
+void cedar::dev::gl::Caren::drawBase()
+{
+  // move to origin
+  glPopMatrix();
+  glPushMatrix();
+
+  // move to object coordinates
+  mTransformationTranspose = mTrunk->getRootTransformation().t();
+  glMultMatrixd((GLdouble*)mTransformationTranspose.data);
+
+//  setMaterial(CHROME);
+  glTranslated(-.090 - .025, 0, .010 + .070);
+  cedar::aux::gl::drawBlock(.050, .050, .140, mIsDrawnAsWireFrame); // this is the aluminum block
+//  setMaterial(DARK_BLUE_METAL);
+  glTranslated(-.025 - .010, 0, 0);
+  cedar::aux::gl::drawBlock(.020, .050, .140, mIsDrawnAsWireFrame); // this is the small blue block
+  glTranslated(-.010 - .025, 0, .1625);
+  cedar::aux::gl::drawBlock(.050, .050, .465, mIsDrawnAsWireFrame); // this is the tall pole
+//  setMaterial(CHROME);
+  glTranslated(0, 0, .2325 + .004);
+  cedar::aux::gl::drawBlock(.050, .050, .008, mIsDrawnAsWireFrame); // now comes the aluminum connector
+  glTranslated(0, 0, .004 + .013);
+  glRotated(45, 0, 0, 1);
+  cedar::aux::gl::drawBlock(.031, .031, .026, mIsDrawnAsWireFrame);
+  glRotated(-45, 0, 0, 1);
+  glTranslated(0, 0, .013 + .023);
+  cedar::aux::gl::drawBlock(.050, .050, .046, mIsDrawnAsWireFrame);
+  glTranslated(-.025 + .1175, 0, .023 + .005);
+  cedar::aux::gl::drawBlock(.235, .050, .010, mIsDrawnAsWireFrame); // this is the long horizontal piece
+//  setMaterial(NO_MATERIAL);
+
+}
+
+void cedar::dev::gl::Caren::drawCamera()
+{
+//  setMaterial(WHITE_PLASTIC);
+//  glRotated(90, 1, 0, 0);
+//  glTranslated(0, .020, -.005);
+//  cedar::aux::gl::drawBlock(.060, .110, .060, mIsDrawnAsWireFrame);
+//  glTranslated(0, .055, 0);
+//  setMaterial(BLACK_METAL);
+//  glRotated(-90, 1, 0, 0);
+//  cedar::aux::gl::drawCone(0, .003, .0185, .0185, mResolution, mIsDrawnAsWireFrame);
+//  cedar::aux::gl::drawCone(0, .003, .0225, .0225, mResolution, mIsDrawnAsWireFrame);
+//  glTranslated(0, 0, .0001);
+//  cedar::aux::gl::drawDisk(0, .0185, mResolution, mResolution, false, mIsDrawnAsWireFrame);
+//  glTranslated(0, 0, .0029);
+//  cedar::aux::gl::drawDisk(.0185, .0225, mResolution, mResolution, false, mIsDrawnAsWireFrame);
+//  setMaterial(NO_MATERIAL);
+}
---
+++ cedar/processing/Triggerable.cpp
@@ -0,0 +1,153 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        Triggerable.cpp
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2011 11 16
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+// CEDAR INCLUDES
+#include "cedar/processing/Triggerable.h"
+#include "cedar/processing/Trigger.h"
+#include "cedar/auxiliaries/assert.h"
+
+// SYSTEM INCLUDES
+#include <QReadLocker>
+#include <QWriteLocker>
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+cedar::proc::Triggerable::Triggerable(bool isLooped)
+:
+mIsLooped(isLooped),
+mState(cedar::proc::Triggerable::STATE_UNKNOWN)
+{
+}
+
+cedar::proc::Triggerable::~Triggerable()
+{
+  // empty default implementation
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+boost::signals2::connection cedar::proc::Triggerable::connectToStateChanged(boost::function<void ()> slot)
+{
+  return mStateChanged.connect(slot);
+}
+
+void cedar::proc::Triggerable::setParentTrigger(cedar::proc::TriggerPtr parent)
+{
+  if (this->isLooped())
+  {
+    // If there is already a parent trigger for looped steps, disconnect it first!
+    CEDAR_ASSERT(!parent || !this->mParentTrigger.lock());
+  }
+  this->mParentTrigger = parent;
+}
+
+cedar::proc::TriggerPtr cedar::proc::Triggerable::getParentTrigger()
+{
+  return this->mParentTrigger.lock();
+}
+
+void cedar::proc::Triggerable::callOnStart()
+{
+  this->onStart();
+  if (mFinished)
+  {
+    for (size_t i = 0; i < this->mFinished->getListeners().size(); ++i)
+    {
+      this->mFinished->getListeners().at(i)->callOnStart();
+    }
+  }
+}
+
+void cedar::proc::Triggerable::callOnStop()
+{
+  this->onStop();
+  this->setState(cedar::proc::Triggerable::STATE_UNKNOWN, "");
+  if (mFinished)
+  {
+    for (size_t i = 0; i < this->mFinished->getListeners().size(); ++i)
+    {
+      this->mFinished->getListeners().at(i)->callOnStop();
+    }
+  }
+}
+
+void cedar::proc::Triggerable::setState(cedar::proc::Triggerable::State newState, const std::string& annotation)
+{
+  // Only act if the state actually changes.
+  QWriteLocker locker(&mStateLock);
+  if (newState != this->mState || annotation != this->mStateAnnotation)
+  {
+    this->mState = newState;
+    this->mStateAnnotation = annotation;
+    locker.unlock();
+    mStateChanged();
+//    emit stateChanged();
+  }
+}
+
+cedar::proc::Triggerable::State cedar::proc::Triggerable::getState() const
+{
+  QReadLocker locker(&mStateLock);
+  return this->mState;
+}
+
+std::string cedar::proc::Triggerable::getStateAnnotation() const
+{
+  QReadLocker locker(&mStateLock);
+  return this->mStateAnnotation;
+}
+
+void cedar::proc::Triggerable::onStart()
+{
+  // empty as a default implementation
+}
+
+void cedar::proc::Triggerable::onStop()
+{
+  // empty as a default implementation
+}
+
+cedar::proc::TriggerPtr cedar::proc::Triggerable::getFinishedTrigger()
+{
+  if (!this->mFinished)
+  {
+    mFinished = cedar::proc::TriggerPtr(new cedar::proc::Trigger("processingDone"));
+  }
+  return this->mFinished;
+}
