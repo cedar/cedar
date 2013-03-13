40d6d53628531c77ba5136a01e24d0cf3f1ea215
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2013-03-13 14:25:04 +0100

    The intrusive pointer add/release ref functions are now accessible via argument dependent lookup. This makes the code compatible with older clang compilers.

--- cedar/auxiliaries/IntrusivePtrBase.h
+++ cedar/auxiliaries/IntrusivePtrBase.h
@@ -48,8 +48,14 @@
 #include <boost/intrusive_ptr.hpp>
 #include <boost/detail/atomic_count.hpp>
 
-extern CEDAR_AUX_LIB_EXPORT void intrusive_ptr_add_ref(cedar::aux::IntrusivePtrBase const *pObject);
-extern CEDAR_AUX_LIB_EXPORT void intrusive_ptr_release(cedar::aux::IntrusivePtrBase const *pObject);
+namespace cedar
+{
+  namespace aux
+  {
+    extern CEDAR_AUX_LIB_EXPORT void intrusive_ptr_add_ref(cedar::aux::IntrusivePtrBase const *pObject);
+    extern CEDAR_AUX_LIB_EXPORT void intrusive_ptr_release(cedar::aux::IntrusivePtrBase const *pObject);
+  }
+}
 
 /*!@brief A base class for any classes that make use of boost::intrusive_ptr.
  */
@@ -58,8 +64,8 @@ class cedar::aux::IntrusivePtrBase
   //--------------------------------------------------------------------------------------------------------------------
   // friends
   //--------------------------------------------------------------------------------------------------------------------
-  friend void ::intrusive_ptr_add_ref(cedar::aux::IntrusivePtrBase const *pObject);
-  friend void ::intrusive_ptr_release(cedar::aux::IntrusivePtrBase const *pObject);
+  friend void cedar::aux::intrusive_ptr_add_ref(cedar::aux::IntrusivePtrBase const *pObject);
+  friend void cedar::aux::intrusive_ptr_release(cedar::aux::IntrusivePtrBase const *pObject);
   //--------------------------------------------------------------------------------------------------------------------
   // constructors and destructor
   //--------------------------------------------------------------------------------------------------------------------
