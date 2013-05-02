cdf6264e22d9d9386667a2123169f82fe26f00e2
Mathis Richter <mathis.richter@ini.rub.de>
2013-05-02 17:21:56 +0200

    Ported the MatrixMultiply step from the ObjectRecognitionPlugin into cedar.

--- cedar/processing/steps/namespace.h
+++ cedar/processing/steps/namespace.h
@@ -61,6 +61,7 @@ namespace cedar
       CEDAR_DECLARE_PROC_CLASS(Convolution);
       CEDAR_DECLARE_PROC_CLASS(ColorConversion);
       CEDAR_DECLARE_PROC_CLASS(Flip);
+      CEDAR_DECLARE_PROC_CLASS(MatrixMultiply);
       CEDAR_DECLARE_PROC_CLASS(MatrixTypeConverter);
       CEDAR_DECLARE_PROC_CLASS(Projection);
       CEDAR_DECLARE_PROC_CLASS(Resize);
