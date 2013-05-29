63ddbd1adc20c4016fc7bda8c15f3b6a3925b436
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2013-05-29 12:49:46 +0200

    Columns in the configurable widget now stretch and items are expanded by default.

--- cedar/auxiliaries/gui/Configurable.cpp
+++ cedar/auxiliaries/gui/Configurable.cpp
@@ -127,6 +127,10 @@ cedar::aux::gui::Configurable::Configurable()
   QStringList header_labels;
   header_labels << "Property" << "Value";
   this->mpPropertyTree->setHeaderLabels(header_labels);
+
+  // make first section stretch
+  this->mpPropertyTree->header()->setResizeMode(0, QHeaderView::Stretch);
+  this->mpPropertyTree->header()->setResizeMode(1, QHeaderView::ResizeToContents);
 }
 
 
@@ -146,6 +150,7 @@ void cedar::aux::gui::Configurable::display(cedar::aux::ConfigurablePtr configur
   QTreeWidgetItem* p_item = new QTreeWidgetItem(text);
   this->mpPropertyTree->addTopLevelItem(p_item);
   this->mpPropertyTree->setFirstItemColumnSpanned(p_item, true);
+  p_item->setExpanded(true);
 
   this->append(configurable, p_item, std::string());
 }
@@ -178,6 +183,7 @@ void cedar::aux::gui::Configurable::append(cedar::aux::ConfigurablePtr configura
     pItem->addChild(child_item);
     this->mpPropertyTree->setFirstItemColumnSpanned(child_item, true);
     this->append(child, child_item, path);
+    child_item->setExpanded(true);
   }
 }
 
