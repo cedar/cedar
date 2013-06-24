c1fb78a6817d0edff72607df399751c6f18ef0a8
Kai Kuchenbecker <Kai.Kuchenbecker@ini.rub.de>
2013-06-24 10:16:29 +0200

    removed superfluous cedar_use_yarp

--- cedar/auxiliaries/net/detail/transport/collated/CollatedNetWriter.h
+++ cedar/auxiliaries/net/detail/transport/collated/CollatedNetWriter.h
@@ -43,9 +43,6 @@
 #ifndef CEDAR_COLLATED_NET_WRITER_H
 #define CEDAR_COLLATED_NET_WRITER_H
 
-#include "cedar/configuration.h"
-#ifdef CEDAR_USE_YARP
-
 // LOCAL INCLUDES
 #include "cedar/auxiliaries/net/detail/namespace.h"
 #include "cedar/auxiliaries/net/detail/transport/collated/CollatedNetBase.h"
@@ -150,7 +147,4 @@ public:
 
 } } } } // end namespaces
 
-#endif // CEDAR_USE_YARP
-
 #endif
-
