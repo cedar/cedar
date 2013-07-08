7b273e7fbae82322cf54425a81792fb543d291f7
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2013-07-08 16:20:49 +0200

    Made the margins on plot created windows by step items when no main window is present a bit smaller.

---
+++ cedar/processing/gui/Settings.h
@@ -0,0 +1,304 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        Settings.h
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2011 07 26
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_PROC_GUI_SETTINGS_H
+#define CEDAR_PROC_GUI_SETTINGS_H
+
+// CEDAR INCLUDES
+#include "cedar/processing/gui/namespace.h"
+#include "cedar/auxiliaries/BoolParameter.h"
+#include "cedar/auxiliaries/Configurable.h"
+#include "cedar/auxiliaries/EnumParameter.h"
+#include "cedar/auxiliaries/Enum.h"
+#include "cedar/auxiliaries/EnumType.h"
+#include "cedar/auxiliaries/namespace.h"
+
+// SYSTEM INCLUDES
+#include <QDockWidget>
+#include <set>
+
+
+/*!@brief All settings concerning the currently visible widgets of the ui: sizes, where they are docked and so on.
+ *
+ * More detailed description of the class.
+ * @todo management of plugins could be moved somewhere else
+ */
+class cedar::proc::gui::Settings : public cedar::aux::Configurable
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // friends
+  //--------------------------------------------------------------------------------------------------------------------
+  friend class cedar::proc::gui::UiSettings;
+  friend class cedar::aux::Singleton<cedar::proc::gui::Settings>;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // nested types
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  /*!@brief The docking behavior of a widget.
+   */
+  class DockSettings : public cedar::aux::Configurable
+  {
+    public:
+      //!@brief standard constructor
+      DockSettings();
+
+      //!@brief gets visibility and floating state from a QDockWidget
+      void getFrom(QDockWidget *pDock);
+      //!@brief sets visibility and floating state to a QDockWidget
+      void setTo(QDockWidget *pDock);
+
+    private:
+      //!@brief flag if widget is visible
+      cedar::aux::BoolParameterPtr mVisible;
+      //!@brief flag if widget is free-floating or docked
+      cedar::aux::BoolParameterPtr mFloating;
+  };
+
+  //!@cond SKIPPED_DOCUMENTATION
+  CEDAR_GENERATE_POINTER_TYPES(DockSettings);
+  //!@endcond
+
+  class StepDisplayMode
+  {
+    public:
+      //! The base type of enum ids of this class.
+      typedef cedar::aux::EnumId Id;
+
+      //! Typedef of the shared pointer of enum values belonging to this class.
+      typedef boost::shared_ptr<cedar::aux::EnumBase> TypePtr;
+
+      /*! @brief Construct method that fills the enum.
+       *  @see cedar::aux::EnumBase
+       */
+      static void construct()
+      {
+        mType.type()->def(cedar::aux::Enum(ICON_ONLY, "ICON_ONLY", "icon only"));
+        mType.type()->def(cedar::aux::Enum(TEXT_FOR_LOOPED, "TEXT_FOR_LOOPED", "text for looped steps"));
+        mType.type()->def(cedar::aux::Enum(ICON_AND_TEXT, "ICON_AND_TEXT", "icon and text"));
+      }
+
+      //! @returns A const reference to the base enum object.
+      static const cedar::aux::EnumBase& type()
+      {
+        return *(mType.type());
+      }
+
+      //! @returns A pointer to the base enum object.
+      static TypePtr typePtr()
+      {
+        return mType.type();
+      }
+
+      //! Identifier for the role input.
+      static const Id ICON_ONLY = 0;
+      //! Identifier for the role output.
+      static const Id TEXT_FOR_LOOPED = 1;
+      //! Identifier for the role buffer.
+      static const Id ICON_AND_TEXT = 2;
+
+    private:
+      //! The base enum object.
+      static cedar::aux::EnumType<cedar::proc::gui::Settings::StepDisplayMode> mType;
+  };
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  //!@brief The standard constructor.
+  Settings();
+
+public:
+  //!@brief Destructor
+  ~Settings();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief loads the UI settings
+  void load();
+  //!@brief saves the UI settings
+  void save();
+
+  //!@brief returns a list of all plugins that should be loaded on start-up
+  const std::set<std::string>& pluginsToLoad();
+  //!@brief adds a plugin to the list of plugins that are loaded on start-up
+  void addPluginToLoad(const std::string& path);
+  //!@brief removes a plugin from the list of plugins that are loaded on start-up
+  void removePluginToLoad(const std::string& path);
+
+  //!@brief returns the settings concerning the docking behavior for the log widget
+  DockSettingsPtr logSettings();
+  //!@brief returns the settings concerning the docking behavior for the tools widget
+  DockSettingsPtr toolsSettings();
+  //!@brief returns the settings concerning the docking behavior for the property pane
+  DockSettingsPtr propertiesSettings();
+  //!@brief returns the settings concerning the docking behavior for the steps widget
+  DockSettingsPtr stepsSettings();
+
+  //!@brief stores the state of the main window
+  void storeMainWindow(QMainWindow *pWindow);
+  //!@brief restores a state of the main window
+  void restoreMainWindow(QMainWindow *pWindow);
+
+  //!@brief returns the last directory, from which a plugin was loaded
+  cedar::aux::DirectoryParameterPtr lastPluginLoadDialogLocation();
+
+  //!@brief returns the last directory, from which an architecture was loaded
+  cedar::aux::DirectoryParameterPtr lastArchitectureLoadDialogDirectory();
+
+  //!@brief returns the last directory, from which an architecture was loaded
+  cedar::aux::DirectoryParameterPtr lastArchitectureExportDialogDirectory();
+
+  //!@brief appends an architecture configuration file to the history of files
+  void appendArchitectureFileToHistory(const std::string& filePath);
+
+  //!@brief returns a history of architecture files
+  cedar::aux::StringVectorParameterPtr getArchitectureFileHistory();
+
+  //!@brief Whether the graphics item shadow effects are used.
+  bool useGraphicsItemShadowEffects() const;
+
+  //!@brief Sets whether the graphics item shadow effects are used.
+  void setUseGraphicsItemShadowEffects(bool useShadows);
+
+  //!@brief Get the snap to grid state.
+  bool snapToGrid() const;
+
+  //!@brief Set the snap to grid state.
+  void snapToGrid(bool snap);
+
+  //! Disables writing.
+  void disableWriting(bool disable = true)
+  {
+    this->mWritingDisabled = disable;
+  }
+
+  //! Default display mode for steps.
+  StepDisplayMode::Id getDefaultDisplayMode() const
+  {
+    return this->_mDefaultStepDisplayMode->getValue();
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+private:
+  //! Disables writing of the properties; this is useful for unit tests that shouldn't alter the configuration.
+  bool mWritingDisabled;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+private:
+  //!@brief List of plugins that should be loaded on startup.
+  cedar::aux::StringSetParameterPtr mPluginsToLoad;
+
+  //!@brief the settings concerning the docking behavior for the log widget
+  DockSettingsPtr mLog;
+
+  //!@brief the settings concerning the docking behavior for the steps widget
+  DockSettingsPtr mSteps;
+
+  //!@brief the settings concerning the docking behavior for the tools widget
+  DockSettingsPtr mTools;
+
+  //!@brief the settings concerning the docking behavior for the property pane
+  DockSettingsPtr mProperties;
+  
+  //!@brief list of bytes coming from Qt (sizes, ...)
+  cedar::aux::StringParameterPtr mMainWindowGeometry;
+
+  //!@brief list of bytes coming from Qt (minimized, maximized, ...)
+  cedar::aux::StringParameterPtr mMainWindowState;
+
+  //!@brief Directory, where the PluginLoadDialog is supposed to open.
+  cedar::aux::DirectoryParameterPtr mPluginLoadDialogLocation;
+
+  //!@brief Directory, where the load dialog for architectures is supposed to open.
+  cedar::aux::DirectoryParameterPtr mArchitectureLoadDialogDirectory;
+
+  //!@brief Directory, where the load dialog for architectures is supposed to open.
+  cedar::aux::DirectoryParameterPtr mArchitectureExportDialogDirectory;
+
+  //!@brief History of architecture files recently loaded.
+  cedar::aux::StringVectorParameterPtr mRecentArchitectureFiles;
+
+  //!@brief Disables or enables graphics item shadows.
+  cedar::aux::BoolParameterPtr mUseGraphicsItemShadowEffects;
+
+  //!@brief Disables or enables graphics item shadows.
+  cedar::aux::BoolParameterPtr mSnapToGrid;
+
+  //! Default display mode for steps.
+  cedar::aux::EnumParameterPtr _mDefaultStepDisplayMode;
+
+}; // class cedar::proc::gui::Settings
+
+namespace cedar
+{
+  namespace proc
+  {
+    namespace gui
+    {
+      typedef cedar::aux::Singleton<cedar::proc::gui::Settings> SettingsSingleton;
+    }
+  }
+}
+
+CEDAR_PROC_EXPORT_SINGLETON(cedar::proc::gui::Settings);
+
+#endif // CEDAR_PROC_GUI_SETTINGS_H
+
---
+++ tests/interactive/devices/gl/Caren/main.cpp
@@ -0,0 +1,210 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+ ----- Institute:   Ruhr-Universitaet Bochum
+                    Institut fuer Neuroinformatik
+
+ ----- File:        main.cpp
+
+ ----- Maintainer:  Hendrik Reimann
+ ----- Email:       hendrik.reimann@ini.rub.de
+ ----- Date:        2010 11 17
+
+ ----- Description: interactive test for the Caren simulation
+
+ ----- Credits:
+ -----------------------------------------------------------------------------*/
+
+// CEDAR INCLUDES
+#include "cedar/devices/robot/SimulatedKinematicChain.h"
+#include "cedar/devices/robot/gl/Caren.h"
+#include "cedar/devices/robot/gui/KinematicChainWidget.h"
+#include "cedar/devices/robot/gui/MountedCameraViewer.h"
+#include "cedar/auxiliaries/systemFunctions.h"
+#include "cedar/auxiliaries/gl/Scene.h"
+#include "cedar/auxiliaries/gui/Viewer.h"
+#include "cedar/auxiliaries/gui/SceneWidget.h"
+#include "cedar/auxiliaries/sleepFunctions.h"
+#include "cedar/auxiliaries/gl/Cylinder.h"
+#include "cedar/auxiliaries/gl/Sphere.h"
+#include "cedar/auxiliaries/gl/Chessboard.h"
+
+// SYSTEM INCLUDES
+#include <QApplication>
+
+
+int main(int argc, char **argv)
+{
+  QApplication a(argc, argv);
+
+  // find resources
+  std::string trunk_configuration_file = cedar::aux::locateResource("configs/caren_trunk.json");
+  std::string arm_configuration_file = cedar::aux::locateResource("configs/kuka_lwr4.json");
+  std::string head_configuration_file = cedar::aux::locateResource("configs/caren_head.json");
+  std::string camera_middle_configuration_file = cedar::aux::locateResource("configs/caren_camera_middle.json");
+  std::string finger_one_configuration_file = cedar::aux::locateResource("configs/sdh_finger_one.json");
+  std::string finger_two_configuration_file = cedar::aux::locateResource("configs/sdh_finger_two.json");
+  std::string finger_three_configuration_file = cedar::aux::locateResource("configs/sdh_finger_three.json");
+  std::string palm_configuration_file = cedar::aux::locateResource("configs/sdh_palm.json");
+
+  // create simulated kinematic chains
+  cedar::dev::robot::KinematicChainPtr caren_trunk(new cedar::dev::robot::SimulatedKinematicChain());
+  caren_trunk->readJson(trunk_configuration_file);
+  cedar::dev::robot::KinematicChainPtr caren_arm(new cedar::dev::robot::SimulatedKinematicChain());
+  caren_arm->readJson(arm_configuration_file);
+  cedar::dev::robot::KinematicChainPtr caren_head(new cedar::dev::robot::SimulatedKinematicChain());
+  caren_head->readJson(head_configuration_file);
+  cedar::dev::robot::KinematicChainPtr finger_one(new cedar::dev::robot::SimulatedKinematicChain());
+  finger_one->readJson(finger_one_configuration_file);
+  cedar::dev::robot::KinematicChainPtr finger_two(new cedar::dev::robot::SimulatedKinematicChain());
+  finger_two->readJson(finger_two_configuration_file);
+  cedar::dev::robot::KinematicChainPtr finger_three(new cedar::dev::robot::SimulatedKinematicChain());
+  finger_three->readJson(finger_three_configuration_file);
+  cedar::dev::robot::KinematicChainPtr palm(new cedar::dev::robot::SimulatedKinematicChain());
+  palm->readJson(palm_configuration_file);
+
+  // link kinematic chains to each other
+  caren_trunk->setEndEffector(caren_arm->getRootCoordinateFrame());
+  caren_arm->setEndEffector(palm->getRootCoordinateFrame());
+
+  // initialize head to look straight down
+  caren_head->setJointAngle(1, 1.4);
+
+  // create scene and viewer to display the arm
+  cedar::aux::gl::ScenePtr scene(new cedar::aux::gl::Scene());
+  scene->setSceneLimit(2);
+  scene->drawFloor(false);
+  cedar::aux::gui::Viewer viewer(scene);
+  viewer.setSceneRadius(scene->getSceneLimit());
+
+  // create visualization objects
+  cedar::dev::robot::gl::CarenPtr caren_visualization
+  (
+    new cedar::dev::robot::gl::Caren
+    (
+      caren_trunk,
+      caren_arm,
+      caren_head,
+      palm,
+      finger_one,
+      finger_two,
+      finger_three
+    )
+  );
+
+  // add visualization objects to scene
+  scene->addObjectVisualization(caren_visualization);
+
+  // create control widgets for the scene and the arm
+  cedar::aux::gui::SceneWidgetPtr scene_widget(new cedar::aux::gui::SceneWidget(scene));
+  cedar::dev::robot::gui::KinematicChainWidget widget_trunk(caren_trunk);
+  cedar::dev::robot::gui::KinematicChainWidget widget_arm(caren_arm);
+  cedar::dev::robot::gui::KinematicChainWidget widget_head(caren_head);
+  cedar::dev::robot::gui::KinematicChainWidget widget_finger_one(finger_one);
+  cedar::dev::robot::gui::KinematicChainWidget widget_finger_two(finger_two);
+  cedar::dev::robot::gui::KinematicChainWidget widget_finger_three(finger_three);
+  cedar::dev::robot::gui::KinematicChainWidget widget_palm(palm);
+
+  // create a cylinder visualization and add it to the scene
+  cedar::aux::LocalCoordinateFramePtr cylinder_local_coordinate_frame(new cedar::aux::LocalCoordinateFrame());
+  cylinder_local_coordinate_frame->setName("cylinder");
+  cylinder_local_coordinate_frame->setTranslation(1, 0, 1);
+  cedar::aux::gl::ObjectVisualizationPtr cylinder
+  (
+    new cedar::aux::gl::Cylinder(cylinder_local_coordinate_frame, .01, .02, 0, 0.8, 0)
+  );
+  scene->addObjectVisualization(cylinder);
+
+  // create a sphere visualization and add it to the scene
+  cedar::aux::LocalCoordinateFramePtr sphere_local_coordinate_frame(new cedar::aux::LocalCoordinateFrame());
+  sphere_local_coordinate_frame->setName("sphere");
+  sphere_local_coordinate_frame->setTranslation(.5, .0, 0.0);
+  cedar::aux::gl::ObjectVisualizationPtr sphere
+  (
+    new cedar::aux::gl::Sphere(sphere_local_coordinate_frame, .005, 0, 0.8, 0)
+  );
+  scene->addObjectVisualization(sphere);
+
+  // create a chess board visualization and add it to the scene
+  cedar::aux::LocalCoordinateFramePtr chessboard_one_local_coordinate_frame(new cedar::aux::LocalCoordinateFrame());
+  chessboard_one_local_coordinate_frame->setName("chess board");
+  chessboard_one_local_coordinate_frame->setTranslation(.1, -.14, .0);
+  cedar::aux::gl::ObjectVisualizationPtr chessboard_one
+  (
+    new cedar::aux::gl::Chessboard(chessboard_one_local_coordinate_frame, .175, .245, 0.0001, 5, 7, 0, 0, 0)
+  );
+  scene->addObjectVisualization(chessboard_one);
+
+  // create a chess board visualization and add it to the scene
+  cedar::aux::LocalCoordinateFramePtr chessboard_two_local_coordinate_frame(new cedar::aux::LocalCoordinateFrame());
+  chessboard_two_local_coordinate_frame->setName("chess board");
+  chessboard_two_local_coordinate_frame->setTranslation(.553, -.3745, .0);
+  cedar::aux::gl::ObjectVisualizationPtr chessboard_two
+  (
+    new cedar::aux::gl::Chessboard(chessboard_two_local_coordinate_frame, .175, .245, 0.0001, 5, 7, 0, 0, 0)
+  );
+  scene->addObjectVisualization(chessboard_two);
+
+  // create a chess board visualization and add it to the scene
+  cedar::aux::LocalCoordinateFramePtr chessboard_three_local_coordinate_frame(new cedar::aux::LocalCoordinateFrame());
+  chessboard_three_local_coordinate_frame->setName("chess board");
+  chessboard_three_local_coordinate_frame->setTranslation(.553, 1.2535, .0);
+  cedar::aux::gl::ObjectVisualizationPtr chessboard_three
+  (
+    new cedar::aux::gl::Chessboard(chessboard_three_local_coordinate_frame, .175, .245, 0.0001, 5, 7, 0, 0, 0)
+  );
+  scene->addObjectVisualization(chessboard_three);
+
+
+
+  // create a mounted camera viewer
+  cedar::dev::robot::gui::MountedCameraViewer camera_viewer(scene, caren_head, false);
+  camera_viewer.readJson(camera_middle_configuration_file);
+  camera_viewer.setSceneRadius(scene->getSceneLimit());
+
+  // show widgets
+  scene_widget->show();
+  widget_trunk.show();
+  widget_arm.show();
+  widget_head.show();
+  widget_finger_one.show();
+  widget_finger_two.show();
+  widget_finger_three.show();
+  widget_palm.show();
+  viewer.show();
+  camera_viewer.show();
+
+  caren_trunk->startTimer(50.0);
+  caren_arm->startTimer(50.0);
+  caren_head->startTimer(50.0);
+  viewer.startTimer(50);
+  camera_viewer.startTimer(50);
+  a.exec();
+
+  caren_trunk->stop();
+  caren_arm->stop();
+  caren_head->stop();
+  caren_trunk->wait();
+  caren_arm->wait();
+  caren_head->wait();
+
+  return 0;
+}
