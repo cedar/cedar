845b077ea6971a432b2368cea6b60f2d7bad65a2
Christian Bodenstein <christian.bodenstein@ini.rub.de>
2013-07-23 14:07:08 +0200

    Add Recorder

---
+++ cedar/auxiliaries/MatData.cpp
@@ -0,0 +1,216 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        MatData.cpp
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2012 07 06
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include "cedar/auxiliaries/MatData.h"
+#include "cedar/auxiliaries/math/tools.h"
+#include "cedar/auxiliaries/stringFunctions.h"
+
+// SYSTEM INCLUDES
+#include <vector>
+#include <string>
+#include <fstream>
+#include <boost/lexical_cast.hpp>
+#include<iostream>
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+cedar::aux::MatData::~MatData()
+{
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+std::string cedar::aux::MatData::getDescription() const
+{
+  std::string description;
+  description += Super::getDescription();
+  description += "<hr />";
+
+  this->lockForRead();
+  const cv::Mat& mat = this->getData();
+  if (mat.empty())
+  {
+    description += "Empty matrix.";
+  }
+  else
+  {
+    unsigned int dim = cedar::aux::math::getDimensionalityOf(mat);
+    description += cedar::aux::toString(dim) + "-dimensional matrix";
+    if (dim != 0)
+    {
+      description += "<br />size: ";
+      if (dim == 1)
+      {
+        description += cedar::aux::toString(cedar::aux::math::get1DMatrixSize(mat));
+      }
+      else
+      {
+        for (unsigned int i = 0; i < dim; ++i)
+        {
+          if (i > 0)
+          {
+            description += " x ";
+          }
+          description += cedar::aux::toString(mat.size[i]);
+        }
+      }
+    }
+    description += "<br />";
+    description += "type: " + cedar::aux::math::matrixTypeToString(mat) + "<br />";
+    description += "channels: " + cedar::aux::toString(mat.channels());
+  }
+
+  this->unlock();
+
+  return description;
+}
+
+std::string cedar::aux::MatData::serializeData()
+{
+  this->lockForRead();
+  // using of a string stream to cast doubles to strings. This is much faster compared to
+  // boost lexical_cast<>
+  std::ostringstream ss;
+
+  //creating index that addresses an element in the n dimensional Mat
+  std::vector<int> index;
+  for( int k=0; k<mData.dims; k++ )
+  {
+    index.push_back(0);
+  }
+
+  //iterate  as long the last dimension has exceeded
+  while(index[mData.dims-1] < mData.size[mData.dims-1])
+  {
+    // get memory address of the element
+    uchar* element = mData.data;
+    for(int i =0; i <mData.dims;i++)
+    {
+      element += mData.step[i]*index[i];
+    }
+    //check data type
+    for(int i=0;i<mData.channels();i++)
+    {
+      switch(mData.depth())
+      {
+        case CV_8U:
+        {
+          ss << (int)(*(uchar*)(element+i*1)) << ",";
+          break;
+        }
+        case CV_8S:
+        {
+          ss << (int)(*(schar*)(element+i*1)) << ",";
+          break;
+        }
+        case CV_16U:
+        {
+          ss << *(unsigned short*)(element+i*2) << ",";
+          break;
+        }
+        case CV_16S:
+        {
+          ss << *(short*)(element+i*2) << ",";
+          break;
+        }
+        case CV_32S:
+        {
+          ss << *(int*)(element+i*4) << ",";
+          break;
+        }
+        case CV_32F:
+        {
+          ss << *(float*)(element+i*4) << ",";
+          break;
+        }
+        case CV_64F:
+        {
+          ss << *(double*)(element+i*8) << ",";
+          break;
+        }
+      }
+    }
+
+    //increase index
+    index[0]++;
+    for(int i =0; i < mData.dims-1;i++)
+    {
+      if(index[i]>=mData.size[i])
+      {
+        index[i]=0;
+        index[i+1]++;
+      }
+    }
+  }
+
+  this->unlock();
+  return ss.str();
+}
+
+std::string cedar::aux::MatData::serializeHeader()
+{
+  this->lockForRead();
+  std::ostringstream s;
+  s << "Mat" << ",";
+  s << mData.type() << ",";
+  s << mData.dims << ",";
+  for(int i =0; i < mData.dims;i++)
+  {
+    s << mData.size[i] << ",";
+  }
+  this->unlock();
+  return s.str();
+}
+
+cedar::aux::DataPtr cedar::aux::MatData::clone()
+{
+  lockForRead();
+  MatDataPtr neu(new MatData(mData.clone()));
+  unlock();
+  return neu;
+}
+
+unsigned int cedar::aux::MatData::getDimensionality() const
+{
+  return cedar::aux::math::getDimensionalityOf(this->mData);
+}
