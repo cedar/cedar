58d4f786952a3023bc679f17999decad1e70c2d7
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2013-08-02 14:03:33 +0200

    The cedar testingUtilities lib now has the proper name.

--- cedar/testingUtilities/measurementFunctions.h
+++ cedar/testingUtilities/measurementFunctions.h
@@ -54,22 +54,22 @@ namespace cedar
     /*! Writes a test measurement with the given id and duration to the cout stream in a manner that can automatically
      *  be parsed by the build server.
      */
-    CEDAR_TESTING_LIB_EXPORT void write_measurement
-                             (
-                               const std::string& id,
-                               double duration
-                             );
+    CEDAR_TESTING_UTILITIES_LIB_EXPORT void write_measurement
+                                       (
+                                         const std::string& id,
+                                         double duration
+                                       );
 
     /*! Measures the time it takes to run the given function and writes the measurement to the cout stream in a manner
      *  that can automatically be parsed by the build server.
      *
      *  @see write_measurement
      */
-    CEDAR_TESTING_LIB_EXPORT void test_time
-                             (
-                               std::string id,
-                               std::function<void()> fun
-                             );
+    CEDAR_TESTING_UTILITIES_LIB_EXPORT void test_time
+                                       (
+                                         std::string id,
+                                         std::function<void()> fun
+                                       );
   }
 }
 
