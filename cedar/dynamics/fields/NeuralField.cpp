f80a66b33bdffb189cdc07afbbc50d8da36e101e
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2013-08-14 15:00:59 +0200

    Added a field plot that uses only image plots.

--- cedar/dynamics/fields/NeuralField.cpp
+++ cedar/dynamics/fields/NeuralField.cpp
@@ -90,6 +90,15 @@ namespace
     field_plot_data.appendData(DataRole::OUTPUT, "sigmoided activation");
     declaration->definePlot(field_plot_data);
 
+    // define field plot again, but this time with image plots
+    //!@todo different icon
+    ElementDeclaration::PlotDefinition field_image_plot_data("field plot (image)", ":/cedar/dynamics/gui/field_image_plot.svg");
+    field_image_plot_data.mData.push_back(boost::make_shared<cedar::proc::PlotData>(DataRole::BUFFER, "input sum", false, "cedar::aux::gui::ImagePlot"));
+    field_image_plot_data.mData.push_back(boost::make_shared<cedar::proc::PlotData>(DataRole::BUFFER, "activation", true, "cedar::aux::gui::ImagePlot"));
+    field_image_plot_data.mData.push_back(boost::make_shared<cedar::proc::PlotData>(DataRole::OUTPUT, "activation", true, "cedar::aux::gui::ImagePlot"));
+    field_image_plot_data.mData.push_back(boost::make_shared<cedar::proc::PlotData>(DataRole::OUTPUT, "sigmoided activation", false, "cedar::aux::gui::ImagePlot"));
+    declaration->definePlot(field_image_plot_data);
+
     ElementDeclaration::PlotDefinition kernel_plot_data("kernel", ":/cedar/dynamics/gui/kernel_plot.svg");
     kernel_plot_data.appendData(DataRole::BUFFER, "lateral kernel");
     declaration->definePlot(kernel_plot_data);
