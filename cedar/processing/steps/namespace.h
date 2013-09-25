8e2a2cf678231764f4638730987994de7bac44da
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2013-09-25 13:41:44 +0200

    Added the mask step from the utilities plugin.

--- cedar/processing/steps/namespace.h
+++ cedar/processing/steps/namespace.h
@@ -64,6 +64,7 @@ namespace cedar
       CEDAR_DECLARE_PROC_CLASS(ColorConversion);
       CEDAR_DECLARE_PROC_CLASS(Flip);
       CEDAR_DECLARE_PROC_CLASS(Histogram);
+      CEDAR_DECLARE_PROC_CLASS(Mask);
       CEDAR_DECLARE_PROC_CLASS(MatrixMultiply);
       CEDAR_DECLARE_PROC_CLASS(MatrixTypeConverter);
       CEDAR_DECLARE_PROC_CLASS(Projection);
