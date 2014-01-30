14ce22c116404b4b4fd574f5831b8f93f59e1e59
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2014-01-30 18:36:50 +0100

    In the rightclick menu of the matrixsliceplot3d, users can now select along which dimension the plot slices.

--- cedar/auxiliaries/gui/MatrixSlicePlot3D.h
+++ cedar/auxiliaries/gui/MatrixSlicePlot3D.h
@@ -46,6 +46,7 @@
 // FORWARD DECLARATIONS
 #include "cedar/auxiliaries/MatData.fwd.h"
 #include "cedar/auxiliaries/gui/MatrixSlicePlot3D.fwd.h"
+#include "cedar/auxiliaries/LockableMember.h"
 
 // SYSTEM INCLUDES
 #include <QLabel>
@@ -115,6 +116,13 @@ private:
 
   void plotClicked(QMouseEvent* pEvent, double relativeImageX, double relativeImageY);
 
+  void fillContextMenu(QMenu& menu);
+
+  static void getSetup(int& dim0, int& dim1, int slicedDimension);
+
+private slots:
+  void slicedDimensionSelected();
+
   //--------------------------------------------------------------------------------------------------------------------
   // members
   //--------------------------------------------------------------------------------------------------------------------
@@ -132,6 +140,9 @@ private:
 
   //! desired columns of the slice plot
   unsigned int mDesiredColumns;
+
+  //! Dimension along which the slices are made.
+  cedar::aux::LockableMember<unsigned int> mSlicedDimension;
 
 }; // class cedar::aux::gui::MatrixSlicePlot3D
 
