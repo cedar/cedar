d2f45917dffd7ac47da0a0bb96dc11bba65b20a4
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2014-02-24 18:47:55 +0100

    Some code cleanup.

--- cedar/auxiliaries/ArithmeticExpressionEvaluator.cpp
+++ cedar/auxiliaries/ArithmeticExpressionEvaluator.cpp
@@ -88,66 +88,62 @@ namespace boost { namespace phoenix
 }}
 #endif
 
-namespace client
+template <typename Iterator>
+struct cedar::aux::ArithmeticExpressionEvaluator::ParserGrammar
+:
+boost::spirit::qi::grammar<Iterator, boost::spirit::ascii::space_type, boost::spirit::utree()>
 {
-    namespace qi = boost::spirit::qi;
-    namespace ascii = boost::spirit::ascii;
-    namespace spirit = boost::spirit;
+  typedef boost::spirit::qi::rule<Iterator, boost::spirit::ascii::space_type, boost::spirit::utree()> RuleType;
+  ParserGrammar(const std::map<std::string, double>& variables) : ParserGrammar::base_type(expression)
+  {
+    using boost::spirit::qi::uint_;
+    using boost::spirit::qi::lit;
+    using boost::spirit::qi::_val;
+    using boost::spirit::qi::_1;
 
-    template <typename Iterator>
-    struct calculator : qi::grammar<Iterator, ascii::space_type, spirit::utree()>
+    expression =
+        term                   [_val = _1]
+        >> *(   ('+' >> term   [_val = _val + _1])
+            |   ('-' >> term   [_val = _val - _1])
+            )
+        ;
+
+    term =
+        factor                 [_val = _1]
+        >> *(   ('*' >> factor [_val = _val * _1])
+            |   ('/' >> factor [_val = _val / _1])
+            )
+        ;
+
+    factor =
+        uint_                  [_val = _1]
+        | variable             [_val = _1]
+        |   '(' >> expression  [_val = _1] >> ')'
+        |   ('-' >> factor     [_val = -_1])
+        |   ('+' >> factor     [_val = _1])
+        ;
+
+    for (const auto& variable_value_pair : variables)
     {
-        calculator(const std::map<std::string, double>& variables) : calculator::base_type(expression)
-        {
-            using qi::uint_;
-            using qi::lit;
-            using qi::_val;
-            using qi::_1;
+      variable %= lit(variable_value_pair.first) [_val = variable_value_pair.second];
+    }
 
-            expression =
-                term                            [_val = _1]
-                >> *(   ('+' >> term            [_val = _val + _1])
-                    |   ('-' >> term            [_val = _val - _1])
-                    )
-                ;
+    BOOST_SPIRIT_DEBUG_NODE(expression);
+    BOOST_SPIRIT_DEBUG_NODE(term);
+    BOOST_SPIRIT_DEBUG_NODE(factor);
+  }
 
-            term =
-                factor                          [_val = _1]
-                >> *(   ('*' >> factor          [_val = _val * _1])
-                    |   ('/' >> factor          [_val = _val / _1])
-                    )
-                ;
-
-            factor =
-                uint_                           [_val = _1]
-                | variable                      [_val = _1]
-                |   '(' >> expression           [_val = _1] >> ')'
-                |   ('-' >> factor              [_val = -_1])
-                |   ('+' >> factor              [_val = _1])
-                ;
-
-            for (const auto& variable_value_pair : variables)
-            {
-              variable %= lit(variable_value_pair.first) [_val = variable_value_pair.second];
-            }
-
-
-            BOOST_SPIRIT_DEBUG_NODE(expression);
-            BOOST_SPIRIT_DEBUG_NODE(term);
-            BOOST_SPIRIT_DEBUG_NODE(factor);
-        }
-
-        qi::rule<Iterator, ascii::space_type, spirit::utree()> expression, term, factor, variable;
-    };
-}
+  RuleType expression;
+  RuleType term;
+  RuleType factor;
+  RuleType variable;
+};
 
 //----------------------------------------------------------------------------------------------------------------------
 // constructors and destructor
 //----------------------------------------------------------------------------------------------------------------------
 
 cedar::aux::ArithmeticExpressionEvaluator::ArithmeticExpressionEvaluator()
-//:
-//mGrammar(new cedar::aux::ArithmeticExpressionEvaluator::CalculatorGrammar())
 {
 }
 
@@ -164,14 +160,14 @@ double cedar::aux::ArithmeticExpressionEvaluator::evaluate
   using boost::spirit::ascii::space;
   using boost::spirit::utree;
   typedef std::string::const_iterator iterator_type;
-  typedef client::calculator<iterator_type> calculator;
+  typedef cedar::aux::ArithmeticExpressionEvaluator::ParserGrammar<iterator_type> ParserGrammar;
 
-  calculator calc(variables); // Our grammar
+  ParserGrammar grammar(variables); // Our grammar
   std::string::const_iterator iter = expression.begin();
 
   std::string::const_iterator end = expression.end();
   utree ut;
-  bool r = phrase_parse(iter, end, calc, space, ut);
+  bool r = phrase_parse(iter, end, grammar, space, ut);
 
 
   if (r && iter == end)
