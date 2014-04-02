386dbc9670d666f6b5f5300cfbfbe1230b647e9a
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2014-04-02 10:42:17 +0200

    Increased the number of decimals for double vectors to four.

--- cedar/auxiliaries/gui/DoubleVectorParameter.h
+++ cedar/auxiliaries/gui/DoubleVectorParameter.h
@@ -91,6 +91,17 @@ namespace cedar
         pWidget->setValue(value);
       }
       //!@endcond
+
+      //!@cond SKIPPED_DOCUMENTATION
+      template<>
+      inline QDoubleSpinBox* cedar::aux::gui::VectorParameterAbstraction<double, QDoubleSpinBox>::create(unsigned int index)
+      {
+        auto p_widget = new QDoubleSpinBox();
+        p_widget->setToolTip(QString("Set value for entry number %1.").arg(index));
+        p_widget->setDecimals(4);
+        return p_widget;
+      }
+      //!@endcond
     }
   }
 }
