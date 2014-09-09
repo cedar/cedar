fdd66dcc8ea6f1d89f6b9edf0ea8569c449b0d33
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2014-09-09 16:38:52 +0200

    The object parameter gui now only shows the class name. The namespace name is shown in the tool tip.

---
+++ cedar/auxiliaries/PathTemplate.h
@@ -0,0 +1,253 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        PathTemplate.h
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2013 07 25
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_AUX_PATH_TEMPLATE_H
+#define CEDAR_AUX_PATH_TEMPLATE_H
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include "cedar/auxiliaries/stringFunctions.h"
+
+// SYSTEM INCLUDES
+#include <vector>
+
+
+namespace cedar
+{
+  namespace aux
+  {
+    //! Policy for paths that determines the separator for path entries.
+    template <char Separator, typename StorageT = std::string>
+    class CharSeparator
+    {
+      public:
+        //! Returns the separator used for the path.
+        static StorageT separator()
+        {
+          StorageT separator;
+          separator += Separator;
+          return separator;
+        }
+    };
+
+    template <typename SeparatorT, typename StorageT = std::string> class PathTemplate;
+  }
+}
+
+
+/*!@brief A generic class for paths of any kind.
+ */
+template <typename SeparatorT, typename StorageT>
+class cedar::aux::PathTemplate
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // nested types
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //! The string type used for storing components of the path.
+  typedef StorageT StringType;
+
+  //! Type of separator used.
+  typedef SeparatorT SeparatorType;
+
+private:
+  //! The type of this class (for meta-programming reasons...)
+  typedef PathTemplate<SeparatorType, StringType> SelfType;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // friends
+  //--------------------------------------------------------------------------------------------------------------------
+  //! Writes the path to a stream.
+  friend std::ostream& operator<< (std::ostream& stream, const SelfType& path)
+  {
+    stream << path.toString();
+    return stream;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //! Constructs an empty path.
+  PathTemplate()
+  {
+  }
+
+  //! Constructs a path from a string.
+  PathTemplate(const StringType& path)
+  {
+    // append the vector above to the path
+    this->fromString(path);
+  }
+
+  //! Constructs a path from a const char string.
+  PathTemplate(const char* path)
+  {
+    // append the vector above to the path
+    this->fromString(StringType(path));
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //! Clears the path.
+  void clear()
+  {
+    this->mComponents.clear();
+  }
+
+  //! Appends a component to the path.
+  void append(const std::vector<StringType>& path)
+  {
+    // append the vector above to the path
+    this->mComponents.insert(this->mComponents.end(), path.begin(), path.end());
+  }
+
+  //! Appends a string component to the path
+  void append(const StringType& string)
+  {
+    // this makes sure the string is properly split into its components if it contains the separator
+    SelfType subpath(string);
+    this->append(subpath);
+  }
+
+  //! Appends another path to this path.
+  void append(const SelfType& other)
+  {
+    this->append(other.mComponents);
+  }
+
+  //! Converts the path to a string.
+  StringType toString() const
+  {
+    return cedar::aux::join(this->mComponents, SeparatorType::separator());
+  }
+
+  //! Returns the number of components in the path.
+  size_t getElementCount() const
+  {
+    return this->mComponents.size();
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public operators
+  //--------------------------------------------------------------------------------------------------------------------
+  //! Concatenates two paths.
+  SelfType operator+(const SelfType& other) const
+  {
+    SelfType copy(*this);
+    copy.append(other);
+    return copy;
+  }
+
+  //! Appends a string to the path.
+  SelfType& operator+=(const StringType& string)
+  {
+    this->append(string);
+    return *this;
+  }
+
+  //! Concatenates two paths.
+  friend StringType operator+(const StringType& string, const SelfType& path)
+  {
+    return string + path.toString();
+  }
+
+  //! Automatic string conversion operator for the path.
+  operator StringType() const
+  {
+    return this->toString();
+  }
+
+  //! Returns the path, starting from the given index.
+  SelfType operator()(size_t start)
+  {
+    return this->operator()(start, this->getElementCount());
+  }
+
+  //! Returns the section of the path between start and end.
+  SelfType operator()(size_t start, size_t end)
+  {
+    if (start >= this->getElementCount())
+    {
+      CEDAR_THROW(cedar::aux::IndexOutOfRangeException, "Invalid start index " + cedar::aux::toString(start) + " in path with " + cedar::aux::toString(this->getElementCount()) + " element(s).");
+    }
+
+    if (end >= this->getElementCount())
+    {
+      CEDAR_THROW(cedar::aux::IndexOutOfRangeException, "Invalid end index " + cedar::aux::toString(end) + " in path with " + cedar::aux::toString(this->getElementCount()) + " element(s).");
+    }
+
+    SelfType slice;
+    slice.mComponents.insert(slice.mComponents.end(), this->mComponents.begin() + start, this->mComponents.end() + end);
+    return slice;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  void fromString(const StringType& string)
+  {
+    this->clear();
+
+    std::vector<StringType> components;
+    cedar::aux::split(string, SeparatorType::separator(), components);
+    this->append(components);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+private:
+  //! The elements of the path, split apart.
+  std::vector<StringType> mComponents;
+
+}; // class cedar::aux::PathTemplate
+
+#endif // CEDAR_AUX_PATH_TEMPLATE_H
+
---
+++ cedar/auxiliaries/stringFunctions.cpp
@@ -0,0 +1,164 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        utilities.cpp
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2014 01 16
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+// CEDAR INCLUDES
+#include "cedar/auxiliaries/stringFunctions.h"
+#include "cedar/version.h"
+
+// SYSTEM INCLUDES
+#include <boost/optional.hpp>
+#include <iomanip>
+#include <sstream>
+
+std::string cedar::aux::versionNumberToString(unsigned int version)
+{
+  std::string res = cedar::aux::toString(CEDAR_GET_VERSION_MAJOR(version));
+  res += "." + cedar::aux::toString(CEDAR_GET_VERSION_MINOR(version));
+  res += "." + cedar::aux::toString(CEDAR_GET_VERSION_BUGFIX(version));
+  return res;
+}
+
+std::string cedar::aux::removeWhiteSpaces(const std::string& stringFromWhichToRemoveWhiteSpaces)
+{
+  std::string result = cedar::aux::erase(stringFromWhichToRemoveWhiteSpaces, " ");
+  result = cedar::aux::erase(result, "\n");
+  result = cedar::aux::erase(result, "\r");
+  return result;
+}
+
+std::string cedar::aux::formatDuration(const cedar::unit::Time& time)
+{
+  struct TimeSubdiv
+  {
+    TimeSubdiv(double max, const std::string& shortHandle, unsigned int precision)
+    :
+    max(max),
+    precision(precision),
+    short_handle(shortHandle)
+    {
+    }
+
+    TimeSubdiv(double max, const std::string& shortHandle)
+    :
+    max(max),
+    short_handle(shortHandle)
+    {
+    }
+
+    TimeSubdiv(const std::string& shortHandle)
+    :
+    short_handle(shortHandle)
+    {
+    }
+
+    boost::optional<double> max;
+    boost::optional<unsigned int> precision;
+    std::string short_handle;
+  };
+
+  double time_in_seconds = time / (1.0 * cedar::unit::seconds);
+
+  static std::vector<TimeSubdiv> time_subdivs;
+  if (time_subdivs.empty())
+  {
+    time_subdivs.push_back(TimeSubdiv(60.0, "s", 2));
+    time_subdivs.push_back(TimeSubdiv(60.0, "m", 2));
+    time_subdivs.push_back(TimeSubdiv(24.0, "h", 2));
+    time_subdivs.push_back(TimeSubdiv(365, "d"));
+    time_subdivs.push_back(TimeSubdiv("y"));
+  }
+
+  std::string time_str;
+
+  double current_max = 1.0;
+  double time_remaining = time_in_seconds;
+  bool first = true;
+  for (const auto& subdiv : time_subdivs)
+  {
+    double subtime;
+    double old_max = current_max;
+    if (subdiv.max)
+    {
+      current_max *= subdiv.max.get();
+      subtime = std::fmod(time_remaining, current_max);
+      time_remaining -= subtime;
+    }
+    else
+    {
+      subtime = time_remaining;
+      time_remaining = 0.0;
+    }
+    std::stringstream stream;
+    // make the conversion zero-padded
+    unsigned int padding = 0;
+    if (first)
+    {
+      stream << std::setprecision(1);
+      stream << std::fixed;
+      padding = 2;
+      first = false;
+    }
+    else
+    {
+      if (!subdiv.precision)
+      {
+        stream << std::fixed;
+      }
+      stream << std::setprecision(0);
+    }
+
+    if (time_remaining > 0.0 && subdiv.precision)
+    {
+      stream << std::setw(subdiv.precision.get() + padding) << std::setfill('0');
+    }
+
+    stream << (subtime / old_max);
+    stream << subdiv.short_handle;
+    std::string subdiv_str = stream.str();
+    if (!time_str.empty())
+    {
+      subdiv_str += " ";
+    }
+    time_str = subdiv_str + time_str;
+
+    if (time_remaining <= 0.0)
+    {
+      break;
+    }
+  }
+
+  return time_str;
+}
