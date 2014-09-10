4a73f0ee0c29e105a40568628bed7e9d404979ba
StephanZibner <StephanZibner@localhost>
2014-09-10 17:19:39 +0200

    fixed issue #954 - hidden and constant parameters are no longer accessible through experiment actions

---
+++ cedar/processing/experiment/gui/ExperimentItemWidget.cpp
@@ -0,0 +1,153 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        ExperimentItemWidget.cpp
+
+    Maintainer:  Christian Bodenstein
+    Email:       christian.bodenstein@ini.rub.de
+    Date:        2014 03 19
+
+    Description: Source file for the class cedar::proc::experiment::gui::ExperimentItemWidget.
+
+    Credits:
+
+======================================================================================================================*/
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include "cedar/processing/experiment/gui/ExperimentItemWidget.h"
+#include "cedar/auxiliaries/gui/Parameter.h"
+#include "cedar/auxiliaries/Parameter.h"
+#include "cedar/auxiliaries/ObjectParameter.h"
+// SYSTEM INCLUDES
+#include <QLabel>
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+cedar::proc::experiment::gui::ExperimentItemWidget::ExperimentItemWidget(QWidget* parent)
+:
+QWidget(parent)
+{
+  this->setLayout(new QHBoxLayout);
+  this->layout()->setMargin(0);
+}
+
+cedar::proc::experiment::gui::ExperimentItemWidget::~ExperimentItemWidget()
+{
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+void cedar::proc::experiment::gui::ExperimentItemWidget::display(cedar::aux::ConfigurablePtr experimentItem)
+{
+  clear();
+  for(cedar::aux::ParameterPtr parameter : experimentItem->getParameters())
+  {
+    cedar::aux::gui::Parameter* parameterWidget;
+    parameterWidget = cedar::aux::gui::ParameterFactorySingleton::getInstance()->get(parameter)->allocateRaw();
+    parameterWidget->setParameter(parameter);
+    parameterWidget->setParent(this);
+
+    if(auto object_parameter = boost::dynamic_pointer_cast<cedar::aux::ObjectParameter>(parameter))
+    {
+       QVBoxLayout* row = new QVBoxLayout;
+       row->setAlignment(Qt::AlignTop);
+       cedar::proc::experiment::gui::ExperimentItemWidget* another_widget = new cedar::proc::experiment::gui::ExperimentItemWidget();
+       another_widget->display(object_parameter->getConfigurable());
+       row->addWidget(parameterWidget);
+       row->addWidget(another_widget);
+       ((QBoxLayout*)this->layout())->addLayout(row);
+       connect(object_parameter.get(),SIGNAL(valueChanged()),this,SLOT(objectParameterChanged()));
+    }
+    else
+    {
+      this->layout()->addWidget(parameterWidget);
+    }
+  }
+}
+
+
+void cedar::proc::experiment::gui::ExperimentItemWidget::clear(QLayout* layout)
+{
+  if (layout == NULL)
+  {
+    clear(this->layout());
+    return;
+  }
+  QLayoutItem* child;
+  while((child = layout->takeAt(0)) != 0)
+  {
+      if(child->layout() != 0)
+      {
+        //clear(child->layout());
+      }
+      else if(child->widget() != 0)
+      {
+        delete child->widget();
+      }
+      delete child;
+  }
+}
+
+
+void cedar::proc::experiment::gui::ExperimentItemWidget::objectParameterChanged()
+{
+  auto p_objec_parameter = dynamic_cast<cedar::aux::ObjectParameter*>(QObject::sender());
+  CEDAR_DEBUG_ASSERT(p_objec_parameter != nullptr);
+
+
+  QList<cedar::aux::gui::Parameter*> parameter_list = this->findChildren<cedar::aux::gui::Parameter*>();
+  for(cedar::aux::gui::Parameter* parameter : parameter_list)
+  {
+    if(parameter->getParameter().get() == p_objec_parameter)
+    {
+      QLayoutItem* child;
+      for(int i=0; i < this->layout()->count() ; i++)
+      {
+        child = layout()->itemAt(i);
+        if(child->layout())
+        {
+          QWidget* widget = child->layout()->itemAt(0)->widget();
+          if (widget == parameter)
+          {
+            delete child->layout()->itemAt(1)->widget();
+            cedar::proc::experiment::gui::ExperimentItemWidget* widget
+                    = new cedar::proc::experiment::gui::ExperimentItemWidget();
+            widget->display(p_objec_parameter->getConfigurable());
+            child->layout()->addWidget(widget);
+            return;
+
+          }
+        }
+      }
+    }
+  }
+}
+
---
+++ cedar/processing/gui/ArchitectureWidgetList.h
@@ -0,0 +1,117 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        ArchitectureWidgetList.h
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2014 09 03
+
+    Description: Header file for the class cedar::proc::gui::ArchitectureWidgetList.
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_PROC_GUI_ARCHITECTURE_WIDGET_LIST_H
+#define CEDAR_PROC_GUI_ARCHITECTURE_WIDGET_LIST_H
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include "cedar/processing/gui/ui_ArchitectureWidgetList.h"
+
+// FORWARD DECLARATIONS
+#include "cedar/processing/gui/ArchitectureWidgetList.fwd.h"
+#include "cedar/processing/gui/Group.fwd.h"
+#include "cedar/auxiliaries/Path.fwd.h"
+
+// SYSTEM INCLUDES
+#include <QDialog>
+
+
+/*!@brief A dialog for editing the architecture widgets in a group.
+ */
+class cedar::proc::gui::ArchitectureWidgetList : public QDialog, public Ui_ArchitectureWidgetList
+{
+  Q_OBJECT
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // nested types
+  //--------------------------------------------------------------------------------------------------------------------
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief The standard constructor.
+  ArchitectureWidgetList(QWidget* pParent, cedar::proc::gui::GroupPtr group);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  void appendRow(const std::string& name, const cedar::aux::Path& path);
+
+private slots:
+  void dialogAccepted();
+
+  void addRowClicked();
+
+  void removeRowClicked();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+private:
+  cedar::proc::gui::GroupPtr mGroup;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+private:
+  // none yet
+
+}; // class cedar::proc::gui::ArchitectureWidgetList
+
+#endif // CEDAR_PROC_GUI_ARCHITECTURE_WIDGET_LIST_H
+
