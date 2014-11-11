5b8106c83c7b9deb2d0fcc3c7dbde115e84fb831
StephanZibner <StephanZibner@localhost>
2014-11-11 19:17:18 +0100

    even more code cleanup in trial framework

--- cedar/processing/experiment/Supervisor.cpp
+++ cedar/processing/experiment/Supervisor.cpp
@@ -62,7 +62,22 @@ mpExperiment(nullptr)
 
 void cedar::proc::experiment::Supervisor::step(cedar::unit::Time)
 {
-  this->mpExperiment->executeActionSequences();
+  if (this->mpExperiment->trialIsRunning()) // trial is running
+  {
+    this->mpExperiment->executeActionSequences();
+  }
+  else
+  {
+    // check if there are more trials to run
+    if (this->mpExperiment->hasMoreTrials())
+    {
+      this->mpExperiment->startTrial();
+    }
+    else
+    {
+      this->mpExperiment->stopExperiment();
+    }
+  }
 }
 
 void cedar::proc::experiment::Supervisor::setExperiment(Experiment* experiment)
