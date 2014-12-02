d7db9d1e55c6ab42fe03919824129a2692ac00df
StephanZibner <StephanZibner@localhost>
2014-12-02 14:46:35 +0100

    added an explanatory string for enums, which is displayed in the enum parameter widget

---
+++ cedar/auxiliaries/gui/SurfacePlot.h
@@ -0,0 +1,49 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        SurfacePlot.h
+
+    Maintainer:  Kai Kuchenbecker
+    Email:       kai.kuchenbecker@ini.ruhr-uni-bochum.de
+    Date:        2013 07 30
+
+    Description: Old Header for QwtSurfacePlot
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_AUX_GUI_SURFACE_PLOT_H
+#define CEDAR_AUX_GUI_SURFACE_PLOT_H
+
+#include "cedar/configuration.h"
+// CEDAR INCLUDES
+#include "cedar/auxiliaries/gui/namespace.h"
+#include "cedar/auxiliaries/gui/QwtSurfacePlot.h"
+
+// SYSTEM INCLUDES
+#warning Including this header is deprecated; use cedar/auxiliaries/gui/QwtSurfacePlot.h instead.
+
+#endif // CEDAR_AUX_GUI_SURFACE_PLOT_H
+
