8062cb3cc5dac552d18f8327bdd473147cad0c2b
StephanZibner <StephanZibner@localhost>
2014-12-03 11:13:15 +0100

    added a concept page for the experiment framework

---
+++ cedar/processing/experiment/action/Action.h
@@ -0,0 +1,139 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        Action.h
+
+    Maintainer:  Christian Bodenstein
+    Email:       christian.bodenstein@ini.ruhr-uni-bochum.de
+    Date:        2014 01 22
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_PROC_EXPERIMENT_ACTION_H
+#define CEDAR_PROC_EXPERIMENT_ACTION_H
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include "cedar/auxiliaries/Configurable.h"
+#include "cedar/auxiliaries/ObjectListParameterTemplate.h"
+#include "cedar/auxiliaries/ObjectParameterTemplate.h"
+
+// FORWARD DECLARATIONS
+#include "cedar/processing/experiment/action/Action.fwd.h"
+#include "cedar/processing/experiment/Experiment.fwd.h"
+
+// SYSTEM INCLUDES
+#include <vector>
+
+
+/*!@brief An abstract class for all the derived Action classes.
+ *    The derived class has to implement the run() function.
+ */
+class cedar::proc::experiment::action::Action : public cedar::aux::Configurable
+{
+public:
+
+	//!@brief a parameter for action sequence objects
+	typedef cedar::aux::ObjectListParameterTemplate<cedar::proc::experiment::action::Action> ActionListParameter;
+	typedef cedar::aux::ObjectParameterTemplate<cedar::proc::experiment::action::Action> ActionParameter;
+
+	//!@cond SKIPPED_DOCUMENTATION
+	CEDAR_GENERATE_POINTER_TYPES_INTRUSIVE(ActionListParameter);
+	CEDAR_GENERATE_POINTER_TYPES_INTRUSIVE(ActionParameter);
+	//!@endcond
+
+	#include "cedar/auxiliaries/FactoryManager.h"
+
+private:
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief The standard constructor.
+  Action();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief This function should implement the things that this Action should do
+  virtual void run() = 0;
+
+  //! Called before an experiment is started.
+  virtual void preExperiment();
+
+  //! Called after an experiment is stopped.
+  virtual void postExperiment();
+
+  /*! Reimplement this to check the validity of the action. Return true if the action is valid. If false is returned, an
+   *  inforative message should be added to the @em errors vector.
+   */
+  virtual bool checkValidity(std::vector<std::string>& errors, std::vector<std::string>& warnings) const;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+
+private:
+
+}; // class cedar::proc::experiment::Action
+
+CEDAR_PROC_EXPORT_SINGLETON(cedar::aux::FactoryManager<cedar::proc::experiment::action::ActionPtr>);
+
+namespace cedar
+{
+  namespace proc
+  {
+    namespace experiment
+    {
+      namespace action
+      {
+        //!@brief The singleton instance of the instruction factory manager.
+        typedef cedar::aux::Singleton< cedar::aux::FactoryManager<cedar::proc::experiment::action::ActionPtr>>
+                ActionManagerSingleton;
+      }
+    }
+  }
+}
+
+#endif // CEDAR_PROC_EXPERIMENT_ACTION_H
+
---
+++ cedar/processing/gui/BoostControl.h
@@ -0,0 +1,159 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        BoostControl.h
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2013 06 28
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_PROC_GUI_BOOST_CONTROL_H
+#define CEDAR_PROC_GUI_BOOST_CONTROL_H
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include "cedar/processing/gui/ui_BoostControl.h"
+
+// FORWARD DECLARATIONS
+#include "cedar/processing/Element.fwd.h"
+#include "cedar/processing/Group.fwd.h"
+#include "cedar/processing/gui/BoostControl.fwd.h"
+#include "cedar/processing/gui/View.fwd.h"
+#include "cedar/processing/sources/Boost.fwd.h"
+
+// SYSTEM INCLUDES
+#ifndef Q_MOC_RUN
+  #include <boost/signals2.hpp>
+#endif
+#include <map>
+#include <vector>
+
+
+/*!@brief A widget for conveniently controlling the boosts in a network.
+ */
+class cedar::proc::gui::BoostControl : public QWidget, public Ui_BoostControl
+{
+  Q_OBJECT
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // nested types
+  //--------------------------------------------------------------------------------------------------------------------
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief The standard constructor.
+  BoostControl(cedar::proc::gui::View* view);
+
+  //!@brief The destructor.
+  ~BoostControl();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  /*!@brief Sets the network whose boosts are controlled by this widget.
+   */
+  void setGroup(cedar::proc::GroupPtr network);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  //! Adds a boost to this control widget.
+  void addBoost(cedar::proc::sources::BoostPtr boost);
+
+  //! Translates the boost signal by emitting a Qt signal.
+  void translateElementAddedSignal(cedar::proc::ElementPtr);
+
+  //! Translates the boost signal by emitting a Qt signal.
+  void translateElementRemovedSignal(cedar::proc::ConstElementPtr);
+
+  void addGroup(cedar::proc::GroupPtr group);
+
+  void disconnectSignals();
+
+  QTreeWidgetItem* getGroupItem(const std::string& elementPath);
+
+  void connectRenameSignal(cedar::proc::ElementPtr element);
+
+  bool isBoostItem(QTreeWidgetItem* pItem) const;
+
+  cedar::proc::sources::BoostPtr findBoostFor(QTreeWidgetItem* pItem) const;
+
+signals:
+  //! Used to translate a boost signal to a Qt signal.
+  void elementAddedSignal(QString);
+
+  //! Used to translate a boost signal to a Qt signal.
+  void elementRemovedSignal(QString);
+
+private slots:
+  //! Reacts to an element that has been added in the underlying network.
+  void elementAdded(QString elementName);
+
+  //! Reacts to the removal of a boost in the underlying network.
+  void elementRemoved(QString elementName);
+
+  //! Reacts to the change of the name of a boost in the underlying network.
+  void elementNameChanged();
+
+  //! If possible, centers the boost that was double-clicked.
+  void itemActivated(QTreeWidgetItem* pItem, int column);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+private:
+  cedar::proc::GroupPtr mGroup;
+
+  cedar::proc::gui::View* mpView;
+
+  std::vector<boost::signals2::connection> mElementAddedConnections;
+
+  std::vector<boost::signals2::connection> mElementRemovedConnections;
+
+  std::map<const cedar::proc::Element*, QString> mElementNames;
+
+}; // class cedar::proc::gui::BoostControl
+
+#endif // CEDAR_PROC_GUI_BOOST_CONTROL_H
+
