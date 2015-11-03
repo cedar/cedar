29ca428538fcef033f218d5c6b3b232367bf6ef4
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2015-11-03 09:59:41 +0100

    The MatrixSlicePlot3D now shows the actual CV string when it is unable to plot a ceratin type (e.g., CV_16U instead of 2).

---
+++ cmake/findScripts/FindQwtPlot3D.cmake
@@ -0,0 +1,25 @@
+# Find qwtplot3d and create the following output:
+# qwtplot3d_FOUND - whether qwtplot3d was found or not
+# qwtplot3d_INCLUDE_DIRS - the qwtplot3d include directories
+# qwtplot3d_LIBS - qwtplot3d libraries
+
+# find include dir in set of paths
+find_path(qwtplot3d_INCLUDE_DIRS
+  NAMES qwt3d_plot3d.h qwt3d_gridplot.h
+  PATHS ${CEDAR_DEPENDENCY_QWTPLOT3D}
+)
+# find library in set of paths
+find_library(qwtplot3d_LIBS
+  NAMES qwtplot3d
+  PATHS ${CEDAR_DEPENDENCY_LIBRARIES}
+)
+
+# now check if anything is missing
+if(qwtplot3d_INCLUDE_DIRS AND qwtplot3d_LIBS)
+  set(qwtplot3d_INCLUDE_DIRS "${qwtplot3d_INCLUDE_DIRS}/..")
+  set(qwtplot3d_FOUND true)
+else(qwtplot3d_INCLUDE_DIRS AND qwtplot3d_LIBS)
+  set(qwtplot3d_FOUND false)
+  set(qwtplot3d_LIBS "")
+  set(qwtplot3d_INCLUDE_DIRS "")
+endif(qwtplot3d_INCLUDE_DIRS AND qwtplot3d_LIBS)
---
+++ tests/unit/auxiliaries/LocalCoordinateFrame/main.cpp
@@ -0,0 +1,338 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014, 2015 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+ ----- Institute:   Ruhr-Universitaet Bochum
+                    Institut fuer Neuroinformatik
+
+ ----- File:        main.cpp
+
+ ----- Author:      Hendrik Reimann
+ ----- Email:       hendrik.reimann@ini.rub.de
+ ----- Date:        2010 12 04
+
+ ----- Description: Implements all unit tests for the @em cedar::aux::LocalCoordinateFrame class.
+
+ ----- Credits:
+ ---------------------------------------------------------------------------------------------------------------------*/
+
+// LOCAL INCLUDES
+
+// PROJECT INCLUDES
+#include "cedar/auxiliaries/LocalCoordinateFrame.h"
+#include "cedar/auxiliaries/math/tools.h"
+#include "cedar/auxiliaries/math/constants.h"
+#include "cedar/auxiliaries/utilities.h"
+#include "cedar/units/Length.h"
+#include "cedar/units/PlaneAngle.h"
+
+// SYSTEM INCLUDES
+#include <string>
+#include <math.h>
+
+int main()
+{
+  // the number of errors encountered in this test
+  int errors = 0;
+  
+  // create instance of test class
+  cedar::aux::LocalCoordinateFrame local_coordinate_frame;
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // translation
+  //--------------------------------------------------------------------------------------------------------------------
+  std::cout << "test: translation" << std::endl;
+  local_coordinate_frame.setTranslation
+  (
+    1337.0 * cedar::unit::meters,
+    0.0 * cedar::unit::meters,
+    cedar::aux::math::pi * cedar::unit::meters
+  );
+  if (
+         !cedar::aux::math::isZero<double>
+          (
+            local_coordinate_frame.getTranslationX() / cedar::unit::DEFAULT_LENGTH_UNIT - 1337.0
+          )
+      || !cedar::aux::math::isZero<double>
+          (
+            local_coordinate_frame.getTranslationY() / cedar::unit::DEFAULT_LENGTH_UNIT - 0.0
+          )
+      || !cedar::aux::math::isZero<double>
+          (
+            local_coordinate_frame.getTranslationZ() / cedar::unit::DEFAULT_LENGTH_UNIT - cedar::aux::math::pi
+          )
+      )
+  {
+    errors++;
+    std::cout << "ERROR with setTranslation(double, double, double) or getTranslationX/Y/Z()" << std::endl;
+  }
+
+  cedar::unit::LengthMatrix p1(cv::Mat::ones(4, 1, CV_64FC1), 1 * cedar::unit::meters);
+  p1.matrix.at<double>(0, 0) = 555.555;
+  p1.matrix.at<double>(1, 0) = 2;
+  p1.matrix.at<double>(2, 0) = sqrt(3.0);
+
+  local_coordinate_frame.setTranslation(p1);
+  cedar::unit::LengthMatrix p2 = local_coordinate_frame.getTranslation();
+  if (
+      p2.matrix.at<double>(0, 0) != 555.555
+      || p2.matrix.at<double>(1, 0) != 2.0
+      || p2.matrix.at<double>(2, 0) != sqrt(3.0)
+      || p2.matrix.at<double>(3, 0) != 1.0
+      )
+  {
+    errors++;
+    std::cout << "ERROR with setTranslation(Mat) or getTranslation()" << std::endl;
+  }
+
+  std::vector<cedar::unit::Length> translation;
+  translation.push_back(1.2 * cedar::unit::meters);
+  translation.push_back(3.4 * cedar::unit::meters);
+  translation.push_back(5.6 * cedar::unit::meters);
+  local_coordinate_frame.setTranslation(translation);
+  cedar::unit::LengthMatrix p3 = local_coordinate_frame.getTranslation();
+  if (
+      p3.matrix.at<double>(0, 0) != 1.2
+      || p3.matrix.at<double>(1, 0) != 3.4
+      || p3.matrix.at<double>(2, 0) != 5.6
+      || p3.matrix.at<double>(3, 0) != 1.0
+      )
+  {
+    errors++;
+    std::cout << "ERROR with setTranslation(std::vector<double>) or getTranslation()" << std::endl;
+  }
+
+  local_coordinate_frame.translate(1.2 * cedar::unit::meters, 3.4 * cedar::unit::meters, 5.6 * cedar::unit::meters);
+  if (
+      local_coordinate_frame.getTranslationX() != 2.4 * cedar::unit::meters
+      || local_coordinate_frame.getTranslationY() != 6.8 * cedar::unit::meters
+      || local_coordinate_frame.getTranslationZ() != 11.2 * cedar::unit::meters
+      )
+  {
+    errors++;
+    std::cout << "ERROR with translate(double, double, double)" << std::endl;
+  }
+
+  p1.matrix = cv::Mat::ones(4, 1, CV_64FC1);
+  p1.matrix.at<double>(0, 0) = -0.4;
+  p1.matrix.at<double>(1, 0) = -0.8;
+  p1.matrix.at<double>(2, 0) = 0.8;
+  local_coordinate_frame.translate(p1);
+  p2 = local_coordinate_frame.getTranslation();
+  if (
+      p2.matrix.at<double>(0, 0) != 2.0
+      || p2.matrix.at<double>(1, 0) != 6.0
+      || p2.matrix.at<double>(2, 0) != 12.0
+      || p2.matrix.at<double>(3, 0) != 1.0
+      )
+  {
+    errors++;
+    std::cout << "ERROR with translate(Mat)" << std::endl;
+  }
+
+  local_coordinate_frame.translate(translation);
+  p3 = local_coordinate_frame.getTranslation();
+  if (
+      p3.matrix.at<double>(0, 0) != 3.2
+      || p3.matrix.at<double>(1, 0) != 9.4
+      || p3.matrix.at<double>(2, 0) != 17.6
+      || p3.matrix.at<double>(3, 0) != 1.0
+      )
+  {
+    errors++;
+    std::cout << "ERROR with translate(std::vector<double>)" << std::endl;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // rotate
+  //--------------------------------------------------------------------------------------------------------------------
+  std::cout << "test: rotate" << std::endl;
+  local_coordinate_frame.setTranslation
+  (
+    0.0 * cedar::unit::meters,
+    0.0 * cedar::unit::meters,
+    0.0 * cedar::unit::meters
+  );
+  local_coordinate_frame.rotate(0, cedar::aux::math::pi/2 * cedar::unit::radians);
+  local_coordinate_frame.rotate(1, cedar::aux::math::pi/4 * cedar::unit::radians);
+
+  cv::Mat D = local_coordinate_frame.getTransformation();
+  if
+  (
+    !cedar::aux::math::isZero(D.at<double>(0, 0) - sqrt(2.0)/2)
+    || !cedar::aux::math::isZero(D.at<double>(0, 1) - 0.0)
+    || !cedar::aux::math::isZero(D.at<double>(0, 2) - sqrt(2.0)/2)
+
+    || !cedar::aux::math::isZero(D.at<double>(1, 0) - sqrt(2.0)/2)
+    || !cedar::aux::math::isZero(D.at<double>(1, 1) - 0)
+    || !cedar::aux::math::isZero(D.at<double>(1, 2) - -sqrt(2.0)/2)
+
+    || !cedar::aux::math::isZero(D.at<double>(2, 0) - 0)
+    || !cedar::aux::math::isZero(D.at<double>(2, 1) - 1)
+    || !cedar::aux::math::isZero(D.at<double>(2, 2) - 0)
+
+    || !cedar::aux::math::isZero(D.at<double>(0, 3) - 0)
+    || !cedar::aux::math::isZero(D.at<double>(1, 3) - 0)
+    || !cedar::aux::math::isZero(D.at<double>(2, 3) - 0)
+    || !cedar::aux::math::isZero(D.at<double>(3, 0) - 0)
+    || !cedar::aux::math::isZero(D.at<double>(3, 1) - 0)
+    || !cedar::aux::math::isZero(D.at<double>(3, 2) - 0)
+    || !cedar::aux::math::isZero(D.at<double>(3, 3) - 1)
+  )
+  {
+    errors++;
+    std::cout << "ERROR with rotate()" << std::endl;
+  }
+
+  cv::Mat R = cv::Mat::zeros(4, 4, CV_64FC1);
+  R.at<double>(0, 1) = -1;
+  R.at<double>(1, 2) = -1;
+  R.at<double>(2, 0) = 1;
+  local_coordinate_frame.setRotation(R);
+  cv::Mat R_prime = local_coordinate_frame.getRotation();
+  if
+  (
+    !cedar::aux::math::isZero(R_prime.at<double>(0, 0) - 0)
+    || !cedar::aux::math::isZero(R_prime.at<double>(0, 1) - -1)
+    || !cedar::aux::math::isZero(R_prime.at<double>(0, 2) - 0)
+
+    || !cedar::aux::math::isZero(R_prime.at<double>(1, 0) - 0)
+    || !cedar::aux::math::isZero(R_prime.at<double>(1, 1) - 0)
+    || !cedar::aux::math::isZero(R_prime.at<double>(1, 2) - -1)
+
+    || !cedar::aux::math::isZero(R_prime.at<double>(2, 0) - 1)
+    || !cedar::aux::math::isZero(R_prime.at<double>(2, 1) - 0)
+    || !cedar::aux::math::isZero(R_prime.at<double>(2, 2) - 0)
+  )
+  {
+    errors++;
+    std::cout << "ERROR with setRotation(Mat)" << std::endl;
+  }
+
+  cedar::aux::write(R_prime);
+
+  std::vector<double> rotation;
+  rotation.push_back(0);
+  rotation.push_back(0);
+  rotation.push_back(1);
+  rotation.push_back(0);
+  rotation.push_back(1);
+  rotation.push_back(0);
+  rotation.push_back(1);
+  rotation.push_back(0);
+  rotation.push_back(0);
+  local_coordinate_frame.setRotation(rotation);
+  R_prime = local_coordinate_frame.getRotation();
+  if (
+      !cedar::aux::math::isZero(R_prime.at<double>(0, 0) - 0)
+      || !cedar::aux::math::isZero(R_prime.at<double>(0, 1) - 0)
+      || !cedar::aux::math::isZero(R_prime.at<double>(0, 2) - 1)
+
+      || !cedar::aux::math::isZero(R_prime.at<double>(1, 0) - 0)
+      || !cedar::aux::math::isZero(R_prime.at<double>(1, 1) - 1)
+      || !cedar::aux::math::isZero(R_prime.at<double>(1, 2) - 0)
+
+      || !cedar::aux::math::isZero(R_prime.at<double>(2, 0) - 1)
+      || !cedar::aux::math::isZero(R_prime.at<double>(2, 1) - 0)
+      || !cedar::aux::math::isZero(R_prime.at<double>(2, 2) - 0)
+      )
+  {
+    errors++;
+    std::cout << "ERROR with setRotation(std::vector<double>)" << std::endl;
+  }
+
+  cedar::aux::write(R_prime);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // transformation
+  //--------------------------------------------------------------------------------------------------------------------
+  std::cout << "test: transformation" << std::endl;
+  cv::Mat T = cv::Mat::ones(4, 4, CV_64FC1);
+  T.at<double>(0, 0) = 0;
+  T.at<double>(1, 0) = 1;
+  T.at<double>(2, 0) = 0;
+  T.at<double>(0, 1) = -1;
+  T.at<double>(1, 1) = 0;
+  T.at<double>(2, 1) = 0;
+  T.at<double>(0, 2) = 0;
+  T.at<double>(1, 2) = 0;
+  T.at<double>(2, 2) = 1;
+  T.at<double>(0, 3) = 5;
+  T.at<double>(1, 3) = 6;
+  T.at<double>(2, 3) = 7;
+  local_coordinate_frame.setTransformation(T);
+  if (
+      local_coordinate_frame.getTransformation().at<double>(0, 0) != 0.0
+      || local_coordinate_frame.getTransformation().at<double>(1, 0) != 1.0
+      || local_coordinate_frame.getTransformation().at<double>(2, 0) != 0.0
+      || local_coordinate_frame.getTransformation().at<double>(0, 1) != -1.0
+      || local_coordinate_frame.getTransformation().at<double>(1, 1) != 0.0
+      || local_coordinate_frame.getTransformation().at<double>(2, 1) != 0.0
+      || local_coordinate_frame.getTransformation().at<double>(0, 2) != 0.0
+      || local_coordinate_frame.getTransformation().at<double>(1, 2) != 0.0
+      || local_coordinate_frame.getTransformation().at<double>(2, 2) != 1.0
+      || local_coordinate_frame.getTransformation().at<double>(0, 3) != 5.0
+      || local_coordinate_frame.getTransformation().at<double>(1, 3) != 6.0
+      || local_coordinate_frame.getTransformation().at<double>(2, 3) != 7.0
+      )
+  {
+    errors++;
+    std::cout << "ERROR with setTransformation(cv::Mat) or getTransformation()" << std::endl;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // readConfiguration
+  //--------------------------------------------------------------------------------------------------------------------
+  std::cout << "test: readConfiguration" << std::endl;
+  cedar::aux::LocalCoordinateFrame configured_local_coordinate_frame;
+  std::cout << "constructed cofigured_local_coordinate_frame" << std::endl;
+  configured_local_coordinate_frame.readJson("test://unit/auxiliaries/LocalCoordinateFrame/test.json");
+  std::cout << "called cofigured_local_coordinate_frame.readJson()" << std::endl;
+  cv::Mat C = configured_local_coordinate_frame.getTransformation();
+  if (
+      !cedar::aux::math::isZero(C.at<double>(0, 0) - cos(cedar::aux::math::pi/6))
+      || !cedar::aux::math::isZero(C.at<double>(0, 1) - 0)
+      || !cedar::aux::math::isZero(C.at<double>(0, 2) - -0.5)
+      || !cedar::aux::math::isZero(C.at<double>(1, 0) - 0)
+      || !cedar::aux::math::isZero(C.at<double>(1, 1) - 1)
+      || !cedar::aux::math::isZero(C.at<double>(1, 2) - 0)
+      || !cedar::aux::math::isZero(C.at<double>(2, 0) - 0.5)
+      || !cedar::aux::math::isZero(C.at<double>(2, 1) - 0)
+      || !cedar::aux::math::isZero(C.at<double>(2, 2) - cos(cedar::aux::math::pi/6))
+      || !cedar::aux::math::isZero(C.at<double>(0, 3) - 2.0)
+      || !cedar::aux::math::isZero(C.at<double>(1, 3) - 2.2)
+      || !cedar::aux::math::isZero(C.at<double>(2, 3) - 2.5)
+      || !cedar::aux::math::isZero(C.at<double>(3, 0) - 0)
+      || !cedar::aux::math::isZero(C.at<double>(3, 1) - 0)
+      || !cedar::aux::math::isZero(C.at<double>(3, 2) - 0)
+      || !cedar::aux::math::isZero(C.at<double>(3, 3) - 1)
+      )
+  {
+    errors++;
+    std::cout << "ERROR with configured_local_coordinate_frame(const std::string& configFileName)" << std::endl;
+  }
+
+  std::cout << "test finished, there were " << errors << " errors" << std::endl;
+  if (errors > 255)
+  {
+    errors = 255;
+  }
+  return errors;
+}
