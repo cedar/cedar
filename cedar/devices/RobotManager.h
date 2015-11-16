842294f68fd67245f355e5cc9ae7590baba68afa
Oliver Lomp <oliver.lomp@ini.ruhr-uni-bochum.de>
2015-11-16 17:38:41 +0100

    Merge with d0fb939dc6fae34dffe6b0fcfd86bee27ef66562

---
+++ cedar/devices/RobotManager.h
@@ -0,0 +1,261 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014, 2015 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        RobotManager.h
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2013 02 28
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_DEV_ROBOT_MANAGER_H
+#define CEDAR_DEV_ROBOT_MANAGER_H
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include "cedar/devices/namespace.h"
+#include "cedar/auxiliaries/Singleton.h"
+#include "cedar/auxiliaries/Path.h"
+
+// SYSTEM INCLUDES
+#ifndef Q_MOC_RUN
+  #include <boost/signals2.hpp>
+#endif // Q_MOC_RUN
+#include <map>
+#include <vector>
+#include <string>
+
+
+/*!@todo describe.
+ *
+ * @todo describe more.
+ */
+class cedar::dev::RobotManager
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // friends
+  //--------------------------------------------------------------------------------------------------------------------
+  friend class cedar::aux::Singleton<cedar::dev::RobotManager>;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // nested types
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  class Template
+  {
+    public:
+      void addNamedConfiguration(const std::string& name, const cedar::aux::Path& resource);
+
+      std::vector<std::string> getConfigurationNames() const;
+
+      inline void setIconPath(const std::string& iconPath)
+      {
+        this->mIconPath = iconPath;
+      }
+
+      inline const std::string& getIconPath() const
+      {
+        return this->mIconPath;
+      }
+
+      cedar::aux::Path getConfiguration(const std::string& name) const;
+
+    private:
+      std::map<std::string, cedar::aux::Path> mNamedPaths;
+
+      std::string mIconPath;
+  };
+
+private:
+  struct RobotInfo
+  {
+    std::string mTemplateName;
+    std::string mLoadedTemplateConfigurationName;
+    std::string mLoadedTemplateConfiguration;
+  };
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief Destructor
+  virtual ~RobotManager();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  void addRobotName(const std::string& robotName);
+
+  void removeRobot(const std::string& robotName);
+
+  void loadRobotConfiguration(const std::string& robotName, const cedar::aux::Path& configuration);
+
+  void loadRobotTemplateConfiguration(const std::string& robotName, const std::string& configurationName);
+
+  void setRobotTemplateConfigurationName(const std::string& robotName, const std::string& templateName);
+
+  cedar::dev::RobotPtr getRobot(const std::string& robotName) const;
+
+  const std::string& getRobotName(cedar::dev::ConstRobotPtr robot) const;
+
+  std::string getNewRobotName() const;
+
+  void addRobotTemplate(const std::string& name, const Template& robotTemplate);
+
+  /*!@brief Get a list of all the robot templates known to the robot manager
+   */
+  std::vector<std::string> getRobotTemplateNames() const;
+
+  /*!@brief Get a list of all the robots stored in the manager.
+   */
+  std::vector<std::string> getRobotNames() const;
+
+  const Template& getTemplate(const std::string& name) const;
+
+  void setRobotTemplateName(const std::string& robotName, const std::string& templateName);
+
+  const std::string& getRobotTemplateName(const std::string& robotName) const;
+
+  inline boost::signals2::connection connectToRobotNameAddedSignal(boost::function<void (const std::string&)> slot)
+  {
+    return this->mRobotNameAddedSignal.connect(slot);
+  }
+
+  inline boost::signals2::connection connectToRobotConfigurationChangedSignal(boost::function<void (const std::string&)> slot)
+  {
+    return this->mRobotConfigurationLoadedSignal.connect(slot);
+  }
+
+  inline boost::signals2::connection connectToRobotRemovedSignal(boost::function<void (const std::string&)> slot)
+  {
+    return this->mRobotRemovedSignal.connect(slot);
+  }
+
+  /*!
+   * @remarks Throws if no template has been loaded for the robot.
+   */
+  const std::string& getRobotTemplateConfigurationName(const std::string& robotName) const;
+
+  /*!
+   * @remarks Throws if no configuration is loaded for the robot.
+   */
+  const std::string& getRobotTemplateConfiguration(const std::string& robotName) const;
+
+  /*!@brief  Finds the component slot specified by the given path.
+   *         The slot must be given in the form "robot name.slot name".
+   */
+  cedar::dev::ComponentSlotPtr findComponentSlot(const std::string& componentPath) const;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  //!@brief The standard constructor. Private beacuse this is a singleton object.
+  RobotManager();
+
+  RobotInfo& retrieveRobotInfo(const std::string& robotName)
+  {
+    auto iter = this->mRobotInfos.find(robotName);
+    if (iter == this->mRobotInfos.end())
+    {
+      this->mRobotInfos[robotName] = RobotInfo();
+      return this->mRobotInfos[robotName];
+    }
+    else
+    {
+      return iter->second;
+    }
+  }
+
+  //! Saves the state of the robot manager to a file.
+  void store() const;
+
+  //! Restores the state of the robot manager.
+  void restore();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+private:
+  std::map<std::string, cedar::dev::RobotPtr> mRobotInstances;
+
+  std::map<std::string, cedar::aux::Path> mRobotConfigurations;
+
+  //! An association from robot names to the template that have been set for them.
+  std::map<std::string, RobotInfo> mRobotInfos;
+
+  boost::signals2::signal<void (const std::string&)> mRobotNameAddedSignal;
+
+  boost::signals2::signal<void (const std::string&)> mRobotRemovedSignal;
+
+  boost::signals2::signal<void (const std::string&)> mRobotConfigurationLoadedSignal;
+
+  std::map<std::string, Template> mRobotTemplates;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+private:
+  // none yet
+
+}; // class cedar::dev::RobotManager
+
+#include "cedar/auxiliaries/FactoryManager.h"
+
+namespace cedar
+{
+  namespace dev
+  {
+    //!@brief The singleton instance of the robot manager.
+    typedef cedar::aux::Singleton<cedar::dev::RobotManager> RobotManagerSingleton;
+  }
+
+  namespace aux
+  {
+    //!@cond SKIPPED_DOCUMENTATION
+    CEDAR_INSTANTIATE_DEV_TEMPLATE(cedar::aux::Singleton<cedar::dev::RobotManager>);
+    //!@endcond
+  }
+}
+
+#endif // CEDAR_DEV_ROBOT_MANAGER_H
---
+++ cedar/processing/steps/ScalarDivision.cpp
@@ -0,0 +1,162 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014, 2015 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        ScalarDivision.cpp
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2014 04 17
+
+    Description: Source file for the class cedar::proc::steps::ScalarDivision.
+
+    Credits:
+
+======================================================================================================================*/
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include "cedar/processing/steps/ScalarDivision.h"
+#include "cedar/processing/ElementDeclaration.h"
+#include "cedar/processing/typecheck/Matrix.h"
+#include "cedar/auxiliaries/MatData.h"
+
+// SYSTEM INCLUDES
+
+
+//----------------------------------------------------------------------------------------------------------------------
+// register the class
+//----------------------------------------------------------------------------------------------------------------------
+namespace
+{
+  bool declare()
+  {
+    using cedar::proc::ElementDeclarationPtr;
+    using cedar::proc::ElementDeclarationTemplate;
+
+    ElementDeclarationPtr multiply_decl
+    (
+      new ElementDeclarationTemplate<cedar::proc::steps::ScalarDivision>
+      (
+        "Utilities",
+        "cedar.processing.ScalarDivision"
+      )
+    );
+    multiply_decl->setIconPath(":/steps/scalar_division.svg");
+    multiply_decl->setDescription
+    (
+      "Divides an input matrix by a scalar that can also be input into the step."
+    );
+
+    multiply_decl->declare();
+
+    return true;
+  }
+
+  bool declared = declare();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// constructors and destructor
+//----------------------------------------------------------------------------------------------------------------------
+
+cedar::proc::steps::ScalarDivision::ScalarDivision()
+:
+mResult(new cedar::aux::MatData(cv::Mat())),
+_mTreatDivZero(new cedar::aux::BoolParameter(this, "treat division by zero", true)),
+_mDivZeroReplacement(new cedar::aux::DoubleParameter(this, "zero divisor replacement", 0.000001, cedar::aux::DoubleParameter::LimitType::positiveZero()))
+{
+  auto matrix_slot = this->declareInput("matrix");
+  cedar::proc::typecheck::Matrix matrix_check;
+  matrix_check.addAcceptedType(CV_32F);
+  matrix_slot->setCheck(matrix_check);
+
+  auto divisor_slot = this->declareInput("divisor");
+  cedar::proc::typecheck::Matrix divisor_check;
+  divisor_check.addAcceptedDimensionality(0);
+  divisor_check.addAcceptedType(CV_32F);
+  divisor_slot->setCheck(divisor_check);
+
+  this->declareOutput("result", this->mResult);
+
+  QObject::connect(this->_mTreatDivZero.get(), SIGNAL(valueChanged()), this, SLOT(zeroDivisionTreatmentChanged()));
+  QObject::connect(this->_mDivZeroReplacement.get(), SIGNAL(valueChanged()), this, SLOT(zeroDivisionTreatmentChanged()));
+  this->_mDivZeroReplacement->setConstant(true);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// methods
+//----------------------------------------------------------------------------------------------------------------------
+
+void cedar::proc::steps::ScalarDivision::zeroDivisionTreatmentChanged()
+{
+  this->_mDivZeroReplacement->setConstant(this->_mTreatDivZero->getValue() != true);
+  this->onTrigger();
+}
+
+void cedar::proc::steps::ScalarDivision::compute(const cedar::proc::Arguments&)
+{
+  float divisor = this->mDivisor->getData().at<float>(0, 0);
+  const cv::Mat& matrix = this->mMatrix->getData();
+
+  if (this->_mTreatDivZero->getValue() && std::abs(divisor) <= std::numeric_limits<float>::epsilon())
+  {
+    this->mResult->setData(matrix / this->_mDivZeroReplacement->getValue());
+  }
+  else
+  {
+    this->mResult->setData(matrix / divisor);
+  }
+}
+
+void cedar::proc::steps::ScalarDivision::inputConnectionChanged(const std::string& slotName)
+{
+  auto mat_data = boost::dynamic_pointer_cast<cedar::aux::ConstMatData>(this->getInput(slotName));
+  if (slotName == "matrix")
+  {
+    this->mMatrix = mat_data;
+
+    // allocate an output matrix of appropriate size
+    if (mat_data)
+    {
+      this->mResult->setData(mat_data->getData().clone());
+    }
+    else
+    {
+      this->mResult->setData(cv::Mat());
+    }
+
+    // tell successive steps that the output changed
+    this->emitOutputPropertiesChangedSignal("result");
+  }
+  else if (slotName == "divisor")
+  {
+    this->mDivisor = mat_data;
+  }
+  else
+  {
+    CEDAR_ASSERT(false && "This should not happen.");
+  }
+}
