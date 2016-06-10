2d3e4a15f2cd9a04a1b4aecd609ddd263a65f56c
jokeit <37776957+jsjokeit@users.noreply.github.com>
2016-06-10 18:51:44 +0200

    scroll less when loading robot JSONs

---
+++ cedar/devices/gui/RobotCard.h
@@ -0,0 +1,177 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014, 2015 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        RobotCard.h
+
+    Maintainer:  Oliver Lomp
+    Email:       oliver.lomp@ini.ruhr-uni-bochum.de
+    Date:        2013 03 04
+
+    Description:
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_DEV_GUI_ROBOT_CARD_H
+#define CEDAR_DEV_GUI_ROBOT_CARD_H
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include "cedar/devices/gui/namespace.h"
+#include "cedar/devices/namespace.h"
+
+// SYSTEM INCLUDES
+#include <QFrame>
+#include <QLabel>
+#include <QListWidgetItem>
+#include <QComboBox>
+#include <QLineEdit>
+#include <QPushButton>
+#ifndef Q_MOC_RUN
+  #include <boost/signals2.hpp>
+#endif // Q_MOC_RUN
+#include <string>
+
+
+/*!@brief A class that displays an icon in the RobotCard widget.
+ */
+class cedar::dev::gui::RobotCardIconHolder : public QLabel
+{
+  Q_OBJECT
+
+  public:
+    RobotCardIconHolder(cedar::dev::gui::RobotCard* pCard);
+
+    std::string getRobotName() const;
+
+    void setIconFromTemplate(const std::string& templateName);
+
+  signals:
+    void robotDropped(const QString& robotName);
+
+  protected:
+    void dragEnterEvent(QDragEnterEvent* pEvent);
+    void dropEvent(QDropEvent* pEvent);
+
+  private:
+    QListWidgetItem* itemFromMime(QDropEvent* pEvent);
+
+    void setRobotFromTemplate(const std::string& name);
+
+  private:
+    cedar::dev::gui::RobotCard* mpCard;
+};
+
+
+
+/*!@todo describe.
+ *
+ * @todo describe more.
+ */
+class cedar::dev::gui::RobotCard : public QFrame
+{
+  Q_OBJECT
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // nested types
+  //--------------------------------------------------------------------------------------------------------------------
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief The standard constructor.
+  RobotCard(const QString& robotName);
+
+  //!@brief Destructor
+  virtual ~RobotCard();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  std::string getRobotName() const;
+
+  std::string getRobotTemplateName() const
+  {
+    return this->mRobotTemplateName;
+  }
+
+  void setRobotTemplate(const std::string& templateName);
+
+public slots:
+  void robotDropped(const QString& robotName);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private slots:
+  void robotNameEditValueChanged(const QString& robotName);
+
+private:
+  void robotRemoved(const std::string& robotName);
+
+  void updateConnectionIcon();
+
+private slots:
+  void selectedConfigurationChanged(int index);
+
+  void deleteClicked();
+
+  void connectClicked();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+private:
+  QComboBox* mpConfigurationSelector;
+
+  QLineEdit* mpRobotNameEdit;
+
+  cedar::dev::gui::RobotCardIconHolder* mpIcon;
+
+  QPushButton* mpConnectButton;
+
+  std::string mRobotTemplateName;
+
+  cedar::dev::RobotPtr mRobot;
+
+  boost::signals2::connection mRobotRemovedConnection;
+
+  std::string mCurrentName;
+}; // class cedar::dev::gui::RobotCard
+
+#endif // CEDAR_DEV_GUI_ROBOT_CARD_H
+
