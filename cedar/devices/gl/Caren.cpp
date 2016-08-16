4e12fd2499ffbfc629d1e8caeac810c1aa37fd8f
Nico Kuerschner <Nico.Kuerschner@ini.rub.de>
2016-08-16 17:43:55 +0200

    outcommented cedar::dev::gl::Caren::initializeGl() content, as for now it only causes problems

--- cedar/devices/gl/Caren.cpp
+++ cedar/devices/gl/Caren.cpp
@@ -89,8 +89,8 @@ void cedar::dev::gl::Caren::initializeGl()
   //cedar::dev::KinematicChainPtr fingerTwo = boost::dynamic_pointer_cast <cedar::dev::KinematicChain> (mRobot->getComponent("fingerTwo"));
   //cedar::dev::KinematicChainPtr fingerThree = boost::dynamic_pointer_cast <cedar::dev::KinematicChain> (mRobot->getComponent("fingerThree"));
 
-  mTrunk = boost::dynamic_pointer_cast <cedar::dev::KinematicChain>(mRobot->getComponent("trunk"));
-  mArm = boost::dynamic_pointer_cast <cedar::dev::KinematicChain>(mRobot->getComponent("arm"));
+  //mTrunk = boost::dynamic_pointer_cast <cedar::dev::KinematicChain>(mRobot->getComponent("trunk"));
+  //mArm = boost::dynamic_pointer_cast <cedar::dev::KinematicChain>(mRobot->getComponent("arm"));
 
   //mHead = head;
   //mPalm = palm;
@@ -98,14 +98,14 @@ void cedar::dev::gl::Caren::initializeGl()
   //mFingerTwo = fingerTwo;
   //mFingerThree = fingerThree;
 
-  mTrunkVisualization = cedar::dev::gl::KinematicChainPtr( new cedar::dev::gl::PowerCube110(mTrunk) );
-  mArmVisualization = cedar::dev::gl::KinematicChainPtr( new cedar::dev::gl::KukaArm(mArm) );
+  //mTrunkVisualization = cedar::dev::gl::KinematicChainPtr( new cedar::dev::gl::PowerCube110(mTrunk) );
+  //mArmVisualization = cedar::dev::gl::KinematicChainPtr( new cedar::dev::gl::KukaArm(mArm) );
 
   //mHandVisualization = cedar::dev::gl::SdhPtr( new cedar::dev::gl::Sdh(fingerOne, fingerTwo, fingerThree, palm) );
   //mHeadVisualization = cedar::dev::gl::KinematicChainPtr( new cedar::dev::gl::PowerCubeWrist90(head) );
 
-  mTrunkVisualization->initializeGl();
-  mArmVisualization->initializeGl();
+  //mTrunkVisualization->initializeGl();
+  //mArmVisualization->initializeGl();
   //mHandVisualization->initializeGl();
   //mHeadVisualization->initializeGl();
 
