e775b80da57454f4e6af76f711e62a355afaa500
jokeit <jokeit@localhost>
2018-01-19 12:06:54 +0100

    minor text

--- cedar/processing/steps/ComponentMultiply.cpp
+++ cedar/processing/steps/ComponentMultiply.cpp
@@ -73,7 +73,7 @@ namespace
     multiply_decl->setIconPath(":/steps/component_multiply.svg");
     multiply_decl->setDescription
     (
-      "Multiplies two matrices component-wise."
+      "Multiplies two matrices element-wise."
     );
 
     multiply_decl->declare();
