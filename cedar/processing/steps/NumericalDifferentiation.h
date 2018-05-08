d270b8f8c99286a97bf60cfeff69eabff40e921d
jokeit <37776957+jsjokeit@users.noreply.github.com>
2018-05-08 14:58:30 +0200

    added the NumericDifferentiation Step

---
+++ cedar/processing/steps/NumericalDifferentiation.h
@@ -0,0 +1,124 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014, 2015, 2016, 2017 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+ 
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        NumericalDifferentiation.h
+
+    Maintainer:  jokeit
+    Email:       jean-stephane.jokeit@ini.ruhr-uni-bochum.de
+    Date:        
+
+    Description: Header file for the class cedar::proc::steps::NumericalDifferentiation.
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef CEDAR_PROC_STEPS__NUMERICAL_DIFFERENTIATION_H
+#define CEDAR_PROC_STEPS__NUMERICAL_DIFFERENTIATION_H
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+#include <cedar/processing/Step.h>
+#include <cedar/processing/InputSlotHelper.h>
+#include <cedar/auxiliaries/MatData.h>
+#include <cedar/auxiliaries/DoubleParameter.h>
+#include <cv.h>
+
+// FORWARD DECLARATIONS
+#include "cedar/processing/steps/NumericalDifferentiation.fwd.h"
+
+// SYSTEM INCLUDES
+
+
+/*!@todo describe.
+ *
+ * @todo describe more.
+ */
+class cedar::proc::steps::NumericalDifferentiation : public cedar::proc::Step
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  // nested types
+  //--------------------------------------------------------------------------------------------------------------------
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // constructors and destructor
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  //!@brief The standard constructor.
+  NumericalDifferentiation();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // public methods
+  //--------------------------------------------------------------------------------------------------------------------
+public:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // protected methods
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // private methods
+  //--------------------------------------------------------------------------------------------------------------------
+private:
+  void inputConnectionChanged(const std::string& inputName);
+
+  void compute(const cedar::proc::Arguments& arguments);
+  void recompute();
+  void reset();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // members
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+  // none yet
+private:
+  //!@brief MatrixData representing the input. Storing it like this saves time during computation.
+  cedar::aux::ConstMatDataPtr mInput;
+  cedar::aux::ConstMatDataPtr mDelayOptional;
+
+  //!@brief The output data.
+  cedar::aux::MatDataPtr mOutput;
+
+  cv::Mat mOneBack;
+  cv::Mat mTwoBack;
+  cv::Mat mThreeBack;
+  cv::Mat mFourBack;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // parameters
+  //--------------------------------------------------------------------------------------------------------------------
+protected:
+
+private:
+  cedar::unit::Time mLastTime;
+
+}; // class cedar::proc::steps::NumericalDifferentiation
+
+#endif // CEDAR_PROC_STEPS__NUMERICAL_DIFFERENTIATION_H
+
