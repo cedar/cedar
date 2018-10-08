96ac6912561c1868a2aa99821541cbbccfc1a857
Mathis Richter <mathis.richter@ini.rub.de>
2018-10-08 12:19:13 +0200

    Renamed CAREN configurations.

--- cedar/devices/RobotManager.cpp
+++ cedar/devices/RobotManager.cpp
@@ -70,21 +70,13 @@ cedar::dev::RobotManager::RobotManager()
   khepera_template.addNamedConfiguration("serial", cedar::aux::Path("resource://robots/khepera/serial_configuration.json"));
   this->addRobotTemplate("khepera", khepera_template);
 
-  // kuka ---------------------------------------------------------------------------------------------------------
-  cedar::dev::RobotManager::Template kuka_template;
-  kuka_template.setIconPath(":/cedar/dev/gui/icons/caren_icon_256.png");
-  kuka_template.addNamedConfiguration("FRI", cedar::aux::Path("resource://robots/caren/fri_configuration.json"));
-  kuka_template.addNamedConfiguration("simulator", cedar::aux::Path("resource://robots/caren/simulator_configuration.json"));
-  kuka_template.addNamedConfiguration("yarp", cedar::aux::Path("resource://robots/caren/yarp_configuration.json"));
-  this->addRobotTemplate("kuka", kuka_template);
-
   // caren ---------------------------------------------------------------------------------------------------------
   cedar::dev::RobotManager::Template caren_template;
   caren_template.setIconPath(":/cedar/dev/gui/icons/caren_icon_256.png");
-  caren_template.addNamedConfiguration("FRI", cedar::aux::Path("resource://robots/caren_test/fri_configuration.json"));
-  caren_template.addNamedConfiguration("simulator", cedar::aux::Path("resource://robots/caren_test/simulator_configuration.json"));
-  caren_template.addNamedConfiguration("yarp", cedar::aux::Path("resource://robots/caren_test/yarp_configuration.json"));
-  caren_template.addNamedConfiguration("real", cedar::aux::Path("resource://robots/caren_test/real_configuration.json"));
+  caren_template.addNamedConfiguration("hardware", cedar::aux::Path("resource://robots/caren/hardware_configuration.json"));
+  caren_template.addNamedConfiguration("simulator", cedar::aux::Path("resource://robots/caren/simulator_configuration.json"));
+  caren_template.addNamedConfiguration("webots", cedar::aux::Path("resource://robots/caren/webots_configuration.json"));
+  caren_template.addNamedConfiguration("test", cedar::aux::Path("resource://robots/caren/test_configuration.json"));
   this->addRobotTemplate("caren", caren_template);
 
   // youbot
