458827dddcc7b714a60460468a53695194ff19bd
raul-ini <37666884+raul-ini@users.noreply.github.com>
2018-11-10 19:28:51 +0100

    Update FindQGLViewer.cmake

--- cmake/findScripts/FindQGLViewer.cmake
+++ cmake/findScripts/FindQGLViewer.cmake
@@ -29,7 +29,7 @@ else(NOT CEDAR_USE_QT5)
     else(NOT WIN32)
     find_library(QGLViewer_LIBS
                 NAMES qglviewer-qt5 QGLViewer-qt5 QGLViewer qglviewer QGLViewer2 QGLViewer2.dll
-                PATHS ${CEDAR_DEPENDENCY_QGLVIEWER}
+                PATHS ${CEDAR_DEPENDENCY_LIBRARIES}
             )
     endif(NOT WIN32)
   endif(APPLE)
