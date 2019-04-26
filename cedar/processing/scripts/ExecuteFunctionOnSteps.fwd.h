673f0dbd680a361f23dc5312740f4df4b2352774
tekuejpp <jan.tekuelve@ini.rub.de>
2019-04-26 14:46:35 +0200

    Added a Cpp Script that allows to execute a function on all instances of a particular class in the architecture. However each function needs to be implemented for that: Right now it may only reset the weights of all Memory Traces (Preshapes) or HebbianConnections

---
+++ cedar/processing/scripts/ExecuteFunctionOnSteps.fwd.h
@@ -0,0 +1,63 @@
+/*======================================================================================================================
+
+    Copyright 2011, 2012, 2013, 2014, 2015 Institut fuer Neuroinformatik, Ruhr-Universitaet Bochum, Germany
+
+    This file is part of cedar.
+
+    cedar is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    cedar is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with cedar. If not, see <http://www.gnu.org/licenses/>.
+
+========================================================================================================================
+
+    Institute:   Ruhr-Universitaet Bochum
+                 Institut fuer Neuroinformatik
+
+    File:        RecruitGroup.fwd.h
+
+    Maintainer:  Jan Tekülve
+    Email:       jan.tekuelve@ini.rub.de
+    Date:        2016 11 03
+
+    Description: Forward declaration file for the class ExecuteFunctionOnSteps
+
+    Credits:
+
+======================================================================================================================*/
+
+#ifndef EXECUTE_FUNCTION_ON_STEPS_FWD_H
+#define EXECUTE_FUNCTION_ON_STEPS_FWD_H
+
+// CEDAR CONFIGURATION
+#include "cedar/configuration.h"
+
+// CEDAR INCLUDES
+
+// SYSTEM INCLUDES
+#ifndef Q_MOC_RUN
+#include <boost/smart_ptr.hpp>
+#endif // Q_MOC_RUN
+
+
+namespace cedar
+{
+  namespace proc
+  {
+    namespace scripts
+    {
+      //!@cond SKIPPED_DOCUMENTATION
+      class ExecuteFunctionOnSteps;
+      //!@endcond
+    }
+  }
+}
+#endif // EXECUTE_FUNCTION_ON_STEPS_FWD_H
