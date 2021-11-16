0b18a4bcb28badb54eb4e7c5ac42e758f07fb277
Jan Tekülve <jan.tekuelve@ini.rub.de>
2021-11-16 15:49:44 +0100

    Fixed an error in the gui::Scene that caused a recurring RTTI error on Windows

--- cedar/processing/gui/Scene.cpp
+++ cedar/processing/gui/Scene.cpp
@@ -1133,11 +1133,14 @@ void cedar::proc::gui::Scene::mouseReleaseEvent(QGraphicsSceneMouseEvent *pMouse
   //Case 3: Moving between same groups
   else
   {
-    //First item of list is enough, since all have the same group
-    if(cedar::proc::gui::Element* guiElement = dynamic_cast<cedar::proc::gui::Element*>(items_to_move.front()))
-    {
-      targetGroup = this->getGroupFor(guiElement->getElement()->getGroup().get());
-    }
+	if(items_to_move.size() != 0)
+	{
+		//First item of list is enough, since all have the same group
+		if(cedar::proc::gui::Element* guiElement = dynamic_cast<cedar::proc::gui::Element*>(items_to_move.front()))
+		{
+		  targetGroup = this->getGroupFor(guiElement->getElement()->getGroup().get());
+		}
+	}
   }
 
   if (this->mDraggingItems && this->mpDraggingGraphicsBase && this->mStartMovingPositionOfClicked != this->mpDraggingGraphicsBase->pos())
